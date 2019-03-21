5639a4ad433998fb6cf8d82eae41458209b7c79a
Tyfyter <tyfyter2616@gmail.com>
2019-03-21 11:49:33 -0400

    Finally got the repository working

---
+++ Items/name.cs
@@ -0,0 +1,38 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Items
+{
+	public class name : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("name");
+			Tooltip.SetDefault("This is a modded sword.");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 50;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Origins.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace Origins
+{
+	class Origins : Mod
+	{
+		public Origins()
+		{
+		}
+	}
+}
