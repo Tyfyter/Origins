d4ba385f1a5f041cc2f64dee82958fee1c8c35d2
Tyfyter <Tyfyter@users.noreply.github.com>
2021-09-08 21:43:37 -0400

    made spacing in UIUtils.cs more consistent

--- UI/UIUtils.cs
+++ UI/UIUtils.cs
@@ -17,14 +17,14 @@ namespace Tyfyter.Utils {
             ItemSlot.Draw(spriteBatch, ref item, ItemSlot.Context.ChatItem, destination.TopLeft(), lightColor);
         }
         public static void DrawColoredItemSlot(SpriteBatch spriteBatch, ref Item item, Vector2 position, Texture2D backTexture, Color slotColor, Color lightColor = default) {
-			spriteBatch.Draw(backTexture, position, null, slotColor, 0f, default(Vector2), Main.inventoryScale, SpriteEffects.None, 0f);
-		    ItemSlot.Draw(spriteBatch, ref item, ItemSlot.Context.ChatItem, position, lightColor);
+            spriteBatch.Draw(backTexture, position, null, slotColor, 0f, default(Vector2), Main.inventoryScale, SpriteEffects.None, 0f);
+            ItemSlot.Draw(spriteBatch, ref item, ItemSlot.Context.ChatItem, position, lightColor);
         }
         public class SingleItemSlotUI : UIState {
             public virtual float SlotX => 0;
             public virtual float SlotY => 0;
             public RefItemSlot itemSlot = null;
-			protected internal Queue<Action> itemSlotQueue = new Queue<Action>() { };
+            protected internal Queue<Action> itemSlotQueue = new Queue<Action>() { };
             /// <summary>
             /// Passes the parameters to an action to be added in Update
             /// </summary>
@@ -72,61 +72,61 @@ namespace Tyfyter.Utils {
                 base.Update(gameTime);
             }
         }
-		public class RefItemSlot : UIElement {
-			public static Color MissingSlotColor => new Color(160, 160, 160, 160);
-			public bool slotSourceMissing = false;
+        public class RefItemSlot : UIElement {
+            public static Color MissingSlotColor => new Color(160, 160, 160, 160);
+            public bool slotSourceMissing = false;
             public bool shiftClickToInventory = false;
             internal Ref<Item> item;
-			internal readonly int _context;
-			private readonly float _scale;
-			internal Func<Item, bool> ValidItemFunc;
-			protected internal int index = -1;
-			public Color colorMult = default;
-			public (Texture2D texture, Color color)[] extraTextures = new (Texture2D, Color)[] { };
-			public RefItemSlot(int context = ItemSlot.Context.InventoryItem, float scale = 1f, Ref<Item> _item = null) {
-				_context = context;
-				_scale = scale;
-				item = _item;
-				Width.Set(Main.inventoryBack9Texture.Width * scale, 0f);
-				Height.Set(Main.inventoryBack9Texture.Height * scale, 0f);
-			}
-			protected override void DrawSelf(SpriteBatch spriteBatch) {
-				float oldScale = Main.inventoryScale;
-				Main.inventoryScale = 0.85f * _scale;
-				Rectangle rectangle = GetDimensions().ToRectangle();
-				rectangle.Width = (int)(rectangle.Width * Main.inventoryScale);
-				rectangle.Height = (int)(rectangle.Width * Main.inventoryScale);
+            internal readonly int _context;
+            private readonly float _scale;
+            internal Func<Item, bool> ValidItemFunc;
+            protected internal int index = -1;
+            public Color colorMult = default;
+            public (Texture2D texture, Color color)[] extraTextures = new (Texture2D, Color)[] { };
+            public RefItemSlot(int context = ItemSlot.Context.InventoryItem, float scale = 1f, Ref<Item> _item = null) {
+                _context = context;
+                _scale = scale;
+                item = _item;
+                Width.Set(Main.inventoryBack9Texture.Width * scale, 0f);
+                Height.Set(Main.inventoryBack9Texture.Height * scale, 0f);
+            }
+            protected override void DrawSelf(SpriteBatch spriteBatch) {
+                float oldScale = Main.inventoryScale;
+                Main.inventoryScale = 0.85f * _scale;
+                Rectangle rectangle = GetDimensions().ToRectangle();
+                rectangle.Width = (int)(rectangle.Width * Main.inventoryScale);
+                rectangle.Height = (int)(rectangle.Width * Main.inventoryScale);
 
-				if (ContainsPoint(Main.MouseScreen) && !PlayerInput.IgnoreMouseInterface) {
-					Main.LocalPlayer.mouseInterface = true;
+                if (ContainsPoint(Main.MouseScreen) && !PlayerInput.IgnoreMouseInterface) {
+                    Main.LocalPlayer.mouseInterface = true;
                     int context = _context;
                     if (shiftClickToInventory && ItemSlot.ShiftInUse) {
                         context = ItemSlot.Context.EquipAccessory;
                     }
-					if (slotSourceMissing) {
-						if (Main.mouseItem?.IsAir ?? true) {
-							ItemSlot.Handle(ref item.Value, context);
-						}
-					} else {
-						if (ValidItemFunc == null || ValidItemFunc(Main.mouseItem)) {
-							ItemSlot.Handle(ref item.Value, context);
-						}
-					}
-				}
-				// Draw draws the slot itself and Item. Depending on context, the color will change, as will drawing other things like stack counts.
-				Color slotColor = colorMult;
+                    if (slotSourceMissing) {
+                        if (Main.mouseItem?.IsAir ?? true) {
+                            ItemSlot.Handle(ref item.Value, context);
+                        }
+                    } else {
+                        if (ValidItemFunc == null || ValidItemFunc(Main.mouseItem)) {
+                            ItemSlot.Handle(ref item.Value, context);
+                        }
+                    }
+                }
+                // Draw draws the slot itself and Item. Depending on context, the color will change, as will drawing other things like stack counts.
+                Color slotColor = colorMult;
                 Color slotColorMult = Color.White;
-				if (slotSourceMissing) {
+                if (slotSourceMissing) {
                     slotColorMult = MissingSlotColor;
-				}
-				Item _ = new Item();
-				DrawColoredItemSlot(spriteBatch, ref _, rectangle.TopLeft(), Main.inventoryBack13Texture, slotColor.MultiplyRGBA(slotColorMult));
-				foreach ((Texture2D texture, Color color) texture in extraTextures) {
-					spriteBatch.Draw(texture.texture, rectangle.TopLeft(), null, texture.color.MultiplyRGBA(slotColorMult), 0, default, Main.inventoryScale, SpriteEffects.None, 0);
-				}
-				ItemSlot.Draw(spriteBatch, ref item.Value, ItemSlot.Context.ChatItem, rectangle.TopLeft(), slotColorMult);
-				Main.inventoryScale = oldScale;
-			}
-		}
-	}
+                }
+                Item _ = new Item();
+                DrawColoredItemSlot(spriteBatch, ref _, rectangle.TopLeft(), Main.inventoryBack13Texture, slotColor.MultiplyRGBA(slotColorMult));
+                foreach ((Texture2D texture, Color color) texture in extraTextures) {
+                    spriteBatch.Draw(texture.texture, rectangle.TopLeft(), null, texture.color.MultiplyRGBA(slotColorMult), 0, default, Main.inventoryScale, SpriteEffects.None, 0);
+                }
+                ItemSlot.Draw(spriteBatch, ref item.Value, ItemSlot.Context.ChatItem, rectangle.TopLeft(), slotColorMult);
+                Main.inventoryScale = oldScale;
+            }
+        }
+    }
 }
