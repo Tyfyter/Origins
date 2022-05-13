89e7e4a9733427c4281fa1eaba4cbf77d71e6e3f
Tyfyter <tyfyter2616@gmail.com>
2022-05-13 01:14:33 -0400

    Defiled flying weapon enemy
    tri-
    word-forming element meaning "three, having three, once every three," from Latin tres (neuter tria) or Greek treis, trias "three"
    
    bi-
    word-forming element meaning "two, having two, twice, double, doubly, twofold, once every two," etc., from Latin bi- "twice, double," from Old Latin dvi- (cognate with Sanskrit dvi-, Greek di-, dis-, Old English twi-, German zwei- "twice, double")

--- Items/Prefixes.cs
+++ Items/Prefixes.cs
@@ -9,19 +9,21 @@ using Microsoft.Xna.Framework;
 
 namespace Origins.Items {
     public class Heavy_Cal_Prefix : ModPrefix {
-        public override PrefixCategory Category => PrefixCategory.Ranged;
+		public override PrefixCategory Category => PrefixCategory.Ranged;
+        public override bool Autoload(ref string name) {
+            return true;
+        }
         public override void SetDefaults() {
             DisplayName.SetDefault("Heavy Caliber");
         }
-        public override void ModifyValue(ref float valueMult) {
-            int a = Type;
-            valueMult = 1.5f;
+        public override float RollChance(Item item) {
+            return 1f;
         }
         public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus) {
-            damageMult += 0.20f;// dmg
+            damageMult += 0.20f;// damage
             critBonus += 10;// crit
-            shootSpeedMult -= 0.15f;// vel
-            knockbackMult += 0.20f;// kb
+            shootSpeedMult -= 0.15f;// velocity
+            knockbackMult += 0.20f;// knockback
         }
     }
 }
---
+++ NPCs/Defiled/Enchanted_Trident.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+
+namespace Origins.NPCs.Defiled {
+    public class Enchanted_Trident : ModNPC {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Profaned Bident");
+            Main.npcFrameCount[npc.type] = 3;
+        }
+        public override void SetDefaults() {
+            npc.CloneDefaults(NPCID.CursedHammer);
+            npc.aiStyle = NPCAIStyleID.Flying_Weapon;
+            npc.lifeMax = 175;
+            npc.defense = 16;
+            npc.damage = 85;
+            npc.width = 40;
+            npc.height = 40;
+            npc.knockBackResist = 0.35f;
+        }
+		public override void AI() {
+			if (npc.ai[0] == 2) {
+                npc.ai[1] += 0.25f;
+			}
+		}
+		public override void HitEffect(int hitDirection, double damage) {
+
+        }
+    }
+}
