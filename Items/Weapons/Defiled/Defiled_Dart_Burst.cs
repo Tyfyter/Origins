b380e30d9cdc1a45155179c449ff7fdabfa3ecc3
Tyfyter <tyfyter2616@gmail.com>
2022-05-24 23:12:27 -0400

    Defiled Mimic with single drop

--- Items/Weapons/Defiled/Defiled_Dart_Burst.cs
+++ Items/Weapons/Defiled/Defiled_Dart_Burst.cs
@@ -5,32 +5,34 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.Items.Weapons.Defiled {
-    public class Defiled_Burst : ModItem {
-        public override void SetStaticDefaults() {
-            DisplayName.SetDefault("The Kruncher");
+    public class Defiled_Dart_Burst : ModItem {
+		public override string Texture => "Origins/Items/Weapons/Defiled/Defiled_Burst";
+		public override void SetStaticDefaults() {
+            DisplayName.SetDefault("The Kruncher but it shoots darts");
             Tooltip.SetDefault("Very pointy");
         }
         public override void SetDefaults() {
-            item.damage = 15;
+            item.damage = 25;
             item.ranged = true;
             item.noMelee = true;
-            item.crit = -1;
+            item.crit = -4;
             item.width = 56;
             item.height = 18;
-            item.useTime = 36;
-            item.useAnimation = 36;
+            item.useTime = 40;
+            item.useAnimation = 40;
             item.useStyle = 5;
             item.knockBack = 5;
             item.shootSpeed = 20f;
-            item.shoot = ProjectileID.Bullet;
-            item.useAmmo = AmmoID.Bullet;
-            item.value = 15000;
+            item.shoot = ProjectileID.PurificationPowder;
+            item.useAmmo = AmmoID.Dart;
+            item.value = 80000;
             item.useTurn = false;
-            item.rare = ItemRarityID.Blue;
+            item.rare = ItemRarityID.Pink;
+            item.autoReuse = true;
             item.UseSound = new LegacySoundStyle(SoundID.Item, Origins.Sounds.Krunch);
         }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
-            for(int i = 0; i<5; i++)Projectile.NewProjectile(position, new Vector2(speedX, speedY).RotatedByRandom(i/10f), type, damage, knockBack, player.whoAmI);
+            for(int i = 0; i<3; i++)Projectile.NewProjectile(position, new Vector2(speedX, speedY).RotatedByRandom(i/10f), type, damage, knockBack, player.whoAmI);
             return false;
         }
 	}
---
+++ NPCs/Defiled/Defiled_Mimic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Origins.Items.Weapons.Defiled;
+
+namespace Origins.NPCs.Defiled {
+    public class Defiled_Mimic : ModNPC {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Defiled Mimic");
+            Main.npcFrameCount[npc.type] = 14;
+        }
+        public override void SetDefaults() {
+            npc.CloneDefaults(NPCID.BigMimicCorruption);
+        }
+        public override void FindFrame(int frameHeight) {
+            npc.CloneFrame(NPCID.BigMimicCorruption, frameHeight);
+        }
+		public override void NPCLoot() {
+			switch (Main.rand.Next(5)) {
+				case 0:
+				Item.NewItem(npc.position, npc.Size, ModContent.ItemType<Defiled_Dart_Burst>(), 1, prefixGiven: -1);
+				break;
+				case 1:
+				Item.NewItem(npc.position, npc.Size, ItemID.ClingerStaff, 1, prefixGiven: -1);
+				break;
+				case 2:
+				Item.NewItem(npc.position, npc.Size, ItemID.ChainGuillotines, 1, prefixGiven: -1);
+				break;
+				case 3:
+				Item.NewItem(npc.position, npc.Size, ItemID.PutridScent, 1, prefixGiven: -1);
+				break;
+				case 4:
+				Item.NewItem(npc.position, npc.Size, ItemID.WormHook, 1, prefixGiven:-1);
+				break;
+			}
+			Item.NewItem(npc.position, npc.Size, ItemID.GreaterHealingPotion, Main.rand.Next(5, 11));
+			Item.NewItem(npc.position, npc.Size, ItemID.GreaterManaPotion, Main.rand.Next(5, 16));
+		}
+		public override void HitEffect(int hitDirection, double damage) {
+            //spawn gore if npc is dead after being hit
+            if(npc.life<0) {
+                for(int i = 0; i < 3; i++)Gore.NewGore(npc.position+new Vector2(Main.rand.Next(npc.width),Main.rand.Next(npc.height)), npc.velocity, mod.GetGoreSlot("Gores/NPCs/DF3_Gore"));
+                for(int i = 0; i < 6; i++)Gore.NewGore(npc.position+new Vector2(Main.rand.Next(npc.width),Main.rand.Next(npc.height)), npc.velocity, mod.GetGoreSlot("Gores/NPCs/DF_Effect_Medium"+Main.rand.Next(1,4)));
+            }
+        }
+    }
+}
