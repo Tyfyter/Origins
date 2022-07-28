aa563bfe5d14ce1fc479f4d0e7810d602e80206a
Tyfyter <tyfyter2616@gmail.com>
2022-07-27 20:23:47 -0600

    acid freak is now less freaky

--- NPCs/TownNPCs/Acid_Freak.cs
+++ NPCs/TownNPCs/Acid_Freak.cs
@@ -13,6 +13,7 @@ using Origins.World.BiomeData;
 using Terraria.GameContent.Bestiary;
 using Origins.Items.Materials;
 using Origins.Projectiles.Weapons;
+using Terraria.GameContent.UI;
 
 namespace Origins.NPCs.TownNPCs {
 	//[AutoloadHead]
@@ -28,6 +29,7 @@ namespace Origins.NPCs.TownNPCs {
 			NPCID.Sets.AttackTime[Type] = 90; // The amount of time it takes for the NPC's attack animation to be over once it starts.
 			NPCID.Sets.AttackAverageChance[Type] = 30;
 			NPCID.Sets.HatOffsetY[Type] = 4; // For when a party is active, the party hat spawns at a Y offset.
+			NPCID.Sets.ActsLikeTownNPC[Type] = true;
 
 			// Influences how the NPC looks in the Bestiary
 			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers(0) {
@@ -37,17 +39,25 @@ namespace Origins.NPCs.TownNPCs {
 
 			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
 
-			// Set Example Person's biome and neighbor preferences with the NPCHappiness hook. You can add happiness text and remarks with localization (See an example in ExampleMod/Localization/en-US.lang).
-			// NOTE: The following code uses chaining - a style that works due to the fact that the SetXAffection methods return the same NPCHappiness instance they're called on.
 			NPC.Happiness
 				//.SetBiomeAffection<>(AffectionLevel.Like)
 				//.SetBiomeAffection<>(AffectionLevel.Dislike)
-				.SetBiomeAffection<Brine_Pool>((AffectionLevel)0) // Example Person likes the Example Surface Biome
+				.SetBiomeAffection<Brine_Pool>((AffectionLevel)0)
 			; // < Mind the semicolon!
 		}
         public override void SetDefaults() {
             NPC.CloneDefaults(NPCID.WitchDoctor);
-			//AnimationType = NPCID.WitchDoctor;
+			AnimationType = NPCID.Merchant;
+		}
+		public override bool PreAI() {
+			NPC.wet = false;
+			NPC.breathCounter = 0;
+			return true;
+		}
+		public override void PostAI() {
+			if (Collision.WetCollision(NPC.position, NPC.width, NPC.height) && !Collision.honey) {
+				NPC.wet = true;
+			}
 		}
 		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
 			// We can use AddRange instead of calling Add multiple times in order to add multiple items at once
@@ -96,14 +106,15 @@ namespace Origins.NPCs.TownNPCs {
 			randExtraCooldown = 30;
 		}
 
-		 public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
-		 	projType = ModContent.ProjectileType<Acid_Shot>();
+		public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
+			projType = ModContent.ProjectileType<Acid_Shot>();
 			attackDelay = 1;
 		}
 
 		public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset) {
 			multiplier = 8f;
 			randomOffset = 2f;
+			gravityCorrection = 30;
 		}
 	}
 }
