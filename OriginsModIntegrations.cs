2b483b0f8472003c44e20a16bbeedd80725e2e2b
Tyfyter <tyfyter2616@gmail.com>
2022-08-05 23:38:23 -0600

    Create OriginsModIntegrations.cs

---
+++ OriginsModIntegrations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Origins {
+	public class OriginsModIntegrations : ILoadable {
+		Mod wikiThis;
+		public void Load(Mod mod) {
+			if (!Main.dedServ && ModLoader.TryGetMod("Wikithis", out wikiThis)) {
+				wikiThis.Call("AddModURL", Origins.instance, "tyfyter.github.io/OriginsWiki");
+				Origins.instance.Logger.Info("Added Wikithis integration");
+			}
+		}
+
+		public void Unload() {
+		}
+	}
+}
