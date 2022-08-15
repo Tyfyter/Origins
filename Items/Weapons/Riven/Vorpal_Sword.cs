13c4072d5bf824fa8c595d25a226e1d16ad29403
Tyfyter <tyfyter2616@gmail.com>
2022-08-14 18:01:14 -0600

    swingy vorpal sword

--- Items/Weapons/Riven/Vorpal_Sword.cs
+++ Items/Weapons/Riven/Vorpal_Sword.cs
@@ -1,11 +1,18 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Tyfyter.Utils;
 
 namespace Origins.Items.Weapons.Riven {
     public class Vorpal_Sword : ModItem {
-		static short glowmask;
 		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Vorpal Sword");
 			Tooltip.SetDefault("You hear whispers nearby...");
@@ -15,10 +22,14 @@ namespace Origins.Items.Weapons.Riven {
 		public override void SetDefaults() {
 			Item.damage = 17;
 			Item.DamageType = DamageClass.Melee;
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
 			Item.width = 42;
 			Item.height = 50;
-			Item.useTime = 20;
-			Item.useAnimation = 20;
+			Item.useTime = 17;
+			Item.useAnimation = 34;
+			Item.shoot = ModContent.ProjectileType<Vorpal_Sword_Slash>();
+			Item.shootSpeed = 12;
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.knockBack = 0f;
 			Item.value = 5000;
@@ -26,6 +37,7 @@ namespace Origins.Items.Weapons.Riven {
 			Item.rare = ItemRarityID.Blue;
 			Item.UseSound = SoundID.Item1;
 			Item.ArmorPenetration = 9999;
+			Item.UseSound = null;
 			//Item.glowMask = glowmask;
 		}
 		static int textIndex = -1;
@@ -143,6 +155,87 @@ namespace Origins.Items.Weapons.Riven {
 				line.Y += Main.rand.Next(-2, 3);
 			}
 			return true;
+		}
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			if (!player.controlUseItem) {
+				player.itemAnimation = 0;
+				player.itemTime = 0;
+				return false;
+			}
+			SoundEngine.PlaySound(SoundID.Item1, position);
+			Projectile.NewProjectile(source, position, velocity, type, damage, knockback * 0.25f, player.whoAmI, ai1: player.itemAnimation == player.itemTime ? -1 : 1);
+			return false;
+		}
+	}
+	public class Vorpal_Sword_Slash : ModProjectile {
+		public override string Texture => "Origins/Items/Weapons/Riven/Vorpal_Sword";
+		public override void SetDefaults() {
+			Projectile.CloneDefaults(ProjectileID.PiercingStarlight);
+			Projectile.aiStyle = 0;
+			Projectile.extraUpdates = 0;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = 600;
+		}
+		public override void OnSpawn(IEntitySource source) {
+			if (source is EntitySource_ItemUse itemUse) {
+				if (itemUse.Entity is Player player) {
+					Projectile.ai[1] *= player.direction;
+					Projectile.scale *= player.GetAdjustedItemScale(itemUse.Item);
+				} else {
+					Projectile.scale *= itemUse.Item.scale;
+				}
+			}
+		}
+		public override void AI() {
+			Player player = Main.player[Projectile.owner];
+			player.immune = true;
+			player.immuneAlpha = 0;
+			player.immuneTime = 15;
+			float swingFactor = 1 - player.itemTime / (float)player.itemTimeMax;
+			Projectile.rotation = MathHelper.Lerp(-2.75f, 2f, swingFactor) * Projectile.ai[1];
+			/*player.velocity = Vector2.Lerp(
+				Vector2.Lerp(player.velocity, Vector2.Zero, swingFactor),
+				Projectile.velocity * 3f,
+				MathHelper.Lerp(swingFactor, 0, swingFactor)
+			);*/
+			float realRotation = Projectile.rotation + Projectile.velocity.ToRotation();
+			Projectile.Center = player.MountedCenter - Projectile.velocity + (Vector2)new PolarVec2(32, realRotation);
+			Projectile.timeLeft = player.itemTime * Projectile.MaxUpdates;
+			player.heldProj = Projectile.whoAmI;
+			player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, realRotation - MathHelper.PiOver2);
+		}
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
+			//Vector2 vel = Projectile.velocity.SafeNormalize(Vector2.Zero) * Projectile.width * 0.95f;
+			Vector2 vel = (Projectile.velocity.RotatedBy(Projectile.rotation) / 12f) * Projectile.width * 0.95f;
+			for (int j = 0; j <= 1; j++) {
+				Rectangle hitbox = projHitbox;
+				Vector2 offset = vel * j;
+				hitbox.Offset((int)offset.X, (int)offset.Y);
+				if (hitbox.Intersects(targetHitbox)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		public override void CutTiles() {
+			DelegateMethods.tilecut_0 = TileCuttingContext.AttackProjectile;
+			Vector2 end = Projectile.Center + Projectile.velocity.RotatedBy(Projectile.rotation).SafeNormalize(Vector2.UnitX) * 50f * Projectile.scale;
+			Utils.PlotTileLine(Projectile.Center, end, 80f * Projectile.scale, DelegateMethods.CutTiles);
+		}
+
+		public override bool PreDraw(ref Color lightColor) {
+			Main.EntitySpriteDraw(
+				TextureAssets.Projectile[Type].Value,
+				Projectile.Center - Main.screenPosition,
+				null,
+				lightColor,
+				Projectile.rotation + Projectile.velocity.ToRotation() + (MathHelper.PiOver4 * Projectile.ai[1]),
+				new Vector2(14, 25 + 11 * Projectile.ai[1]),// origin point in the sprite, 'round which the whole sword rotates
+				Projectile.scale,
+				Projectile.ai[1] > 0 ? SpriteEffects.None : SpriteEffects.FlipVertically,
+				0
+			);
+			return false;
 		}
 	}
 }
