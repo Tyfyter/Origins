7e72f3c94417e61eb5c625912ab4ee5ce42bd230
Tyfyter <tyfyter2616@gmail.com>
2022-12-26 21:31:50 -0700

    rainbow pangolin and sleetfire
    sleetfire uses temp texture since the flamethrower projectile texture isn't available yet

---
+++ Items/Pets/Chromatic_Scale.cs
@@ -0,0 +1,269 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Origins.Items.Pets;
+using System;
+using System.IO;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.Drawing;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Items.Pets {
+    public class Chromatic_Scale : ModItem {
+        internal static int projectileID = 0;
+        internal static int buffID = 0;
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Strange Power Up");
+            Tooltip.SetDefault("The very first sprite.");
+
+            SacrificeTotal = 1;
+        }
+        public override void SetDefaults() {
+            Item.DefaultToVanitypet(projectileID, buffID);
+            Item.width = 32;
+            Item.height = 32;
+            Item.value = Item.buyPrice(0, 30, 0, 0);
+            Item.rare = ItemRarityID.Cyan;// dev items are cyan rarity, despite being expert exclusive
+            Item.buffType = buffID;
+            Item.shoot = projectileID;
+            Item.UseSound = Origins.Sounds.PowerUp;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+            player.AddBuff(Item.buffType, 2); // The item applies the buff, the buff spawns the projectile
+
+            return false;
+        }
+    }
+    public class Chromatic_Pangolin : ModProjectile {
+        public bool OnGround {
+            get {
+                return Projectile.localAI[1] > 0;
+            }
+            set {
+                Projectile.localAI[1] = value ? 2 : 0;
+            }
+        }
+        public sbyte CollidingX {
+            get {
+                return (sbyte)Projectile.localAI[0];
+            }
+            set {
+                Projectile.localAI[0] = value;
+            }
+        }
+
+        public override void SetStaticDefaults() {
+            Chromatic_Scale.projectileID = Type;
+			DisplayName.SetDefault("Chromatic Pangolin");
+            //Origins.ExplosiveProjectiles[Projectile.type] = true;
+			// Sets the amount of frames this minion has on its spritesheet
+			Main.projFrames[Type] = 4;
+
+			// These below are needed for a minion
+			// Denotes that this projectile is a pet or minion
+			Main.projPet[Type] = true;
+		}
+
+		public sealed override void SetDefaults() {
+            Projectile.width = 50;
+			Projectile.height = 26;
+			Projectile.tileCollide = true;
+			Projectile.friendly = false;
+			Projectile.minionSlots = 0f;
+			Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 1;
+            Projectile.ignoreWater = false;
+            DrawOriginOffsetY = 2;
+            //Projectile.scale = 1.5f;
+		}
+
+		// Here you can decide if your minion breaks things like grass or pots
+		public override bool? CanCutTiles() {
+			return false;
+		}
+
+		public override void AI() {
+			Player player = Main.player[Projectile.owner];
+
+			#region Active check
+			// This is the "active check", makes sure the minion is alive while the player is alive, and despawns if not
+			if (player.dead || !player.active) {
+				player.ClearBuff(Chromatic_Scale.buffID);
+			}
+			if (player.HasBuff(Chromatic_Scale.buffID)) {
+				Projectile.timeLeft = 2;
+			}
+			#endregion
+
+			#region General behavior
+			Vector2 idlePosition = player.Bottom;
+            idlePosition.X -= 48f * player.direction;
+            idlePosition.Y -= 12f * Projectile.scale;
+
+			// Teleport to player if distance is too big
+			Vector2 vectorToIdlePosition = (idlePosition + new Vector2(6*player.direction, 0)) - Projectile.Center;
+			float distanceToIdlePosition = vectorToIdlePosition.Length();
+			if (Projectile.soundDelay == 1) {
+                SoundEngine.PlaySound(SoundID.Item29.WithPitch(1f), Projectile.Center);
+                Projectile.soundDelay = 0;
+			}
+            if (distanceToIdlePosition > 500f) {
+				if (Main.myPlayer == player.whoAmI) {
+                    ParticleOrchestrator.RequestParticleSpawn(
+                        false,
+                        ParticleOrchestraType.RainbowRodHit,
+                        new ParticleOrchestraSettings() {
+                            PositionInWorld = Projectile.Center
+                    });
+                    // Whenever you deal with non-regular events that change the behavior or position drastically, make sure to only run the code on the owner of the projectile,
+                    // and then set netUpdate to true
+                    Projectile.position = idlePosition;
+                    Projectile.velocity *= 0.1f;
+                    Projectile.netUpdate = true;
+                    Projectile.soundDelay = 2;
+                    ParticleOrchestrator.RequestParticleSpawn(
+                        false,
+                        ParticleOrchestraType.RainbowRodHit,
+                        new ParticleOrchestraSettings() {
+                            PositionInWorld = Projectile.Center
+                    });
+                }
+			}
+
+            // If your minion is flying, you want to do this independently of any conditions
+            float overlapVelocity = 0.04f;
+			for (int i = 0; i < Main.maxProjectiles; i++) {
+				// Fix overlap with other minions
+				Projectile other = Main.projectile[i];
+				if (i != Projectile.whoAmI && other.active && other.owner == Projectile.owner && Math.Abs(Projectile.position.X - other.position.X) + Math.Abs(Projectile.position.Y - other.position.Y) < Projectile.width) {
+					if (Projectile.position.X < other.position.X) Projectile.velocity.X -= overlapVelocity;
+					else Projectile.velocity.X += overlapVelocity;
+
+					if (Projectile.position.Y < other.position.Y) Projectile.velocity.Y -= overlapVelocity;
+					else Projectile.velocity.Y += overlapVelocity;
+				}
+			}
+			#endregion
+
+			#region Movement
+
+            float speed = 6f;
+            float inertia = 12f;
+			if (distanceToIdlePosition > 250f) {
+                speed = 10f;
+			}
+            int direction = Math.Sign(vectorToIdlePosition.X);
+            Projectile.spriteDirection = direction;
+            if(vectorToIdlePosition.Y < 160 && CollidingX == direction && OnGround) {
+                float jumpStrength = 6;
+                if(Collision.TileCollision(Projectile.position - new Vector2(0, 18), new Vector2(4 * direction, 0), Projectile.width, Projectile.height, false, false).X == 0) {
+                    jumpStrength+=2;
+                    if(Collision.TileCollision(Projectile.position - new Vector2(0, 36), new Vector2(4 * direction, 0), Projectile.width, Projectile.height, false, false).X == 0) {
+                        jumpStrength+=2;
+                    }
+                }
+                Projectile.velocity.Y = -jumpStrength;
+            }
+			if (distanceToIdlePosition > 6f) {
+				// The immediate range around the player (when it passively floats about)
+
+				// This is a simple movement formula using the two parameters and its desired direction to create a "homing" movement
+				vectorToIdlePosition.Normalize();
+				vectorToIdlePosition *= speed;
+				Projectile.velocity.X = (Projectile.velocity.X * (inertia - 1) + vectorToIdlePosition.X) / inertia;
+            } else {
+                inertia = 6f;
+                Projectile.velocity.X = (Projectile.velocity.X * (inertia - 1)) / inertia;
+            }
+            #endregion
+
+            //gravity
+            Projectile.velocity.Y += 0.4f;
+
+            #region Animation and visuals
+            if (Projectile.velocity.LengthSquared() < 1) {
+                Projectile.spriteDirection = Projectile.direction = player.direction;
+            }
+            if (OnGround) {
+                Projectile.localAI[1]--;
+                const int frameSpeed = 4;
+                if(Math.Abs(Projectile.velocity.X) < 0.05f) {
+                    Projectile.velocity.X = 0f;
+                }
+                if((Projectile.velocity.X!=0) ^ (Projectile.oldVelocity.X!=0)) {
+                    Projectile.frameCounter = 0;
+                    Projectile.frame = 0;
+                }
+                if(Projectile.velocity.X!=0) {
+                    Projectile.frameCounter++;
+                    if(Projectile.frameCounter >= frameSpeed) {
+                        Projectile.frameCounter = 0;
+                        Projectile.frame++;
+                        if(Projectile.frame >= 4) {
+                            Projectile.frame = 0;
+                        }
+                    }
+                }
+            }else {
+                Projectile.frame = 2;
+            }
+			#endregion
+		}
+		public override void SendExtraAI(BinaryWriter writer) {
+            writer.Write((byte)Projectile.soundDelay);
+		}
+		public override void ReceiveExtraAI(BinaryReader reader) {
+            Projectile.soundDelay = reader.ReadByte();
+		}
+		public override bool PreKill(int timeLeft) {
+            return true;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity) {
+            if(oldVelocity.Y > Projectile.velocity.Y) {
+                OnGround = true;
+            } else {
+                if(Collision.SlopeCollision(Projectile.position, new Vector2(0, 4), Projectile.width, Projectile.height).Y!=4) {
+                    OnGround = true;
+                }
+            }
+            if(oldVelocity.X > Projectile.velocity.X) {
+                CollidingX = (sbyte)(1-Collision.TileCollision(Projectile.position, Vector2.UnitX, Projectile.width, Projectile.height, false, false).X);
+            }else if(oldVelocity.X < Projectile.velocity.X) {
+                CollidingX = (sbyte)(-1-Collision.TileCollision(Projectile.position, -Vector2.UnitX, Projectile.width, Projectile.height, false, false).X);
+            }else {
+                CollidingX = 0;
+            }
+            return true;
+        }
+    }
+}
+namespace Origins.Buffs {
+    public class Chromatic_Pangolin_Buff : ModBuff {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Chromatic Pangolin");
+            Description.SetDefault("It rainbow, tooltip coming soon.");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+            Chromatic_Scale.buffID = Type;
+        }
+
+        public override void Update(Player player, ref int buffIndex) { // This method gets called every frame your buff is active on your player.
+            player.buffTime[buffIndex] = 18000;
+
+            int projType = Chromatic_Scale.projectileID;
+
+            // If the player is local, and there hasn't been a pet projectile spawned yet - spawn it.
+            if (player.whoAmI == Main.myPlayer && player.ownedProjectileCounts[projType] <= 0) {
+                var entitySource = player.GetSource_Buff(buffIndex);
+
+                Projectile.NewProjectile(entitySource, player.Center, Vector2.Zero, projType, 0, 0f, player.whoAmI);
+            }
+        }
+    }
+}
---
+++ Items/Weapons/Other/Sleetfire.cs
@@ -0,0 +1,71 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Origins.Projectiles.Weapons;
+using Terraria.GameContent.Creative;
+
+namespace Origins.Items.Weapons.Other {
+    public class Sleetfire : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Sleetfire");
+            Tooltip.SetDefault("Uses gel as ammo");
+            ItemID.Sets.SkipsInitialUseSound[Item.type] = true;
+            SacrificeTotal = 1;
+        }
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.EldMelter);
+            Item.damage = 12;
+            Item.useAnimation = 20;
+            Item.useTime = 4;
+            Item.width = 36;
+            Item.height = 16;
+            Item.useAmmo = ItemID.Gel;
+            Item.shoot = ModContent.ProjectileType<Sleetfire_P>();
+            Item.shootSpeed = 7f;
+            Item.rare = ItemRarityID.Orange;
+            Item.reuseDelay = 9;
+            Item.ArmorPenetration = 5;
+        }
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+            position += velocity.SafeNormalize(default) * 36;
+		}
+	}
+    public class Sleetfire_P : ModProjectile {
+        //public override string Texture => "Terraria/Images/Projectile_85";
+        public override string Texture => "Terraria/Images/Projectile_465";//temp texture
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Sleetfire");
+            //Main.projFrames[Type] = 7;
+            Main.projFrames[Type] = 4;//temp value for temp texture
+        }
+        public override void SetDefaults() {
+            Projectile.width = Projectile.height = 6;
+            Projectile.penetrate = 2;
+            Projectile.friendly = true;
+            Projectile.alpha = 255;
+            Projectile.extraUpdates = 1;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            DrawOriginOffsetX = -45;
+            DrawOriginOffsetY = -45;
+        }
+		public override void AI() {
+            Projectile.ai[0]++;
+            Projectile.scale = Utils.Remap(Projectile.ai[0], 0f, 45f, 16f / 96f, 66f / 96f);
+            Projectile.alpha = (int)(255 * (1 - (Projectile.ai[0] / 45f)));
+			if (Projectile.ai[0] > 45) {
+                Projectile.Kill();
+			}
+		}
+		public override void ModifyDamageHitbox(ref Rectangle hitbox) {
+            int scale = (int)Utils.Remap(Projectile.ai[0], 0f, 45f, 10f, 60f);
+            hitbox.Inflate(scale, scale);
+		}
+		public override Color? GetAlpha(Color lightColor) {
+			return new Color(0, 200, 255, 255) * (1 - Projectile.alpha / 255f);
+		}
+	}
+}
