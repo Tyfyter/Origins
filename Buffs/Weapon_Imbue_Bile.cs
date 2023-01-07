0d9dfddefdccb4db4c717a65571581bc985f82d6
Tyfyter <tyfyter2616@gmail.com>
2023-01-06 21:30:50 -0700

    flask name fixes

--- Buffs/Weapon_Imbue_Bile.cs
+++ Buffs/Weapon_Imbue_Bile.cs
@@ -1,15 +1,18 @@
 ﻿using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.Buffs {
     public class Weapon_Imbue_Bile : ModBuff {
         public static int ID { get; private set; } = -1;
         public override void SetStaticDefaults() {
-            DisplayName.SetDefault("Weapon Imbue Bile");
+            DisplayName.SetDefault("Weapon Imbue: Bile");
+            BuffID.Sets.IsAFlaskBuff[Type] = true;
+            Main.meleeBuff[Type] = true;
             ID = Type;
         }
         public override void Update(Player player, ref int buffIndex) {
-            player.GetModPlayer<OriginPlayer>().bileFlask = true;
+            player.GetModPlayer<OriginPlayer>().flaskBile = true;
         }
     }
 }
--- Buffs/Weapon_Imbue_Salt.cs
+++ Buffs/Weapon_Imbue_Salt.cs
@@ -1,10 +1,13 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace Origins.Buffs {
     public class Weapon_Imbue_Salt : ModBuff {
 		public static int ID { get; private set; } = -1;
         public override void SetStaticDefaults() {
-            DisplayName.SetDefault("Weapon Imbue Alkahest");
+            DisplayName.SetDefault("Weapon Imbue: Alkahest");
+            BuffID.Sets.IsAFlaskBuff[Type] = true;
+            Main.meleeBuff[Type] = true;
             ID = Type;
         }
     }
