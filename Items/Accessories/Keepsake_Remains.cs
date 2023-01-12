3e775a454aa9fc1c28a5363e5f248f8fa490cf6c
Chee <66661847+ItsChee@users.noreply.github.com>
2023-01-12 14:15:07 -0700

    Accessory Combo Plans Pt 1

---
+++ Items/Accessories/Keepsake_Remains.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Items.Accessories {
+    public class Keepsake_Remains : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Keepsake Remains");
+            Tooltip.SetDefault("Increases armor penetration by 5 and attacks tenderize targets");
+            SacrificeTotal = 1;
+        }
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.Aglet);
+            Item.value = Item.sellPrice(gold: 3);
+            Item.rare = ItemRarityID.Green;
+        }
+        public override void UpdateEquip(Player player) {
+            player.GetModPlayer<OriginPlayer>().symbioteSkull = true;
+            player.GetArmorPenetration(DamageClass.Generic) += 5;
+        }
+    }
+}
