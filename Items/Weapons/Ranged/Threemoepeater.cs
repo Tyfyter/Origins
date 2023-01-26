0a1d0ab1c3a6e4be0acf4456fb7b1adb880973a2
Tyfyter <tyfyter2616@gmail.com>
2023-01-25 17:03:40 -0700

    Threemoepeater & updated completionList

--- Items/Weapons/Ranged/Threemoepeater.cs
+++ Items/Weapons/Ranged/Threemoepeater.cs
@@ -1,5 +1,8 @@
-﻿using Origins.Items.Materials;
+﻿using Microsoft.Xna.Framework;
+using Origins.Items.Materials;
+using System;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,12 +13,13 @@ namespace Origins.Items.Weapons.Ranged {
             SacrificeTotal = 1;
         }
         public override void SetDefaults() {
-            Item.CloneDefaults(ItemID.ChlorophyteShotbow);
+            Item.CloneDefaults(ItemID.HallowedRepeater);
             Item.damage = 14;
             Item.DamageType = DamageClass.Ranged;
             Item.knockBack = 5;
             Item.noMelee = true;
-            Item.useTime = 13;
+			Item.useTime = 13;
+			Item.useAnimation = 13;
             Item.width = 50;
             Item.height = 10;
             Item.UseSound = SoundID.Item11;
@@ -29,5 +33,36 @@ namespace Origins.Items.Weapons.Ranged {
             recipe.AddTile(TileID.Anvils);
             recipe.Register();
         }
-    }
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			for (int i = 0; i < 2; i++) {
+				Projectile.NewProjectile(
+					source,
+					position,
+					velocity.RotatedByRandom(0.2f) * Main.rand.NextFloat(0.9f, 1.1f),
+					Threemoepeater_P.ID,
+					damage,
+					knockback,
+					player.whoAmI
+				);
+			}
+			return true;
+		}
+	}
+	public class Threemoepeater_P : ModProjectile {
+		public override string Texture => "Origins/Projectiles/Weapons/Amoeba_Ball";
+		public static int ID { get; private set; }
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Threemoepeater");
+			ID = Type;
+		}
+		public override void SetDefaults() {
+			Projectile.CloneDefaults(ProjectileID.SpikyBall);
+			Projectile.timeLeft = 180;
+		}
+		public override Color? GetAlpha(Color lightColor) {
+			const float add = 510;
+			const float max = add + 255;
+			return new Color((lightColor.R + add) / max, (lightColor.G + add) / max, (lightColor.B + add) / max, 0.5f) * Math.Min(Projectile.timeLeft / 20f, 1);
+		}
+	}
 }
