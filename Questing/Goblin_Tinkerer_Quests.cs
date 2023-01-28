afae581f9be155a98a1437badd656cf79285479d
Tyfyter <tyfyter2616@gmail.com>
2023-01-28 14:08:08 -0700

    more quest

--- Questing/Goblin_Tinkerer_Quests.cs
+++ Questing/Goblin_Tinkerer_Quests.cs
@@ -48,6 +48,12 @@ namespace Origins.Questing {
 						}
 					};
 					break;
+					case 2:
+					ironBars = ironBarTarget;
+					chains = chainTarget;
+					adhesiveWraps = adhesiveWrapTarget;
+					hasWatch = true;
+					break;
 				}
 			}
 		}
@@ -180,6 +186,11 @@ namespace Origins.Questing {
 						}
 					};
 					break;
+					case 2:
+					leather = leatherTarget;
+					hasPistol = true;
+					hasArm = true;
+					break;
 				}
 			}
 		}
@@ -253,6 +264,148 @@ namespace Origins.Questing {
 		}
 		public override void SetStaticDefaults() {
 			NameKey = "Mods.Origins.Quests.Goblin_Tinkerer.Gun_Glove.Name";
+		}
+		public override void SaveData(TagCompound tag) {
+			//save stage and kills
+			tag.Add("Stage", Stage);
+		}
+		public override void LoadData(TagCompound tag) {
+			//load stage and kills, note that it uses the Stage property so that it sets the event handlers
+			//SafeGet returns the default value (0 for ints) if the tag doesn't have the data
+			Stage = tag.SafeGet<int>("Stage");
+		}
+	}
+	public class Rocket_Boosted_Minecart_Quest : Quest {
+		//backing field for Stage property
+		int stage = 0;
+
+		bool hasMinecart = false;
+
+		int rockets = 0;
+		const int rocketTarget = 50;
+
+		int adhesiveWraps = 0;
+		const int adhesiveWrapTarget = 80;
+
+		int adamantiteBars = 0;
+		const int adamantiteBarTarget = 15;
+		//Stage property so changing quest stage also updates its event handlers
+		public override int Stage {
+			get => stage;
+			set {
+				stage = value;
+				// clear kill event handlers so they only run if the quest is active
+				PreUpdateInventoryEvent = null;
+				UpdateInventoryEvent = null;
+				switch (stage) {
+					case 1:
+					PreUpdateInventoryEvent = () => {
+						rockets = 0;
+						adamantiteBars = 0;
+						adhesiveWraps = 0;
+						hasMinecart = false;
+					};
+					UpdateInventoryEvent = (item) => {
+						if (item.type == ItemID.Minecart) {
+							hasMinecart = true;
+						} else if (item.type == ItemID.RocketI) {
+							rockets += item.stack;
+						} else if (item.type == ModContent.ItemType<Adhesive_Wrap>()) {
+							adhesiveWraps += item.stack;
+						} else if (item.type == ItemID.AdamantiteBar || item.type == ItemID.TitaniumBar) {
+							adamantiteBars += item.stack;
+						}
+					};
+					break;
+					case 2:
+					rockets = rocketTarget;
+					adamantiteBars = adamantiteBarTarget;
+					adhesiveWraps = adhesiveWrapTarget;
+					hasMinecart = true;
+					break;
+				}
+			}
+		}
+		public override bool Started => Stage > 0;
+		public override bool Completed => Stage > 1;
+		public override bool HasStartDialogue(NPC npc) {
+			return npc.type == NPCID.GoblinTinkerer && Stage == 0 && Main.hardMode && ModContent.GetInstance<Gun_Glove_Quest>().Completed;
+		}
+		public override bool HasDialogue(NPC npc) {
+			if (npc.type != NPCID.GoblinTinkerer) return false; // NPCs other than the merchant won't have any dialogue related to this quest
+			switch (Stage) {
+				case 1:
+				return
+					rockets >= rocketTarget &&
+					adamantiteBars >= adamantiteBarTarget &&
+					adhesiveWraps >= adhesiveWrapTarget &&
+					hasMinecart;
+			}
+			return false;
+		}
+		public override string GetDialogue() {
+			switch (Stage) {
+				case 1:
+				return "Complete Quest";
+
+				default:
+				if (Origins.npcChatQuestSelected) {
+					return "Accept";
+				}
+				return Language.GetTextValue(NameKey);
+			}
+		}
+		public override void OnDialogue() {
+			switch (stage) {
+				case 0: {
+					if (Origins.npcChatQuestSelected) {
+						Stage = 1;
+					} else {
+						Main.npcChatText = Language.GetTextValue(
+							"Mods.Origins.Quests.Goblin_Tinkerer.Rocket_Boosted_Minecart.Start",
+							NPC.GetFirstNPCNameOrNull(NPCID.Mechanic) ?? Language.GetTextValue("Mods.Origins.Quests.Goblin_Tinkerer.Generic.Mechanic")
+						);
+						Origins.npcChatQuestSelected = true;// (npcChatQuestSelected is reset to false when the player closes the dialogue box)
+					}
+					break;
+				}
+				case 1: {
+					Item[] inventory = Main.LocalPlayer.inventory;
+					ConsumeItems(
+						inventory,
+						((i) => i.type == ItemID.Minecart, 1),
+						((i) => i.type == ItemID.RocketI, rocketTarget),
+						((i) => i.type == ModContent.ItemType<Adhesive_Wrap>(), adhesiveWrapTarget),
+						((i) => i.type == ItemID.AdamantiteBar || i.type == ItemID.TitaniumBar, adamantiteBarTarget)
+					);
+					Main.npcChatText = Language.GetTextValue("Mods.Origins.Quests.Goblin_Tinkerer.Rocket_Boosted_Minecart.Complete");
+					Stage = 2;
+					break;
+				}
+			}
+		}
+		public override string GetJournalPage() {
+			return Language.GetTextValue(
+				"Mods.Origins.Quests.Goblin_Tinkerer.Rocket_Boosted_Minecart.Journal", //translation key
+				NPC.GetFirstNPCNameOrNull(NPCID.Mechanic) ?? Language.GetTextValue("Mods.Origins.Quests.Goblin_Tinkerer.Generic.Mechanic"),
+
+				StageTagOption(hasMinecart),
+
+				rockets,
+				rocketTarget,
+				StageTagOption(rockets >= rocketTarget),
+
+				adhesiveWraps,
+				adhesiveWrapTarget,
+				StageTagOption(adhesiveWraps >= adhesiveWrapTarget),
+
+				adamantiteBars,
+				adamantiteBarTarget,
+				StageTagOption(adamantiteBars >= adamantiteBarTarget)
+			);
+		}
+		public override void SetStaticDefaults() {
+			NameKey = "Mods.Origins.Quests.Goblin_Tinkerer.Rocket_Boosted_Minecart.Name";
 		}
 		public override void SaveData(TagCompound tag) {
 			//save stage and kills
