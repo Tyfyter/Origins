0cea9f45c4d6d1d35aa47fda355524fc1000d69a
Chee <66661847+ItsChee@users.noreply.github.com>
2023-03-02 11:06:00 -0700

    Balanced Boat Rocker and Others
    Boat Rocker to be an equivalent of the Acrid Impaler, but slightly stronger of course

---
+++ Items/Accessories/Dryads_Inheritance.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Items.Accessories {
+	[AutoloadEquip(EquipType.Neck)]
+	public class Dryads_Inheritance : ModItem {
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Dryad's Inheritance");
+			Tooltip.SetDefault("Increases damage, movement speed, and length of invincibility after taking damage\nEmit a damaging aura that inflicts poison");
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults() {
+			Item.DefaultToAccessory(26, 26);
+			Item.neckSlot = ArmorIDs.Neck.StarVeil;
+			Item.value = Item.sellPrice(gold: 8);
+			Item.rare = ItemRarityID.Lime;
+			Item.accessory = true;
+		}
+		public override void AddRecipes() {
+			Recipe recipe = Recipe.Create(Type);
+			recipe.AddIngredient(ItemID.SporeSac);
+			recipe.AddIngredient(ModContent.ItemType<Last_Descendent>());
+			recipe.AddTile(TileID.TinkerersWorkbench);
+			recipe.Register();
+		}
+		public override void UpdateEquip(Player player) {
+			player.longInvince = true;
+			player.GetModPlayer<OriginPlayer>().guardedHeart = true;
+			//player.GetModPlayer<OriginPlayer>().dryadNecklace = true;
+		}
+	}
+}
--- Items/Weapons/Ranged/Boat_Rocker.cs
+++ Items/Weapons/Ranged/Boat_Rocker.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Origins.Items.Weapons.Ammo;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.Items.Weapons.Ranged {
-	public class Boat_Rocker : Harpoon_Gun {
+    public class Boat_Rocker : Harpoon_Gun {
 		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Boat Rocker");
-			Tooltip.SetDefault("Uses harpoons as ammo\n87.5% chance not to consume ammo");
+			Tooltip.SetDefault("Uses harpoons as ammo\nRight-click while a line is cast to whip it around\n75% chance not to consume ammo");
 			SacrificeTotal = 1;
 		}
 		public override void SetDefaults() {
-			Item.damage = 24;
+			Item.damage = 30;
 			Item.DamageType = DamageClass.Ranged;
 			Item.knockBack = 4;
 			Item.useStyle = ItemUseStyleID.Shoot;
@@ -21,19 +20,19 @@ namespace Origins.Items.Weapons.Ranged {
 			Item.useAnimation = 3;
 			Item.useTime = 3;
 			Item.reuseDelay = 2;
-			Item.width = 56;
-			Item.height = 26;
+			Item.width = 48;
+			Item.height = 22;
 			Item.useAmmo = Harpoon.ID;
 			Item.shoot = Harpoon_P.ID;
-			Item.shootSpeed = 18.75f;
+			Item.shootSpeed = 14f;
 			Item.UseSound = SoundID.Item11;
-			Item.value = Item.buyPrice(gold: 1);
-			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.buyPrice(silver: 58);
+			Item.rare = ItemRarityID.LightRed;
 			Item.autoReuse = true;
 		}
 		public override Vector2? HoldoutOffset() => new Vector2(-8, 0);
 		public override void OnConsumeAmmo(Item ammo, Player player) {
-			if (!Main.rand.NextBool(8)) {
+			if (!Main.rand.NextBool(4)) {
 				ammo.stack++;
 			} else {
 				consume = true;
