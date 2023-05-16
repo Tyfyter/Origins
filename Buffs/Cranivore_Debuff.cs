315fb4c7bbbe30e09f2f48d10f6e9db814f490cf
Tyfyter <tyfyter2616@gmail.com>
2023-05-15 20:52:31 -0600

    cranivore

---
+++ Buffs/Cranivore_Debuff.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Buffs {
+	public class Cranivore_Debuff : ModBuff {
+		public override string Texture => "Terraria/Images/Buff_" + BuffID.Darkness;
+		public static int ID { get; private set; } = -1;
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Cranivore");
+			Main.debuff[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+			ID = Type;
+		}
+		public override void Update(Player player, ref int buffIndex) {
+			player.blind = true;
+			player.lifeRegen -= 16;
+		}
+	}
+}
--- NPCs/MiscE/Cranivore.cs
+++ NPCs/MiscE/Cranivore.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
 using Terraria.Audio;
@@ -10,9 +11,16 @@ using Terraria.ModLoader;
 
 namespace Origins.NPCs.MiscE {
     public class Cranivore : ModNPC {
+		public static new AutoCastingAsset<Texture2D> HeadTexture { get; private set; }
 		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Cranivore");
 			Main.npcFrameCount[Type] = 2;
+			if (!Main.dedServ) {
+				HeadTexture = Mod.Assets.Request<Texture2D>("NPCs/MiscE/Cranivore_Head");
+			}
+		}
+		public override void Unload() {
+			HeadTexture = null;
 		}
 		public override void SetDefaults() {
 			NPC.CloneDefaults(NPCID.DemonEye);
@@ -21,8 +29,10 @@ namespace Origins.NPCs.MiscE {
 			NPC.defense = 5;
 			NPC.damage = 16;
 			NPC.width = 32;
-			NPC.height = 18;
+			NPC.height = 32;
 			NPC.friendly = false;
+			NPC.noGravity = true;
+			NPC.aiStyle = NPCAIStyleID.Demon_Eye;
 		}
 		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
 			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
@@ -33,12 +43,53 @@ namespace Origins.NPCs.MiscE {
 			npcLoot.Add(ItemDropRule.Common(ItemID.RottenChunk, 3));
 		}
 		public override void AI() {
-			NPCAimedTarget target = NPC.GetTargetData();
-			NPC.rotation = NPC.AngleTo(target.Center) + MathHelper.PiOver2;
-			if (Main.rand.NextBool(900)) SoundEngine.PlaySound(Origins.Sounds.DefiledIdle.WithPitchRange(1f, 1.2f), NPC.Center);
-			NPC.FaceTarget();
-			if (!NPC.HasValidTarget) NPC.direction = Math.Sign(NPC.velocity.X);
-			NPC.spriteDirection = NPC.direction;
+			if (NPC.aiStyle == NPCAIStyleID.Star_Cell) {
+				NPC.hide = true;
+				if (NPC.ai[0] == 0f) {
+					NPC.aiStyle = NPCAIStyleID.Demon_Eye;
+				}else if (NPC.ai[0] == 1f) {
+					Player targetPlayer = Main.player[NPC.target];
+					NPC.Top = targetPlayer.Top;
+					targetPlayer.AddBuff(Buffs.Cranivore_Debuff.ID, 5);
+				}
+			} else {
+				NPCAimedTarget target = NPC.GetTargetData();
+				NPC.rotation = NPC.AngleTo(target.Center) + MathHelper.PiOver2;
+				NPC.FaceTarget();
+				if (!NPC.HasValidTarget) NPC.direction = Math.Sign(NPC.velocity.X);
+				NPC.spriteDirection = NPC.direction;
+				NPC.hide = false;
+			}
+		}
+		public override bool? CanHitNPC(NPC target) {
+			return NPC.aiStyle != NPCAIStyleID.Star_Cell;
+		}
+		public override bool CanHitPlayer(Player target, ref int cooldownSlot) {
+			return NPC.aiStyle != NPCAIStyleID.Star_Cell;
+		}
+		public override void OnHitPlayer(Player target, int damage, bool crit) {
+			NPC.aiStyle = NPCAIStyleID.Star_Cell;
+			NPC.ai[0] = 1;
+		}
+		public override void DrawBehind(int index) {
+			if (NPC.aiStyle == NPCAIStyleID.Star_Cell) Main.instance.DrawCacheNPCsOverPlayers.Add(index);
+		}
+		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
+			if (NPC.aiStyle == NPCAIStyleID.Star_Cell) {
+				Player targetPlayer = Main.player[NPC.target];
+				Main.EntitySpriteDraw(
+					HeadTexture,
+					(targetPlayer.Top) - screenPos,
+					null,
+					drawColor,
+					targetPlayer.headRotation,
+					new Vector2(11 - targetPlayer.direction * 4, 2),
+					NPC.scale,
+					targetPlayer.direction == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None,
+				0);
+				return false;
+			}
+			return true;
 		}
 	}
 }
