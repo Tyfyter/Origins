40786f66ffce91c072f1863619532f2501055539
Tyfyter <tyfyter2616@gmail.com>
2023-07-26 05:28:34 -0600

    Update OriginsGlobalBiome.cs

--- World/OriginsGlobalBiome.cs
+++ World/OriginsGlobalBiome.cs
@@ -1,16 +1,18 @@
 ﻿using AltLibrary;
 using AltLibrary.Common.AltBiomes;
 using Origins.Questing;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace Origins.World {
 	public class OriginsGlobalBiome : GlobalBiome {
 		public override void PostConvertTile(AltBiome oldBiome, AltBiome newBiome, int i, int j) {
+			if (WorldGen.generatingWorld) return;
 			Cleansing_Station_Quest cleansingStationQuest = ModContent.GetInstance<Cleansing_Station_Quest>();
-			if (newBiome.BiomeType != oldBiome.BiomeType && cleansingStationQuest.LocalPlayerStarted) {
-				if (oldBiome.BiomeType == BiomeType.Evil) {
+			if (newBiome?.BiomeType != oldBiome?.BiomeType && cleansingStationQuest.LocalPlayerStarted) {
+				if (oldBiome?.BiomeType == BiomeType.Evil) {
 					cleansingStationQuest.UpdateProgress(1);
-				} else if (newBiome.BiomeType == BiomeType.Evil) {
+				} else if (newBiome?.BiomeType == BiomeType.Evil) {
 					cleansingStationQuest.UpdateProgress(-1);
 				}
 			}
