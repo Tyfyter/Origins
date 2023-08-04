3e75b8d3c371bc2ca002ef74e24df119d86e6ca5
Tyfyter <tyfyter2616@gmail.com>
2023-08-04 00:21:32 -0600

    Altered altars

--- Tiles/Defiled/Defiled_Altar.cs
+++ Tiles/Defiled/Defiled_Altar.cs
@@ -16,6 +16,7 @@ namespace Origins.Tiles.Defiled {
 			Main.tileNoAttach[Type] = true;
 			Main.tileHammer[Type] = true;
 			Main.tileLighted[Type] = true;
+			TileID.Sets.PreventsTileRemovalIfOnTopOfIt[Type] = true;
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
 			TileObjectData.newTile.CoordinateHeights = new[] { 18, 18 };
 			TileObjectData.addTile(Type);
@@ -42,110 +43,7 @@ namespace Origins.Tiles.Defiled {
 		}
 
 		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
-			if (Main.netMode == NetmodeID.MultiplayerClient || !Main.hardMode || WorldGen.noTileActions || WorldGen.gen) {
-				return;
-			}
-			ushort defiledStoneID = (ushort)ModContent.TileType<Defiled_Stone>();
-			int type = WorldGen.altarCount % 3;
-			float veinCount = Main.maxTilesX / 4200;
-			int veinSize = 1 - type;
-			veinCount = veinCount * 310f - (85 * type);
-			veinCount *= 0.85f;
-			veinCount /= WorldGen.altarCount / 3 + 1;
-			int messageID = 12;
-			switch (type) {
-				case 0:
-				if (WorldGen.SavedOreTiers.Cobalt == -1) {
-					WorldGen.SavedOreTiers.Cobalt = TileID.Cobalt;
-					if (WorldGen.genRand.NextBool(2)) {
-						WorldGen.SavedOreTiers.Cobalt = TileID.Palladium;
-					}
-				}
-				messageID = 12;
-				if (WorldGen.SavedOreTiers.Cobalt == TileID.Palladium) {
-					messageID += 9;
-					veinCount *= 0.9f;
-				}
-				type = WorldGen.SavedOreTiers.Cobalt;
-				veinCount *= 1.05f;
-				break;
-				case 1:
-				if (WorldGen.SavedOreTiers.Mythril == -1) {
-					WorldGen.SavedOreTiers.Mythril = TileID.Mythril;
-					if (WorldGen.genRand.NextBool(2)) {
-						WorldGen.SavedOreTiers.Mythril = TileID.Orichalcum;
-					}
-				}
-				messageID = 13;
-				if (WorldGen.SavedOreTiers.Mythril == TileID.Orichalcum) {
-					messageID += 9;
-					veinCount *= 0.9f;
-				}
-				type = WorldGen.SavedOreTiers.Mythril;
-				break;
-				default:
-				if (WorldGen.SavedOreTiers.Adamantite == -1) {
-					WorldGen.SavedOreTiers.Adamantite = TileID.Adamantite;
-					if (WorldGen.genRand.NextBool(2)) {
-						WorldGen.SavedOreTiers.Adamantite = TileID.Titanium;
-					}
-				}
-				messageID = 14;
-				if (WorldGen.SavedOreTiers.Adamantite == TileID.Titanium) {
-					messageID += 9;
-					veinCount *= 0.9f;
-				}
-				type = WorldGen.SavedOreTiers.Adamantite;
-				break;
-			}
-
-			if (Main.netMode == NetmodeID.SinglePlayer) {
-				Main.NewText(Lang.misc[messageID].Value, 50, byte.MaxValue, 130);
-			} else if (Main.netMode == NetmodeID.Server) {
-				ChatHelper.BroadcastChatMessage(NetworkText.FromKey(Lang.misc[messageID].Key), new Color(50, 255, 130));
-			}
-
-			for (int k = 0; k < veinCount; k++) {
-				int oreX = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
-				double minDepth = Main.worldSurface;
-				if (type == TileID.Mythril || type == TileID.Orichalcum) {
-					minDepth = Main.rockLayer;
-				}
-				if (type == TileID.Adamantite || type == TileID.Titanium) {
-					minDepth = (Main.rockLayer + Main.rockLayer + Main.maxTilesY) / 3.0;
-				}
-				int oreY = WorldGen.genRand.Next((int)minDepth, Main.maxTilesY - 150);
-				WorldGen.OreRunner(oreX, oreY, WorldGen.genRand.Next(5, 9 + veinSize), WorldGen.genRand.Next(5, 9 + veinSize), (ushort)type);
-			}
-
-			int stoneType = WorldGen.genRand.Next(3);
-			int stoneCount = 0;
-			while (stoneType != 2 && stoneCount++ < 1000) {
-				int stoneX = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
-				int stoneY = WorldGen.genRand.Next((int)Main.rockLayer + 50, Main.maxTilesY - 300);
-				if (!Main.tile[stoneX, stoneY].HasTile || Main.tile[stoneX, stoneY].TileType != 1) {
-					continue;
-				}
-				if (stoneType == 1) {
-					Main.tile[stoneX, stoneY].TileType = defiledStoneID;
-				} else {
-					Main.tile[stoneX, stoneY].TileType = TileID.Pearlstone;
-				}
-				if (Main.netMode == NetmodeID.Server) {
-					NetMessage.SendTileSquare(-1, stoneX, stoneY, 1);
-				}
-				break;
-			}
-
-			if (Main.netMode != NetmodeID.MultiplayerClient) {
-				int wraithCount = Main.rand.Next(2) + 1;
-				for (int l = 0; l < wraithCount; l++) {
-					NPC.SpawnOnPlayer(Player.FindClosest(new Vector2(i * 16, j * 16), 16, 16), NPCID.Wraith);
-				}
-			}
-
-			WorldGen.altarCount++;
-			AchievementsHelper.NotifyProgressionEvent(6);
+			WorldGen.SmashAltar(i, j);
 		}
 		public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) {
 			r = g = b = 0.5f;
