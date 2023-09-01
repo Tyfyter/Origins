f56ddf2d02ca0ddc79ca5e0d2f6956f1b0c8f43d
Tyfyter <tyfyter2616@gmail.com>
2023-08-31 23:13:26 -0600

    slightly changed riven hive surface background

--- Backgrounds/Riven_Surface_Background.cs
+++ Backgrounds/Riven_Surface_Background.cs
@@ -4,21 +4,26 @@ using Origins;
 using Origins.Reflection;
 using Origins.World.BiomeData;
 using ReLogic.Content;
+using System;
 using Terraria;
 using Terraria.GameContent;
+using Terraria.Graphics.Effects;
 using Terraria.ModLoader;
 
 namespace Origins.Backgrounds {
 	public class Riven_Surface_Background : ModSurfaceBackgroundStyle {
-		Asset<Texture2D> glowTexture;
+		Asset<Texture2D> farGlowTexture;
+		Asset<Texture2D> closeGlowTexture;
 		public override int ChooseFarTexture() {
 			int textureSlot = BackgroundTextureLoader.GetBackgroundSlot("Origins/Backgrounds/Riven_Background3");
-			glowTexture ??= ModContent.Request<Texture2D>("Origins/Backgrounds/Riven_Background3_Glow");
-			Color glowColor = Color.Lerp(MainReflection.ColorOfSurfaceBackgroundsModified, Color.White, Riven_Hive.NormalGlowValue.GetValue() * 0.5f + 0.25f);
-			glowColor.A = 255;
+			farGlowTexture ??= ModContent.Request<Texture2D>("Origins/Backgrounds/Riven_Background3_Glow");
+			Color baseColor = MainReflection.ColorOfSurfaceBackgroundsModified;
+			Color glowColor = Color.Lerp(baseColor, Color.White, Riven_Hive.NormalGlowValue.GetValue() * 0.5f + 0.25f);
+			glowColor *= Main.bgAlphaFarBackLayer[Slot];
+			glowColor.A = baseColor.A;
 			for (int i = 0; i < MainReflection.bgLoops.GetValue(Main.instance); i++) {
 				Main.spriteBatch.Draw(
-					glowTexture.Value,
+					farGlowTexture.Value,
 					new Vector2(MainReflection.bgStartX.GetValue(Main.instance) + MainReflection.bgWidthScaled * i, MainReflection.bgTopY.GetValue(Main.instance)),
 					new Rectangle(0, 0, Main.backgroundWidth[textureSlot], Main.backgroundHeight[textureSlot]),
 					glowColor,
@@ -38,7 +43,59 @@ namespace Origins.Backgrounds {
 		public override int ChooseCloseTexture(ref float scale, ref double parallax, ref float a, ref float b) {
 			return BackgroundTextureLoader.GetBackgroundSlot("Origins/Backgrounds/Riven_Background");
 		}
+		public override bool PreDrawCloseBackground(SpriteBatch spriteBatch) {
+			closeGlowTexture ??= ModContent.Request<Texture2D>("Origins/Backgrounds/Riven_Background_Glow");
+			Color baseColor = MainReflection.ColorOfSurfaceBackgroundsModified;
+			Color glowColor = Color.Lerp(baseColor, Color.White, Riven_Hive.NormalGlowValue.GetValue() * 0.5f + 0.25f);
+			glowColor *= Main.bgAlphaFrontLayer[Slot];
+			glowColor.A = baseColor.A;
 
+			MainReflection.bgScale = 1.25f;
+			double bgParallax = 0.37;
+			float a = 1800f;
+			float b = 1750f;
+
+			float bgScale = MainReflection.bgScale;
+			int textureSlot = ChooseCloseTexture(ref bgScale, ref bgParallax, ref a, ref b);
+			Main.instance.LoadBackground(textureSlot);
+			MainReflection.bgScale = bgScale * 2f;
+			MainReflection.bgWidthScaled = (int)(Main.backgroundWidth[textureSlot] * MainReflection.bgScale);
+			SkyManager.Instance.DrawToDepth(Main.spriteBatch, 1f / (float)bgParallax);
+			MainReflection.Instance_bgStartX = (int)(-Math.IEEERemainder(Main.screenPosition.X * bgParallax, MainReflection.bgWidthScaled) - (MainReflection.bgWidthScaled / 2));
+			MainReflection.Instance_bgTopY = (int)((-(Main.screenPosition.Y + MainReflection.Instance_screenOff / 2f)) / (Main.worldSurface * 16.0) * a + b)
+				+ (int)MainReflection.Instance_scAdj;
+			if (Main.gameMenu) {
+				MainReflection.Instance_bgTopY = 320;
+			}
+			MainReflection.Instance_bgLoops = Main.screenWidth / MainReflection.bgWidthScaled + 2;
+			if (Main.screenPosition.Y < Main.worldSurface * 16.0 + 16.0) {
+				for (int i = 0; i < MainReflection.Instance_bgLoops; i++) {
+					Vector2 position = new Vector2(MainReflection.Instance_bgStartX + MainReflection.bgWidthScaled * i, MainReflection.Instance_bgTopY);
+					Rectangle frame = new Rectangle(0, 0, Main.backgroundWidth[textureSlot], Main.backgroundHeight[textureSlot]);
+					Main.spriteBatch.Draw(
+						TextureAssets.Background[textureSlot].Value,
+						position,
+						frame,
+						MainReflection.ColorOfSurfaceBackgroundsModified,
+						0f,
+						default,
+						MainReflection.bgScale,
+						SpriteEffects.None, 
+					0f);
+					Main.spriteBatch.Draw(
+						closeGlowTexture.Value,
+						position,
+						frame,
+						glowColor,
+						0f,
+						default,
+						MainReflection.bgScale,
+						SpriteEffects.None, 
+					0f);
+				}
+			}
+			return false;
+		}
 		public override void ModifyFarFades(float[] fades, float transitionSpeed) {
 			for (int i = 0; i < fades.Length; i++) {
 				if (i == Slot) {
--- Reflection/MainReflection.cs
+++ Reflection/MainReflection.cs
@@ -7,14 +7,21 @@ using Terraria.ModLoader;
 namespace Origins.Reflection {
 	public class MainReflection : ILoadable {
 		public static FastFieldInfo<Main, int> bgLoops;
+		public static int Instance_bgLoops { get => bgLoops.GetValue(Main.instance); set => bgLoops.SetValue(Main.instance, value); }
 		public static FastFieldInfo<Main, int> bgStartX;
+		public static int Instance_bgStartX { get => bgStartX.GetValue(Main.instance); set => bgStartX.SetValue(Main.instance, value); }
 		public static FastFieldInfo<Main, int> bgTopY;
+		public static int Instance_bgTopY { get => bgTopY.GetValue(Main.instance); set => bgTopY.SetValue(Main.instance, value); }
 		static FastStaticFieldInfo<Main, int> _bgWidthScaled;
 		public static int bgWidthScaled { get => _bgWidthScaled.GetValue(); set => _bgWidthScaled.SetValue(value); }
 		static FastStaticFieldInfo<Main, float> _bgScale;
 		public static float bgScale { get => _bgScale.GetValue(); set => _bgScale.SetValue(value); }
 		static FastStaticFieldInfo<Main, Color> _ColorOfSurfaceBackgroundsModified;
 		public static Color ColorOfSurfaceBackgroundsModified { get => _ColorOfSurfaceBackgroundsModified.GetValue(); set => _ColorOfSurfaceBackgroundsModified.SetValue(value); }
+		public static FastFieldInfo<Main, float> scAdj;
+		public static float Instance_scAdj { get => scAdj.GetValue(Main.instance); set => scAdj.SetValue(Main.instance, value); }
+		public static FastFieldInfo<Main, float> screenOff;
+		public static float Instance_screenOff { get => screenOff.GetValue(Main.instance); set => screenOff.SetValue(Main.instance, value); }
 		public void Load(Mod mod) {
 			bgLoops = new("bgLoops", BindingFlags.NonPublic);
 			bgStartX = new("bgStartX", BindingFlags.NonPublic);
@@ -22,6 +29,8 @@ namespace Origins.Reflection {
 			_bgWidthScaled = new("bgWidthScaled", BindingFlags.NonPublic);
 			_bgScale = new("bgScale", BindingFlags.NonPublic);
 			_ColorOfSurfaceBackgroundsModified = new("ColorOfSurfaceBackgroundsModified", BindingFlags.NonPublic);
+			scAdj = new("scAdj", BindingFlags.NonPublic);
+			screenOff = new("screenOff", BindingFlags.NonPublic);
 		}
 		public void Unload() {
 			bgLoops = null;
@@ -30,6 +39,8 @@ namespace Origins.Reflection {
 			_bgWidthScaled = null;
 			_bgScale = null;
 			_ColorOfSurfaceBackgroundsModified = null;
+			scAdj = null;
+			screenOff = null;
 		}
 	}
 }
