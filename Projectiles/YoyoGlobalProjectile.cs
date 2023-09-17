d06b64bb725308ce2c9db2212531adf18e93904f
Tyfyter <tyfyter2616@gmail.com>
2023-09-16 22:58:39 -0600

    turbo reel works for yoyos
    doesn't make much difference though, since they already return fast

--- Projectiles/YoyoGlobalProjectile.cs
+++ Projectiles/YoyoGlobalProjectile.cs
@@ -13,41 +13,35 @@ using Tyfyter.Utils;
 
 namespace Origins.Projectiles {
 	//separate global for organization
-	public class BoomerangGlobalProjectile : GlobalProjectile {
+	public class YoyoGlobalProjectile : GlobalProjectile {
 		bool isReturning = false;
-		bool boomerangMagnet = false;
 		public override bool InstancePerEntity => true;
 		protected override bool CloneNewInstances => false;
 		public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) {
-			return entity.aiStyle == ProjAIStyleID.Boomerang;
+			return entity.aiStyle == ProjAIStyleID.Yoyo;
 		}
 		public override void SetDefaults(Projectile projectile) {
 			isReturning = false;
-			boomerangMagnet = false;
 		}
 		public override void AI(Projectile projectile) {
 			Player player = Main.player[projectile.owner];
-			if (projectile.ai[0] == 1) {
+			if (projectile.ai[0] == -1) {
 				if (!isReturning && Vector2.Dot(projectile.velocity.SafeNormalize(default), projectile.DirectionTo(player.MountedCenter)) > 0) {
 					OriginPlayer originPlayer = player.GetModPlayer<OriginPlayer>();
-					if (originPlayer.boomerangMagnet) {
-						//projectile.extraUpdates++;
-						boomerangMagnet = true;
+					if (originPlayer.turboReel2) {
+						projectile.extraUpdates += 2;
+					} else if (originPlayer.turboReel) {
+						projectile.extraUpdates++;
 					}
 					isReturning = true;
 				}
 			}
-			if (boomerangMagnet) {
-				projectile.velocity += projectile.DirectionTo(player.MountedCenter) * 1.25f;
-			}
 		}
 		public override void SendExtraAI(Projectile projectile, BitWriter bitWriter, BinaryWriter binaryWriter) {
 			bitWriter.WriteBit(isReturning);
-			bitWriter.WriteBit(boomerangMagnet);
 		}
 		public override void ReceiveExtraAI(Projectile projectile, BitReader bitReader, BinaryReader binaryReader) {
 			isReturning = bitReader.ReadBit();
-			boomerangMagnet = bitReader.ReadBit();
 		}
 	}
 }
