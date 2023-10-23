b3e7aa1734681437a164d97a9f585e220ca36cc6
Tyfyter <tyfyter2616@gmail.com>
2023-10-22 19:12:06 -0600

    fixed Broken Record

--- Items/Other/Consumables/Broken_Record.cs
+++ Items/Other/Consumables/Broken_Record.cs
@@ -1,6 +1,7 @@
 ﻿using Origins.Items.Accessories;
 using Origins.Items.Materials;
 using Origins.Items.Weapons.Melee;
+using Origins.World;
 using Terraria;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
@@ -12,26 +13,21 @@ namespace Origins.Items.Other.Consumables {
 		}
 		public override void SetDefaults() {
 			Item.CloneDefaults(ItemID.CultistBossBag);
-            Item.maxStack = Item.CommonMaxStack;
-            Item.rare = ItemRarityID.Blue;
+			Item.maxStack = Item.CommonMaxStack;
+			Item.rare = ItemRarityID.Blue;
 			Item.expert = false;
 		}
-        public override void AddRecipes()
-        {
-            Recipe recipe = Recipe.Create(Type);
-            recipe.AddIngredient(ModContent.ItemType<Ash_Urn>(), 30);
-            recipe.AddIngredient(ModContent.ItemType<Biocomponent10>(), 15);
-            recipe.AddTile(TileID.DemonAltar);
-            recipe.Register();
-        }
-        public override void ModifyItemLoot(ItemLoot itemLoot) {
-            itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<Cinder_Seal>(), 5));
-            //itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<Neural_Network>(), 5));
-            //itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<Smiths_Hammer>(), 5));
-            //itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<Area_Denial>(), 5));
-            itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<Outreach>(), 5));
-        }
-        public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup) {
+		public override void AddRecipes() {
+			Recipe recipe = Recipe.Create(Type);
+			recipe.AddIngredient(ModContent.ItemType<Ash_Urn>(), 30);
+			recipe.AddIngredient(ModContent.ItemType<Biocomponent10>(), 15);
+			recipe.AddTile(TileID.DemonAltar);
+			recipe.Register();
+		}
+		public override void ModifyItemLoot(ItemLoot itemLoot) {
+			itemLoot.Add(AshenBiomeData.OrbDropRule);
+		}
+		public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup) {
 			itemGroup = ContentSamples.CreativeHelper.ItemGroup.GoodieBags;
 		}
 		public override bool CanRightClick() => true;
--- World/AshenBiomeData.cs
+++ World/AshenBiomeData.cs
@@ -1,5 +1,7 @@
 ﻿using AltLibrary.Common.AltBiomes;
+using Origins.Items.Accessories;
 using Origins.Items.Materials;
+using Origins.Items.Weapons.Melee;
 using Origins.Tiles.Ashen;
 using System;
 using System.Collections.Generic;
@@ -7,11 +9,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.World {
-	public class AshenBiomeData {
+	public class AshenBiomeData : ModBiome {
+		public static IItemDropRule FirstOrbDropRule;
+		public static IItemDropRule OrbDropRule;
+		public override void Load() {
+			FirstOrbDropRule = ItemDropRule.Common(ItemID.FlintlockPistol);
+			FirstOrbDropRule.OnSuccess(ItemDropRule.Common(ItemID.MusketBall, 1, 100, 100));
+
+			OrbDropRule = new OneFromRulesRule(1,
+				FirstOrbDropRule,
+				//ItemDropRule.NotScalingWithLuck(ModContent.ItemType<Area_Denial>()),
+				ItemDropRule.NotScalingWithLuck(ModContent.ItemType<Outreach>()),
+				//ItemDropRule.NotScalingWithLuck(ModContent.ItemType<Smiths_Hammer>()),
+				ItemDropRule.NotScalingWithLuck(ModContent.ItemType<Cinder_Seal>())
+			);
+		}
+		public override void Unload() {
+			FirstOrbDropRule = null;
+			OrbDropRule = null;
+		}
 	}
 	public class Ashen_Alt_Biome : AltBiome {
 		AltMaterialContext materialContext;
