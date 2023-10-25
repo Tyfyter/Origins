7b314fc96a8fe33a4590dbc4b0f757153acad51c
Tyfyter <tyfyter2616@gmail.com>
2023-10-24 22:45:24 -0600

    minor fixes & a perfect world

--- Tiles/Decoration/A_Perfect_World.cs
+++ Tiles/Decoration/A_Perfect_World.cs
@@ -6,33 +6,25 @@ using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 
-namespace Origins.Tiles.Other {
+namespace Origins.Tiles.Decoration {
     public class A_Perfect_World : ModTile {
 		public override void SetStaticDefaults() {
 			Main.tileFrameImportant[Type] = true;
 			Main.tileLavaDeath[Type] = true;
 			TileID.Sets.FramesOnKillWall[Type] = true;
 
-			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+			TileObjectData.newTile.Origin = new Point16(1, 0);
+			TileObjectData.newTile.Height = 2;
 			TileObjectData.addTile(Type);
 
 			AddMapEntry(new Color(120, 85, 60), Language.GetText("MapObject.Painting"));
 		}
-
-		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
-			Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<A_Perfect_World_Item>());
-		}
 	}
 	public class A_Perfect_World_Item : ModItem {
-		public override void SetStaticDefaults() {
-			// DisplayName.SetDefault("A Perfect World");
-			// Tooltip.SetDefault("'The Defiled'");
-		}
-
 		public override void SetDefaults() {
-			Item.CloneDefaults(ItemID.PaintingMartiaLisa);
+			Item.DefaultToPlaceableTile(ModContent.TileType<A_Perfect_World>());
 			Item.value = Item.sellPrice(gold: 2);
-			Item.createTile = ModContent.TileType<A_Perfect_World>();
 		}
 	}
 }
