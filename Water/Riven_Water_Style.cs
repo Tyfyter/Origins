56bc87d9f55f84a4f9887f9dd36317618b49f76b
Tyfyter <tyfyter2616@gmail.com>
2023-11-02 23:45:36 -0600

    made riven water breathe too

--- Water/Riven_Water_Style.cs
+++ Water/Riven_Water_Style.cs
@@ -9,14 +9,15 @@ namespace Origins.Water {
 		public override int GetDropletGore() => GoreID.ChimneySmoke1 + Main.rand.Next(3);
 		public override int GetSplashDust() => 99;
 		public override void LightColorMultiplier(ref float r, ref float g, ref float b) {
-			r = 0.1f;
-			g = 1.05f;
-			b = 1f;
+			float glowValue = World.BiomeData.Riven_Hive.NormalGlowValue.GetValue() * 0.5f + 0.25f;
+			r = 0.1f * glowValue;
+			g = 1.05f * glowValue;
+			b = 1f * glowValue;
 		}
 		public override Color BiomeHairColor() => new Color(50, 250, 230);
 
 		public void AddLight(ref Vector3 color, byte liquidAmount) {
-			float mult = liquidAmount > 200 ? 1 : liquidAmount / 200;
+			float mult = (liquidAmount > 200 ? 1 : liquidAmount / 200) * World.BiomeData.Riven_Hive.NormalGlowValue.GetValue();
 
 			color.Y += 0.9f * mult;
 			color.Z += 1f * mult;
