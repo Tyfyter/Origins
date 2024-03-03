8452ed08f780f863b12d52519a7aaf042e7c8805
Tyfyter <tyfyter2616@gmail.com>
2024-03-02 23:03:19 -0700

    Furnished

--- Tiles/Endowood/Endowood_Furniture.cs
+++ Tiles/Endowood/Endowood_Furniture.cs
@@ -3,6 +3,9 @@ using Origins.Items.Materials;
 using Origins.Tiles.Defiled;
 using System.Collections.Generic;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.ObjectInteractions;
 using Terraria.ID;
 
 namespace Origins.Tiles.Endowood {
@@ -15,6 +18,45 @@ namespace Origins.Tiles.Endowood {
 				.AddTile(TileID.WorkBenches)
 				.Register();
 			};
+		}
+	}
+	public class Endowood_Grandfather_Clock : ClockBase {
+		public override Color MapColor => new(44, 39, 58);
+		public override void OnLoad() {
+			item.OnAddRecipes += (item) => {
+				Recipe.Create(item.type)
+				.AddRecipeGroup(RecipeGroupID.IronBar, 3)
+				.AddIngredient(ItemID.Glass, 6)
+				.AddIngredient<Endowood_Item>(10)
+				.AddTile(TileID.Sawmill)
+				.Register();
+			};
+		}
+	}
+	public class Endowood_Chair : ChairBase {
+		public override Color MapColor => new(44, 39, 58);
+		public override void OnLoad() {
+			item.OnAddRecipes += (item) => {
+				Recipe.Create(item.type)
+				.AddIngredient<Endowood_Item>(4)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+			};
+		}
+	}
+	public class Endowood_Toilet : ChairBase {
+		public override int BaseTileID => TileID.Toilets;
+		public override Color MapColor => new(44, 39, 58);
+		public override void OnLoad() {
+			item.OnAddRecipes += (item) => {
+				Recipe.Create(item.type)
+				.AddIngredient<Endowood_Item>(6)
+				.AddTile(TileID.Sawmill)
+				.Register();
+			};
+		}
+		public override void HitWire(int i, int j) {
+			ToiletHitWire(i, j);
 		}
 	}
 }
--- Tiles/FurnitureBase.cs
+++ Tiles/FurnitureBase.cs
@@ -11,6 +11,7 @@ using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.GameContent;
 using Terraria.GameContent.Drawing;
 using Terraria.GameContent.ObjectInteractions;
 using Terraria.ID;
@@ -23,6 +24,12 @@ namespace Origins.Tiles {
 		public abstract int BaseTileID { get; }
 		public abstract Color MapColor { get; }
 		public virtual bool LavaDeath => true;
+		protected internal TileItem item;
+		public sealed override void Load() {
+			Mod.AddContent(item = new TileItem(this));
+			OnLoad();
+		}
+		public virtual void OnLoad() { }
 		public override void SetStaticDefaults() {
 			Main.tileFrameImportant[Type] = true;
 			Main.tileLavaDeath[Type] = LavaDeath;
@@ -30,6 +37,7 @@ namespace Origins.Tiles {
 			Main.tileTable[Type] = Main.tileTable[BaseTileID];
 			Main.tileSolidTop[Type] = Main.tileSolidTop[BaseTileID];
 			Main.tileNoAttach[Type] = Main.tileNoAttach[BaseTileID];
+			TileID.Sets.HasOutlines[Type] = TileID.Sets.HasOutlines[BaseTileID];
 			TileID.Sets.DisableSmartCursor[Type] = TileID.Sets.DisableSmartCursor[BaseTileID];
 			TileID.Sets.IgnoredByNpcStepUp[Type] = TileID.Sets.IgnoredByNpcStepUp[BaseTileID];
 
@@ -55,15 +63,92 @@ namespace Origins.Tiles {
 			if (TileID.Sets.RoomNeeds.CountsAsChair.Contains(BaseTileID)) AddToArray(ref TileID.Sets.RoomNeeds.CountsAsChair);
 		}
 	}
+	public abstract class ChairBase : FurnitureBase {
+		public override int BaseTileID => TileID.Chairs;
+		public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) {
+			return settings.player.IsWithinSnappngRangeToTile(i, j, PlayerSittingHelper.ChairSittingMaxDistance); // Avoid being able to trigger it from long range
+		}
+		public override bool RightClick(int i, int j) {
+			Player player = Main.LocalPlayer;
+
+			if (player.IsWithinSnappngRangeToTile(i, j, PlayerSittingHelper.ChairSittingMaxDistance)) { // Avoid being able to trigger it from long range
+				player.GamepadEnableGrappleCooldown();
+				player.sitting.SitDown(player, i, j);
+			}
+
+			return true;
+		}
+		public override void MouseOver(int i, int j) {
+			Player player = Main.LocalPlayer;
+
+			if (!player.IsWithinSnappngRangeToTile(i, j, PlayerSittingHelper.ChairSittingMaxDistance)) { // Match condition in RightClick. Interaction should only show if clicking it does something
+				return;
+			}
+
+			player.noThrow = 2;
+			player.cursorItemIconEnabled = true;
+			player.cursorItemIconID = item.Type;
+
+			if (Main.tile[i, j].TileFrameX / 18 < 1) {
+				player.cursorItemIconReversed = true;
+			}
+		}
+		public override void ModifySittingTargetInfo(int i, int j, ref TileRestingInfo info) {
+			// It is very important to know that this is called on both players and NPCs, so do not use Main.LocalPlayer for example, use info.restingEntity
+			Tile tile = Framing.GetTileSafely(i, j);
+
+			//info.directionOffset = info.restingEntity is Player ? 6 : 2; // Default to 6 for players, 2 for NPCs
+			//info.visualOffset = Vector2.Zero; // Defaults to (0,0)
+
+			info.TargetDirection = -1;
+
+			if (tile.TileFrameX != 0) {
+				info.TargetDirection = 1; // Facing right if sat down on the right alternate (added through addAlternate in SetStaticDefaults earlier)
+			}
+
+			// The anchor represents the bottom-most tile of the chair. This is used to align the entity hitbox
+			// Since i and j may be from any coordinate of the chair, we need to adjust the anchor based on that
+			info.AnchorTilePosition.X = i; // Our chair is only 1 wide, so nothing special required
+			info.AnchorTilePosition.Y = j;
+
+			if (tile.TileFrameY % 40 == 0) {
+				info.AnchorTilePosition.Y++; // Here, since our chair is only 2 tiles high, we can just check if the tile is the top-most one, then move it 1 down
+			}
+
+			// Finally, since this is a toilet, it should generate Poo while any tier of Well Fed is active
+			info.ExtraInfo.IsAToilet = BaseTileID == TileID.Toilets;
+		}
+		public static void ToiletHitWire(int i, int j) {
+			// Spawn the toilet effect here when triggered by a signal
+			Tile tile = Main.tile[i, j];
+
+			int spawnX = i;
+			int spawnY = j - (tile.TileFrameY % 40) / 18;
+
+			Wiring.SkipWire(spawnX, spawnY);
+			Wiring.SkipWire(spawnX, spawnY + 1);
+
+			if (Wiring.CheckMech(spawnX, spawnY, 60)) {
+				Projectile.NewProjectile(Wiring.GetProjectileSource(spawnX, spawnY), spawnX * 16 + 8, spawnY * 16 + 12, 0f, 0f, ProjectileID.ToiletEffect, 0, 0f, Main.myPlayer);
+			}
+		}
+	}
 	public abstract class ClockBase : ModTile {
 		public abstract Color MapColor { get; }
 		public virtual bool LavaDeath => true;
+		protected internal TileItem item;
+		public sealed override void Load() {
+			Mod.AddContent(item = new TileItem(this));
+			OnLoad();
+		}
+		public virtual void OnLoad() { }
 		public override void SetStaticDefaults() {
 			// Properties
 			Main.tileFrameImportant[Type] = true;
 			Main.tileNoAttach[Type] = true;
 			Main.tileLavaDeath[Type] = LavaDeath;
 			TileID.Sets.Clock[Type] = true;
+			TileID.Sets.HasOutlines[Type] = true;
 
 			AdjTiles = new int[] { TileID.GrandfatherClocks };
 
@@ -71,12 +156,15 @@ namespace Origins.Tiles {
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
 			TileObjectData.newTile.Height = 5;
 			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16, 16 };
+			TileObjectData.newTile.Origin = new(0, 4);
 			TileObjectData.addTile(Type);
 
 			// Etc
 			AddMapEntry(MapColor, Language.GetText("ItemName.GrandfatherClock"));
 		}
-
+		public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) {
+			return true;
+		}
 		public override bool RightClick(int x, int y) {
 			string text = "AM";
 			// Get current weird time
