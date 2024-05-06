d41c529c4feaa4b762fcc8304f859475a45c8049
Tyfyter <tyfyter2616@gmail.com>
2024-05-06 00:09:43 -0600

    Update Journal_Item.cs

--- Items/Other/Journal_Item.cs
+++ Items/Other/Journal_Item.cs
@@ -5,15 +5,29 @@ using Terraria.ModLoader;
 
 namespace Origins.Items.Other {
 	public class Journal_Item : ModItem {
+		public override void SetDefaults() {
+			Item.CloneDefaults(ItemID.FallenStar);
+			Item.maxStack = 1;
+		}
+		public override bool? UseItem(Player player) {
+			ref bool isUnlocked = ref Main.LocalPlayer.GetModPlayer<OriginPlayer>().journalUnlocked;
+			if (!isUnlocked) {
+				isUnlocked = true;
+				Item.TurnToAir();
+			}
+			return true;
+		}
 		public override void AddRecipes() {
 			Recipe recipe = CreateRecipe();
 			recipe.AddIngredient(ItemID.Book);
 			recipe.AddIngredient(ItemID.Feather);
 			recipe.AddIngredient(ItemID.BlackInk);
-			recipe.AddCondition(LocalizedText.Empty, () => !Main.LocalPlayer.GetModPlayer<OriginPlayer>().journalUnlocked);
 			recipe.AddOnCraftCallback((_, i, _, _) => {
-				Main.LocalPlayer.GetModPlayer<OriginPlayer>().journalUnlocked = true;
-				i.TurnToAir();
+				ref bool isUnlocked = ref Main.LocalPlayer.GetModPlayer<OriginPlayer>().journalUnlocked;
+				if (!isUnlocked) {
+					isUnlocked = true;
+					i.TurnToAir();
+				}
 			});
 			recipe.Register();
 		}
