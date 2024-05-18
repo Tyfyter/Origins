d0a855ee1e02cdbead37c6a2d6a06c71861763ad
Tyfyter <tyfyter2616@gmail.com>
2024-05-18 14:56:36 -0600

    Should fix animated item sprite paths

--- Dev/WikiPageExporter.cs
+++ Dev/WikiPageExporter.cs
@@ -274,7 +274,7 @@ namespace Origins.Dev {
 		public static string GetWikiName(ModNPC modNPC) => modNPC.Name;//maybe switch to WebUtility.UrlEncode(modItem.DisplayName.Value);
 		public static string GetWikiPagePath(string name) => Path.Combine(DebugConfig.Instance.WikiPagePath, name + ".html");
 		public static string GetWikiStatPath(string name) => Path.Combine(DebugConfig.Instance.StatJSONPath, name + ".json");
-		public static string GetWikiItemPath(ModItem modItem) => Main.itemAnimations[modItem.Type] is DrawAnimation ? $"{modItem.Name}" : modItem.Texture.Replace(modItem.Mod.Name, "§ModImage§");
+		public static string GetWikiItemImagePath(ModItem modItem) => Main.itemAnimations[modItem.Type] is not null ? $"Images/{modItem.Name}" : modItem.Texture.Replace(modItem.Mod.Name, "§ModImage§");
 		public static string GetWikiItemRarity(Item item) => (RarityLoader.GetRarity(item.rare)?.Name ?? ItemRarityID.Search.GetName(item.rare)).Replace("Rarity", "");
 		public void Unload() {
 			wikiTemplate = null;
@@ -638,7 +638,7 @@ namespace Origins.Dev {
 			Item item = modItem.Item;
 			JObject data = new();
 			ICustomWikiStat customStat = item.ModItem as ICustomWikiStat;
-			data["Image"] = WikiPageExporter.GetWikiItemPath(modItem);
+			data["Image"] = WikiPageExporter.GetWikiItemImagePath(modItem);
 			data["Name"] = item.Name;
 			JArray types = new("Item");
 			if (item.accessory) types.Add("Accessory");
