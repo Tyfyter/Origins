a8653d7c43140e9de1046bb17af1feeedf3b8826
Tyfyter <tyfyter2616@gmail.com>
2024-06-20 18:58:44 -0600

    bricks

---
+++ Tiles/Other/Encrusted_Brick.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Origins.Dev;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Origins.Tiles.Other {
+	public class Encrusted_Brick : OriginTile, ICustomWikiStat {
+        public string[] Categories => [
+            "Brick"
+        ];
+        public override void SetStaticDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			AddMapEntry(new Color(70, 110, 177));
+		}
+	}
+	public class Encrusted_Brick_Item : ModItem {
+		public override void SetStaticDefaults() {
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults() {
+			Item.DefaultToPlaceableTile(TileType<Encrusted_Brick>());
+		}
+	}
+}
---
+++ Tiles/Other/Lost_Brick.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Origins.Dev;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Origins.Tiles.Other {
+	public class Lost_Brick : OriginTile, ICustomWikiStat {
+        public string[] Categories => [
+            "Brick"
+        ];
+        public override void SetStaticDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			AddMapEntry(new Color(151, 151, 151));
+		}
+	}
+	public class Lost_Brick_Item : ModItem {
+		public override void SetStaticDefaults() {
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults() {
+			Item.DefaultToPlaceableTile(TileType<Lost_Brick>());
+		}
+	}
+}
