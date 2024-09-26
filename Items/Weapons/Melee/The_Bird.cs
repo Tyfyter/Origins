cfec77339c81c361577279e80f6bd1a3ea78535f
Tyfyter <tyfyter2616@gmail.com>
2024-09-26 15:03:50 -0600

    bird rights

--- Items/Weapons/Melee/The_Bird.cs
+++ Items/Weapons/Melee/The_Bird.cs
@@ -2,17 +2,17 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-
 using Origins.Dev;
 using Microsoft.Xna.Framework;
+
 namespace Origins.Items.Weapons.Melee {
 	public class The_Bird : ModItem, ICustomWikiStat {
-        public string[] Categories => [
-            "Sword",
+		public string[] Categories => [
+			"Sword",
 			"DeveloperItem",
 			"ReworkExpected"
-        ];
-        public override void SetDefaults() {
+		];
+		public override void SetDefaults() {
 			Item.CloneDefaults(ItemID.WoodenSword);
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.damage = 99;
@@ -24,12 +24,47 @@ namespace Origins.Items.Weapons.Melee {
 			//Recipe.Create(Type)
 			//.AddIngredient(ModContent.ItemType<Baseball_Bat>())
 			//.AddIngredient(ModContent.ItemType<Razorwire>())
-			//.AddCondition(player.name == "Pandora");
 			//.Register();
 		}
 		public override bool AltFunctionUse(Player player) => true;
 		public override void UseItemFrame(Player player) {
-			
+			int frame = player.bodyFrame.Y / player.bodyFrame.Height;
+			float progress = player.itemAnimation / (float)player.itemAnimationMax;
+			if (player.altFunctionUse == 2) {
+				progress *= progress;
+				frame = 2 + (int)(progress * 4f);
+				if (frame == 5) frame = 0;
+				switch (frame) {
+					case 0:
+					player.itemLocation = player.MountedCenter + new Vector2(-4 * player.direction, 4);
+					break;
+					case 4:
+					player.itemLocation = player.MountedCenter + new Vector2(4 * player.direction, 8);
+					break;
+					case 3:
+					player.itemLocation = player.MountedCenter + new Vector2(6 * player.direction, 8);
+					break;
+					case 2:
+					player.itemLocation = player.MountedCenter + new Vector2(6 * player.direction, -8);
+					break;
+				}
+				player.itemRotation = (progress * 3 - 0.5f) * player.direction;
+			}
+			player.bodyFrame.Y = player.bodyFrame.Height * frame;
+		}
+		public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox) {
+			if (player.altFunctionUse == 2) {
+				switch (player.bodyFrame.Y / player.bodyFrame.Height) {
+					case 0:
+					hitbox.Y += 48;
+					break;
+					case 2 or 3 or 4:
+					hitbox = player.Hitbox;
+					hitbox.X += player.direction * 24;
+					hitbox.Inflate(8, (80 - player.height) / 2);
+					break;
+				}
+			}
 		}
 		public override void ModifyHitNPC(Player player, NPC target, ref NPC.HitModifiers modifiers) {
 			modifiers.HitDirectionOverride = 0;
