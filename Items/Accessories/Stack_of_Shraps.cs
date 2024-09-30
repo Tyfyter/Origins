f787251cdf185460af3ced4423a7300d72d6337f
Tyfyter <tyfyter2616@gmail.com>
2024-09-29 21:37:28 -0600

    Update Stack_of_Shraps.cs

--- Items/Accessories/Stack_of_Shraps.cs
+++ Items/Accessories/Stack_of_Shraps.cs
@@ -1,13 +1,17 @@
 ﻿using Origins.Dev;
+using Origins.Journal;
 using Terraria;
+using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 namespace Origins.Items.Accessories {
-	public class Stack_of_Shraps : ModItem, ICustomWikiStat {
+	public class Stack_of_Shraps : ModItem, ICustomWikiStat, IJournalEntryItem {
 		public string[] Categories => [
 			"Combat",
 			"ExplosiveBoostAcc"
 		];
+		public string IndicatorKey => "Mods.Origins.Journal.Indicator.Whispers";
+		public string EntryName => "Origins/" + typeof(Stack_of_Shraps_Entry).Name;
 		public override void SetDefaults() {
 			Item.DefaultToAccessory(32, 26);
 			Item.value = Item.sellPrice(gold: 2);
@@ -20,5 +24,9 @@ namespace Origins.Items.Accessories {
 			originPlayer.explosiveBlastRadius += 0.15f;
 			originPlayer.explosiveThrowSpeed += 0.2f;
 		}
+	}
+	public class Stack_of_Shraps_Entry : JournalEntry {
+		public override string TextKey => nameof(Stack_of_Shraps);
+		public override ArmorShaderData TextShader => null;
 	}
 }
