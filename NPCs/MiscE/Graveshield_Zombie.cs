b90aa192bebb68f012ac5963005acbf8bc070964
Tyfyter <tyfyter2616@gmail.com>
2024-10-22 15:10:40 -0600

    Graveshield Zombie

---
+++ NPCs/MiscE/Graveshield_Zombie.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Origins.Tiles.Other;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Origins.NPCs.MiscE {
+    public class Graveshield_Zombie : ModNPC {
+		public override void SetStaticDefaults() {
+			NPCID.Sets.ShimmerTransformToNPC[NPC.type] = NPCID.BoneThrowingSkeleton;
+			Main.npcFrameCount[NPC.type] = 3;
+			NPCID.Sets.NPCBestiaryDrawOffset[Type] = NPCExtensions.BestiaryWalkLeft;
+		}
+		public override void SetDefaults() {
+			NPC.CloneDefaults(NPCID.Zombie);
+			NPC.lifeMax = 55;
+			NPC.defense = 14;
+			NPC.damage = 12;
+			NPC.width = 28;
+			NPC.height = 46;
+			NPC.friendly = false;
+			AIType = NPCID.Zombie;
+			AnimationType = NPCID.Zombie;
+			Banner = NPCID.Zombie;
+		}
+		public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+			if ((spawnInfo.Player.ZoneGraveyard || !Main.dayTime) && spawnInfo.Player.ZoneForest) {
+				return 0.05f;
+			}
+			return 0;
+		}
+		public override void ModifyIncomingHit(ref NPC.HitModifiers modifiers) {
+			if (modifiers.HitDirection == -NPC.direction) {
+				modifiers.Defense.Base += 6;
+				modifiers.Knockback *= 0.25f;
+			}
+		}
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
+			bestiaryEntry.AddTags(
+				this.GetBestiaryFlavorText(),
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Graveyard,
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime
+			);
+		}
+		public override void OnKill() {
+			NPC.DropTombstoneTownNPC(NetworkText.FromKey(Lang.misc[19].Key, NPC.GetFullNetName()));
+		}
+		public override void HitEffect(NPC.HitInfo hit) {
+			if (NPC.life < 0 || OriginsModIntegrations.CheckAprilFools()) {
+				Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 4);
+				Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 20f), NPC.velocity, 4);
+				Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 34f), NPC.velocity, 5);
+				Gore.NewGore(NPC.GetSource_Death(), new Vector2(NPC.position.X, NPC.position.Y + 34f), NPC.velocity, 5);
+			}
+		}
+	}
+}
