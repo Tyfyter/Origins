48379c234224977983cdc9f942eb041d9297c5e6
Tyfyter <tyfyter2616@gmail.com>
2024-10-23 13:58:46 -0600

    fixed felnum sound effect playing without bonus

--- Projectiles/OriginGlobalProj.cs
+++ Projectiles/OriginGlobalProj.cs
@@ -254,10 +254,12 @@ namespace Origins.Projectiles {
 			if (target.boss && godHunterEffect != 0f) {
 				modifiers.SourceDamage *= 1 + godHunterEffect;
 			}
-			modifiers.SourceDamage.Flat += felnumBonus / Felnum_Helmet.shock_damage_divisor;
-			if (!OriginPlayer.ShouldApplyFelnumEffectOnShoot(projectile)) {
-				Main.player[projectile.owner].OriginPlayer().usedFelnumShock = true;
-				SoundEngine.PlaySound(SoundID.Item122.WithPitch(1).WithVolume(2), projectile.Center);
+			if (felnumBonus > Felnum_Helmet.shock_damage_divisor * 2) {
+				modifiers.SourceDamage.Flat += felnumBonus / Felnum_Helmet.shock_damage_divisor;
+				if (!OriginPlayer.ShouldApplyFelnumEffectOnShoot(projectile)) {
+					Main.player[projectile.owner].OriginPlayer().usedFelnumShock = true;
+					SoundEngine.PlaySound(SoundID.Item122.WithPitch(1).WithVolume(2), projectile.Center);
+				}
 			}
 		}
 		public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone) {
