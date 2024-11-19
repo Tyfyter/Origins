7453bec0e1b117564de3b955c662877a88b6e4b7
Tyfyter <tyfyter2616@gmail.com>
2024-11-18 18:31:36 -0700

    CORE generator stars now explode

--- Items/Accessories/CORE_Generator.cs
+++ Items/Accessories/CORE_Generator.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoMod.Cil;
 using Origins.Dev;
 using Origins.Items.Weapons.Ammo.Canisters;
 using Origins.Projectiles;
 using PegasusLib;
+using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Tyfyter.Utils;
+
 namespace Origins.Items.Accessories {
 	[AutoloadEquip(EquipType.Waist)]
 	public class CORE_Generator : ModItem, ICustomWikiStat {
@@ -17,9 +20,38 @@ namespace Origins.Items.Accessories {
 			"Combat",
 			"ExplosiveBoostAcc"
 		];
+		public static int ID { get; private set; }
 		public override void SetStaticDefaults() {
 			AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[Type] = AmmoID.Sets.SpecificLauncherAmmoProjectileMatches[ItemID.RocketLauncher];
+			IL_Player.Hurt_HurtInfo_bool += IL_Player_Hurt_HurtInfo_bool;
+			ID = Type;
 		}
+
+		private static void IL_Player_Hurt_HurtInfo_bool(ILContext il) {
+			ILCursor c = new(il);
+			int item = -1;
+			int type = -1;
+			c.GotoNext(MoveType.Before,
+				i => i.MatchLdarg0(),
+				i => i.MatchLdfld<Player>(nameof(Player.starCloakItem_starVeilOverrideItem)),
+				i => i.MatchStloc(out item),
+				i => i.MatchLdcI4(ProjectileID.StarVeilStar),
+				i => i.MatchStloc(out type)
+			);
+			c.GotoPrev(MoveType.After,
+				i => i.MatchLdarg0(),
+				i => i.MatchLdfld<Player>(nameof(Player.starCloakItem)),
+				i => i.MatchStloc(out item)
+			);
+			c.EmitLdloc(item);
+			c.EmitLdloca(type);
+			c.EmitDelegate<StarCloakOverride>(static (Item item, ref int type) => {
+				if (item.type == ID) {
+					type = CORE_Generator_Star_P.ID;
+				}
+			});
+		}
+		delegate void StarCloakOverride(Item item, ref int type);
 		public override void SetDefaults() {
 			Item.DefaultToAccessory(26, 26);
 			Item.value = Item.sellPrice(gold: 7);
@@ -54,7 +86,6 @@ namespace Origins.Items.Accessories {
 			originPlayer.coreGeneratorItem = Item;
 			player.longInvince = true;
 			player.starCloakItem = Item;
-			player.starCloakItem_starVeilOverrideItem = Item;
 		}
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
 			if (CanisterGlobalItem.GetCanisterType(source.AmmoItemIdUsed) != -1) {
@@ -101,6 +132,76 @@ namespace Origins.Items.Accessories {
 				return false;
 			}
 			return true;
+		}
+	}
+	public class CORE_Generator_Star_P : ModProjectile {
+		public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.StarVeilStar;
+		public static int ID { get; private set; }
+		public override void SetStaticDefaults() {
+			ID = Type;
+		}
+		public override void SetDefaults() {
+			Projectile.CloneDefaults(ProjectileID.StarVeilStar);
+			Projectile.DamageType = DamageClasses.Explosive;
+		}
+		public override void AI() {
+			base.AI();
+			if (Main.rand.NextBool(3)) {
+				if (Main.rand.NextBool(9)) {
+					Gore.NewGoreDirect(Projectile.GetSource_FromThis(), Projectile.position, Projectile.velocity.RotatedByRandom(0.1f) * -0.2f, Main.rand.Next(61, 64));
+				} else {
+					Dust.NewDustDirect(
+						Projectile.position,
+						Projectile.width,
+						Projectile.height,
+						DustID.Smoke,
+						Projectile.velocity.X * -0.2f,
+						Projectile.velocity.Y * -0.2f,
+						100,
+						default,
+						1f
+					);
+				}
+			}
+		}
+		public override void OnKill(int timeLeft) {
+			ExplosiveGlobalProjectile.DoExplosion(Projectile, 96, false, sound: SoundID.Item62);
+		}
+		public override bool PreDraw(ref Color lightColor) {
+			Texture2D trailTexture = TextureAssets.Extra[91].Value;
+			Vector2 trailOrigin = new(trailTexture.Width / 2f, 10f);
+			Vector2 gfxOffset = new(0f, Projectile.gfxOffY);
+			float visualTime = (float)Main.timeForVisualEffects / 45f;
+			Vector2 spinOffset = new Vector2(0f, -5f).RotatedBy(MathHelper.TwoPi * visualTime);
+			Vector2 trailPosition = Projectile.Center + Projectile.velocity - Main.screenPosition;
+			Color mainColor = new(77, 35, 61, 70);
+			float rotation = Projectile.velocity.ToRotation() + (float)Math.PI / 2f;
+			Main.EntitySpriteDraw(trailTexture, trailPosition + gfxOffset + spinOffset, null, mainColor, rotation, trailOrigin, 1f, SpriteEffects.None);
+			Main.EntitySpriteDraw(trailTexture, trailPosition + gfxOffset + spinOffset.RotatedBy(MathHelper.TwoPi / 3f), null, mainColor, rotation, trailOrigin, 0.6f, SpriteEffects.None);
+			Main.EntitySpriteDraw(trailTexture, trailPosition + gfxOffset + spinOffset.RotatedBy(MathHelper.TwoPi * 1.5f), null, mainColor, rotation, trailOrigin, 0.8f, SpriteEffects.None);
+			Vector2 pulsePos = Projectile.Center - Projectile.velocity * 0.5f;
+			Color pulseColor = new(255, 44, 0, 80);
+			for (float i = 0f; i < 1f; i += 0.5f) {
+				float pulseFactorFactor = visualTime % 0.5f / 0.5f;
+				pulseFactorFactor = (pulseFactorFactor + i) % 1f;
+				float pulseFactor = pulseFactorFactor * 2f;
+				if (pulseFactor > 1f)
+					pulseFactor = 2f - pulseFactor;
+
+				Main.EntitySpriteDraw(trailTexture, pulsePos - Main.screenPosition + gfxOffset, null, pulseColor * pulseFactor, rotation, trailOrigin, 0.5f + pulseFactorFactor * 0.5f, SpriteEffects.None);
+			}
+			Texture2D starTexture = TextureAssets.Projectile[Projectile.type].Value;
+			Main.EntitySpriteDraw(
+				starTexture,
+				Projectile.Center - Main.screenPosition + gfxOffset,
+				starTexture.Bounds,
+				Color.Coral * Projectile.Opacity,
+				Projectile.rotation,
+				starTexture.Size() * 0.5f,
+				Projectile.scale + 0.1f,
+				Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally
+			);
+			return false;
 		}
 	}
 }
