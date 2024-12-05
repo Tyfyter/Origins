88e636edf60c34d63a1f495b83f60a892e9e2367
Tyfyter <tyfyter2616@gmail.com>
2024-12-05 16:53:43 -0700

    0.4.2.6

--- Reflection/RandomMethods.cs
+++ Reflection/RandomMethods.cs
@@ -2,16 +2,12 @@
 using Terraria.Utilities;
 using PegasusLib;
 using PegasusLib.Reflection;
+using System.Reflection;
 
 namespace Origins.Reflection {
 	public static class RandomMethods<T> {
-		public class Loader : ReflectionLoader {
-			public override Type HostType => typeof(RandomMethods<T>);
-		}
-		static RandomMethods() {
-			new Loader().Load(Origins.instance);
-		}
-		public static FastFieldInfo<WeightedRandom<T>, double> _totalWeight;
+		static FastFieldInfo<WeightedRandom<T>, double> _totalWeight;
+		public static FastFieldInfo<WeightedRandom<T>, double> TotalWeight => _totalWeight ??= new("_totalWeight", BindingFlags.Public | BindingFlags.NonPublic);
 	}
 	public static class RandomMethods {
 		public static T Pop<T>(this WeightedRandom<T> rand) {
@@ -19,10 +15,10 @@ namespace Origins.Reflection {
 				rand.CalculateTotalWeight();
 			}
 			double num = rand.random.NextDouble();
-			num *= RandomMethods<T>._totalWeight.GetValue(rand);
+			num *= RandomMethods<T>.TotalWeight.GetValue(rand);
 			if (num <= 0) {
 				rand.elements.Clear();
-				return default(T);
+				return default;
 			}
 			for (int i = 0; i < rand.elements.Count; i++) {
 				Tuple<T, double> element = rand.elements[i];
@@ -34,7 +30,7 @@ namespace Origins.Reflection {
 				rand.needsRefresh = true;
 				return element.Item1;
 			}
-			return default(T);
+			return default;
 		}
 	}
 }
