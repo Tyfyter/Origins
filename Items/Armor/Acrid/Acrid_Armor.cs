f6282de07015f4fe9ab66faedbfc3a8d1be23955
Tyfyter <tyfyter2616@gmail.com>
2024-12-28 19:03:38 -0700

    Fixed Acrid Armor inconsistency

--- Items/Armor/Acrid/Acrid_Armor.cs
+++ Items/Armor/Acrid/Acrid_Armor.cs
@@ -10,93 +10,21 @@ using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Origins.Items.Armor.Acrid {
-    [AutoloadEquip(EquipType.Head)]
-	public class Acrid_Dome : ModItem, IWikiArmorSet, INoSeperateWikiPage {
-        public string[] Categories => [
-            "ArmorSet",
-            "ExplosiveBoostGear",
-			"ToxicSource"
-        ];
-        public override void SetStaticDefaults() {
-			Origins.AddHelmetGlowmask(this);
-			Origins.AddGlowMask(this);
-		}
-		public override void SetDefaults() {
-			Item.defense = 2;
-			Item.value = Item.sellPrice(gold: 3);
-			Item.rare = ItemRarityID.LightRed;
-		}
-		public override void UpdateEquip(Player player) {
-			player.GetCritChance(DamageClasses.Explosive) += 10;
-			player.GetModPlayer<OriginPlayer>().explosiveThrowSpeed += 0.1f;
-			if (player.wet) {
-				player.GetCritChance(DamageClasses.Explosive) += 8;
-				player.GetModPlayer<OriginPlayer>().explosiveThrowSpeed += 0.08f;
-				player.nightVision = true;
-			}
-			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
-		}
-		public override bool IsArmorSet(Item head, Item body, Item legs) {
-			return body.type == ModContent.ItemType<Acrid_Breastplate>() && legs.type == ModContent.ItemType<Acrid_Greaves>();
-		}
-		public override void UpdateArmorSet(Player player) {
-			player.GetModPlayer<OriginPlayer>().acridSet = true;
-			player.setBonus = Language.GetTextValue("Mods.Origins.SetBonuses.Acrid");
-			player.buffImmune[BuffID.Poisoned] = true;
-			player.buffImmune[BuffID.Venom] = true;
-			player.buffImmune[Toxic_Shock_Debuff.ID] = true;
-			player.AddMaxBreath(63);
-			if (Main.GameUpdateCount % 4 != 0) {
-				player.ignoreWater = true;
-			}
-		}
-		public override void AddRecipes() {
-			Recipe.Create(Type)
-			.AddIngredient(ModContent.ItemType<Eitrite_Bar>(), 13)
-			.AddIngredient(ModContent.ItemType<Rubber>(), 10)
-			.AddTile(TileID.MythrilAnvil)
-			.Register();
-		}
-		public string MergedArmorSetName => "Acrid_Armor";
-		public string ArmorSetName => "Explosive_Acrid_Armor";
-		public int HeadItemID => Type;
-		public int BodyItemID => ModContent.ItemType<Acrid_Breastplate>();
-		public int LegsItemID => ModContent.ItemType<Acrid_Greaves>();
-		public IEnumerable<int> SharedPageItems {
-			get {
-				yield return ModContent.ItemType<Acrid_Headgear>();
-				yield return ModContent.ItemType<Acrid_Helm>();
-				yield return ModContent.ItemType<Acrid_Mask>();
-				yield return ModContent.ItemType<Acrid_Visor>();
-			}
-		}
-	}
-	[AutoloadEquip(EquipType.Head)]
-	public class Acrid_Headgear : ModItem, IWikiArmorSet, INoSeperateWikiPage {
-        public string[] Categories => [
-            "ArmorSet",
-            "MagicBoostGear",
+	public abstract class Acrid_Helmet_Base : ModItem, ICustomWikiStat {
+		public string[] Categories => [
+			"ArmorSet",
+			"ExplosiveBoostGear",
 			"ToxicSource"
 		];
-        public override void SetStaticDefaults() {
+		public override void SetStaticDefaults() {
 			Origins.AddHelmetGlowmask(this);
 			Origins.AddGlowMask(this);
 		}
 		public override void SetDefaults() {
-			Item.defense = 4;
 			Item.value = Item.sellPrice(gold: 3);
 			Item.rare = ItemRarityID.LightRed;
 		}
-		public override void UpdateEquip(Player player) {
-			player.GetDamage(DamageClass.Magic) += 0.1f;
-			player.manaCost -= 0.25f;
-			if (player.wet) {
-				player.GetDamage(DamageClass.Magic) += 0.08f;
-				player.manaCost -= 0.1f;
-				player.nightVision = true;
-			}
-			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
-		}
+		public abstract override void UpdateEquip(Player player);
 		public override bool IsArmorSet(Item head, Item body, Item legs) {
 			return body.type == ModContent.ItemType<Acrid_Breastplate>() && legs.type == ModContent.ItemType<Acrid_Greaves>();
 		}
@@ -118,6 +46,54 @@ namespace Origins.Items.Armor.Acrid {
 			.AddTile(TileID.MythrilAnvil)
 			.Register();
 		}
+	}
+
+	[AutoloadEquip(EquipType.Head)]
+	public class Acrid_Dome : Acrid_Helmet_Base, IWikiArmorSet, INoSeperateWikiPage {
+		public override void SetDefaults() {
+			base.SetDefaults();
+			Item.defense = 2;
+		}
+		public override void UpdateEquip(Player player) {
+			player.GetCritChance(DamageClasses.Explosive) += 10;
+			player.GetModPlayer<OriginPlayer>().explosiveThrowSpeed += 0.1f;
+			if (player.wet) {
+				player.GetCritChance(DamageClasses.Explosive) += 8;
+				player.GetModPlayer<OriginPlayer>().explosiveThrowSpeed += 0.08f;
+				player.nightVision = true;
+			}
+			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
+		}
+		public string MergedArmorSetName => "Acrid_Armor";
+		public string ArmorSetName => "Explosive_Acrid_Armor";
+		public int HeadItemID => Type;
+		public int BodyItemID => ModContent.ItemType<Acrid_Breastplate>();
+		public int LegsItemID => ModContent.ItemType<Acrid_Greaves>();
+		public IEnumerable<int> SharedPageItems {
+			get {
+				yield return ModContent.ItemType<Acrid_Headgear>();
+				yield return ModContent.ItemType<Acrid_Helm>();
+				yield return ModContent.ItemType<Acrid_Mask>();
+				yield return ModContent.ItemType<Acrid_Visor>();
+			}
+		}
+	}
+	[AutoloadEquip(EquipType.Head)]
+	public class Acrid_Headgear : Acrid_Helmet_Base, IWikiArmorSet, INoSeperateWikiPage {
+		public override void SetDefaults() {
+			base.SetDefaults();
+			Item.defense = 4;
+		}
+		public override void UpdateEquip(Player player) {
+			player.GetDamage(DamageClass.Magic) += 0.1f;
+			player.manaCost -= 0.25f;
+			if (player.wet) {
+				player.GetDamage(DamageClass.Magic) += 0.08f;
+				player.manaCost -= 0.1f;
+				player.nightVision = true;
+			}
+			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
+		}
 		public string ArmorSetName => "Magic_Acrid_Armor";
 		public int HeadItemID => Type;
 		public int BodyItemID => ModContent.ItemType<Acrid_Breastplate>();
@@ -125,20 +101,10 @@ namespace Origins.Items.Armor.Acrid {
 		public bool SharedPageSecondary => true;
 	}
 	[AutoloadEquip(EquipType.Head)]
-	public class Acrid_Helm : ModItem, IWikiArmorSet, INoSeperateWikiPage {
-        public string[] Categories => [
-            "ArmorSet",
-            "MeleeBoostGear",
-			"ToxicSource"
-		];
-        public override void SetStaticDefaults() {
-			Origins.AddHelmetGlowmask(this);
-			Origins.AddGlowMask(this);
-		}
+	public class Acrid_Helm : Acrid_Helmet_Base, IWikiArmorSet, INoSeperateWikiPage {
 		public override void SetDefaults() {
+			base.SetDefaults();
 			Item.defense = 18;
-			Item.value = Item.sellPrice(gold: 3);
-			Item.rare = ItemRarityID.LightRed;
 		}
 		public override void UpdateEquip(Player player) {
 			player.GetDamage(DamageClass.Melee) += 0.1f;
@@ -150,27 +116,6 @@ namespace Origins.Items.Armor.Acrid {
 			}
 			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
 		}
-		public override bool IsArmorSet(Item head, Item body, Item legs) {
-			return body.type == ModContent.ItemType<Acrid_Breastplate>() && legs.type == ModContent.ItemType<Acrid_Greaves>();
-		}
-		public override void UpdateArmorSet(Player player) {
-			player.GetModPlayer<OriginPlayer>().acridSet = true;
-			player.setBonus = Language.GetTextValue("Mods.Origins.SetBonuses.Acrid");
-			player.buffImmune[BuffID.Poisoned] = true;
-			player.buffImmune[BuffID.Venom] = true;
-			player.buffImmune[Toxic_Shock_Debuff.ID] = true;
-			player.AddMaxBreath(63);
-			if (Main.GameUpdateCount % 4 != 0) {
-				player.ignoreWater = true;
-			}
-		}
-		public override void AddRecipes() {
-			Recipe.Create(Type)
-			.AddIngredient(ModContent.ItemType<Eitrite_Bar>(), 13)
-			.AddIngredient(ModContent.ItemType<Rubber>(), 10)
-			.AddTile(TileID.MythrilAnvil)
-			.Register();
-		}
 		public string ArmorSetName => "Melee_Acrid_Armor";
 		public int HeadItemID => Type;
 		public int BodyItemID => ModContent.ItemType<Acrid_Breastplate>();
@@ -178,20 +123,10 @@ namespace Origins.Items.Armor.Acrid {
 		public bool SharedPageSecondary => true;
 	}
 	[AutoloadEquip(EquipType.Head)]
-	public class Acrid_Mask : ModItem, IWikiArmorSet, INoSeperateWikiPage {
-        public string[] Categories => [
-            "ArmorSet",
-            "SummmonBoostGear",
-			"ToxicSource"
-		];
-        public override void SetStaticDefaults() {
-			Origins.AddHelmetGlowmask(this);
-			Origins.AddGlowMask(this);
-		}
+	public class Acrid_Mask : Acrid_Helmet_Base, IWikiArmorSet, INoSeperateWikiPage {
 		public override void SetDefaults() {
+			base.SetDefaults();
 			Item.defense = 1;
-			Item.value = Item.sellPrice(gold: 3);
-			Item.rare = ItemRarityID.LightRed;
 		}
 		public override void UpdateEquip(Player player) {
 			player.GetDamage(DamageClass.Summon) += 0.1f;
@@ -203,27 +138,6 @@ namespace Origins.Items.Armor.Acrid {
 			}
 			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
 		}
-		public override bool IsArmorSet(Item head, Item body, Item legs) {
-			return body.type == ModContent.ItemType<Acrid_Breastplate>() && legs.type == ModContent.ItemType<Acrid_Greaves>();
-		}
-		public override void UpdateArmorSet(Player player) {
-			player.GetModPlayer<OriginPlayer>().acridSet = true;
-			player.setBonus = Language.GetTextValue("Mods.Origins.SetBonuses.Acrid");
-			player.buffImmune[BuffID.Poisoned] = true;
-			player.buffImmune[BuffID.Venom] = true;
-			player.buffImmune[Toxic_Shock_Debuff.ID] = true;
-			player.AddMaxBreath(63);
-			if (Main.GameUpdateCount % 4 != 0) {
-				player.ignoreWater = true;
-			}
-		}
-		public override void AddRecipes() {
-			Recipe.Create(Type)
-			.AddIngredient(ModContent.ItemType<Eitrite_Bar>(), 13)
-			.AddIngredient(ModContent.ItemType<Rubber>(), 10)
-			.AddTile(TileID.MythrilAnvil)
-			.Register();
-		}
 		public string ArmorSetName => "Summon_Acrid_Armor";
 		public int HeadItemID => Type;
 		public int BodyItemID => ModContent.ItemType<Acrid_Breastplate>();
@@ -231,20 +145,10 @@ namespace Origins.Items.Armor.Acrid {
 		public bool SharedPageSecondary => true;
 	}
 	[AutoloadEquip(EquipType.Head)]
-	public class Acrid_Visor : ModItem, IWikiArmorSet, INoSeperateWikiPage {
-        public string[] Categories => [
-            "ArmorSet",
-            "RangedBoostGear",
-			"ToxicSource"
-		];
-        public override void SetStaticDefaults() {
-			Origins.AddHelmetGlowmask(this);
-			Origins.AddGlowMask(this);
-		}
+	public class Acrid_Visor : Acrid_Helmet_Base, IWikiArmorSet, INoSeperateWikiPage {
 		public override void SetDefaults() {
+			base.SetDefaults();
 			Item.defense = 9;
-			Item.value = Item.sellPrice(gold: 3);
-			Item.rare = ItemRarityID.LightRed;
 		}
 		public override void UpdateEquip(Player player) {
 			player.GetAttackSpeed(DamageClass.Ranged) += 0.2f;
@@ -253,27 +157,6 @@ namespace Origins.Items.Armor.Acrid {
 				player.nightVision = true;
 			}
 			Lighting.AddLight(player.Center, new Vector3(0, 1, (float)Math.Abs(Math.Sin(Main.GameUpdateCount / 60f))));
-		}
-		public override bool IsArmorSet(Item head, Item body, Item legs) {
-			return body.type == ModContent.ItemType<Acrid_Breastplate>() && legs.type == ModContent.ItemType<Acrid_Greaves>();
-		}
-		public override void UpdateArmorSet(Player player) {
-			player.GetModPlayer<OriginPlayer>().acridSet = true;
-			player.setBonus = Language.GetTextValue("Mods.Origins.SetBonuses.Acrid");
-			player.buffImmune[BuffID.Poisoned] = true;
-			player.buffImmune[BuffID.Venom] = true;
-			player.buffImmune[Toxic_Shock_Debuff.ID] = true;
-			player.AddMaxBreath(63);
-			if (Main.GameUpdateCount % 4 != 0) {
-				player.ignoreWater = true;
-			}
-		}
-		public override void AddRecipes() {
-			Recipe.Create(Type)
-			.AddIngredient(ModContent.ItemType<Eitrite_Bar>(), 13)
-			.AddIngredient(ModContent.ItemType<Rubber>(), 10)
-			.AddTile(TileID.MythrilAnvil)
-			.Register();
 		}
 		public string ArmorSetName => "Ranged_Acrid_Armor";
 		public int HeadItemID => Type;
