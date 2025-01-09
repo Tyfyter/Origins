a43ed49bf1fa2e55ed14fbecd2d081513d475eec
Tyfyter <tyfyter2616@gmail.com>
2025-01-09 13:11:43 -0700

    Organization

---
+++ Dev/ItemWikiProvider.cs
@@ -0,0 +1,370 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json.Linq;
+using Origins.Items.Weapons.Ammo;
+using Origins.Items.Weapons.Ammo.Canisters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Origins.Dev.WikiPageExporter;
+
+namespace Origins.Dev {
+	public class ItemWikiProvider : WikiProvider<ModItem> {
+		public override string PageName(ModItem modItem) {
+			if (modItem.Item.rare == CursedRarity.ID) {
+				return WikiPageExporter.GetWikiName(modItem) + " (Cursed)";
+			}
+			return WikiPageExporter.GetWikiName(modItem);
+		}
+		public override (PageTemplate template, Dictionary<string, object> context) GetPage(ModItem modItem) {
+			Item item = modItem.Item;
+			Dictionary<string, object> context = new() {
+				["Name"] = WikiPageExporter.GetWikiName(modItem),
+				["DisplayName"] = Lang.GetItemNameValue(item.type)
+			};
+			(List<Recipe> recipes, List<Recipe> usedIn) = WikiExtensions.GetRecipes(item);
+			if (recipes.Count > 0 || usedIn.Count > 0) {
+				context["Crafting"] = true;
+				if (recipes.Count > 0) {
+					context["Recipes"] = recipes;
+				}
+				if (usedIn.Count > 0) {
+					context["UsedIn"] = usedIn;
+				}
+			}
+
+			IEnumerable<(string name, LocalizedText text)> pageTexts;
+			if (modItem is ICustomWikiStat wikiStats) {
+				context["PageTextMain"] = wikiStats.PageTextMain.Value;
+				pageTexts = wikiStats.PageTexts;
+			} else {
+				string key = $"WikiGenerator.{modItem.Mod?.Name}.{modItem.LocalizationCategory}.{context["Name"]}.MainText";
+				context["PageTextMain"] = Language.GetOrRegister(key).Value;
+				pageTexts = WikiPageExporter.GetDefaultPageTexts(modItem);
+			}
+			foreach (var text in pageTexts) {
+				context[text.name] = text.text;
+			}
+			return (WikiTemplate, context);
+		}
+		static Dictionary<int, string> sourceCache = null;
+		static void GenerateSourceCache() {
+			if (sourceCache is not null) return;
+			Dictionary<int, StringBuilder> cache = [];
+			StringBuilder GetBuilder(int type) {
+				if (!cache.TryGetValue(type, out StringBuilder builder)) cache[type] = builder = new();
+				return builder;
+			}
+			for (int i = 0; i < Main.recipe.Length; i++) {
+				if (Main.recipe[i].createItem.ModItem?.Mod is Origins) {
+					StringBuilder sources = GetBuilder(Main.recipe[i].createItem.type);
+					RecipeRequirements reqs = new(Main.recipe[i]);
+					string reqsText = reqs.requirements.Length > 0 ? $"(@{string.Join(", ", reqs.requirements.Select(r => r.ToString()))})" : "";
+					if (sources.Length > 0) sources.Append("<div class=divider></div>");
+					sources.Append($"{string.Join("+", Main.recipe[i].requiredItem.Select(i => (i.stack > 1 ? i.stack.ToString() : "") + WikiExtensions.GetItemText(ContentSamples.ItemsByType[i.type], imageOnly: true)))}{reqsText}");
+				}
+			}
+			foreach (AbstractNPCShop shop in NPCShopDatabase.AllShops) {
+				if (ShopTypes.TryGetValue(shop.GetType(), out var func)) {
+					foreach (AbstractNPCShop.Entry entry in func(shop)) {
+						if (entry.Item.ModItem?.Mod is Origins) {
+							StringBuilder sources = GetBuilder(entry.Item.type);
+							if (sources.Length > 0) sources.Append("<div class=divider></div>");
+							sources.Append($"{WikiExtensions.GetNPCText(ContentSamples.NpcsByNetId[shop.NpcType])} (<a-coins>{entry.Item.GetStoreValue()}</a-coins>{string.Join(", ", entry.Conditions.Select(c => c.Description.Value))})");
+						}
+					}
+				}
+			}
+			for (int i = 0; i < ItemID.Sets.ShimmerTransformToItem.Length; i++) {
+				if (ItemLoader.GetItem(ItemID.Sets.ShimmerTransformToItem[i])?.Mod is Origins) {
+					StringBuilder sources = GetBuilder(ItemID.Sets.ShimmerTransformToItem[i]);
+					if (sources.Length > 0) sources.Append("<div class=divider></div>");
+					sources.Append(WikiExtensions.GetItemText(ContentSamples.ItemsByType[i]));
+					sources.Append(" (@ <a is=a-link href=https://terraria.wiki.gg/wiki/Shimmer>Shimmer</a>)");
+				}
+			}
+			for (int i = 0; i < ItemLoader.ItemCount; i++) {
+				List<IItemDropRule> rules = Main.ItemDropsDB.GetRulesForItemID(i);
+				if (rules?.Count > 0) {
+					List<DropRateInfo> drops = [];
+					for (int j = 0; j < rules.Count; j++) {
+						DropRateInfoChainFeed ratesInfo = new(1f);
+						try {
+							rules[j].ReportDroprates(drops, ratesInfo);
+						} catch (Exception) {}
+					}
+					for (int j = 0; j < drops.Count; j++) {
+						if (ItemLoader.GetItem(drops[j].itemId)?.Mod is not Origins) continue;
+						StringBuilder sources = GetBuilder(drops[j].itemId);
+						if (sources.Length > 0) sources.Append("<div class=divider></div>");
+						sources.Append(WikiExtensions.GetItemText(ContentSamples.ItemsByType[i]));
+						if ((drops[j].conditions?.Count ?? 0) > 0) sources.Append(" " + string.Join(", ", drops[j].conditions.Select(r => r.ToString())));
+					}
+				}
+			}
+			for (int i = 0; i < NPCLoader.NPCCount; i++) {
+				List<IItemDropRule> rules = Main.ItemDropsDB.GetRulesForNPCID(i);
+				if (rules?.Count > 0) {
+					List<DropRateInfo> drops = [];
+					for (int j = 0; j < rules.Count; j++) {
+						DropRateInfoChainFeed ratesInfo = new(1f);
+						try {
+							rules[j].ReportDroprates(drops, ratesInfo);
+						} catch (Exception) {}
+					}
+					for (int j = 0; j < drops.Count; j++) {
+						if (ItemLoader.GetItem(drops[j].itemId)?.Mod is not Origins) continue;
+						StringBuilder sources = GetBuilder(drops[j].itemId);
+						if (sources.Length > 0) sources.Append("<div class=divider></div>");
+						sources.Append(WikiExtensions.GetNPCText(ContentSamples.NpcsByNetId[i]));
+						if ((drops[j].conditions?.Count ?? 0) > 0) sources.Append(" " + string.Join(", ", drops[j].conditions.Select(r => r.ToString())));
+					}
+				}
+			}
+
+			sourceCache = new(cache.Select(kvp => new KeyValuePair<int, string>(kvp.Key, kvp.Value.ToString())));
+		}
+		public override IEnumerable<(string, JObject)> GetStats(ModItem modItem) {
+			Item item = modItem.Item;
+			JObject data = [];
+			ICustomWikiStat customStat = item.ModItem as ICustomWikiStat;
+			data["Image"] = customStat?.CustomSpritePath ?? WikiPageExporter.GetWikiItemImagePath(modItem);
+			data["Name"] = item.Name;
+			JArray types = new("Item");
+			if (customStat is not null) foreach (string cat in customStat.Categories) types.Add(cat);
+			if (item.pick != 0 || item.axe != 0 || item.hammer != 0 || item.fishingPole != 0 || item.bait != 0) types.Add("Tool");
+			if (item.accessory) types.Add("Accessory");
+			if (item.damage > 0 && item.useStyle != ItemUseStyleID.None && (!types.Any(t => t.ToString() == "Tool") || types.Any(t => t.ToString() == "ToolWeapon"))) {
+				types.Add("Weapon");
+				WeaponTypes weaponType = WeaponTypes.None;
+				for (int i = 0; i < types.Count; i++) {
+					if (Enum.TryParse(types[i].ToString(), out WeaponTypes type)) {
+						weaponType = type;
+						break;
+					}
+				}
+				if (weaponType == WeaponTypes.None) {
+					if (item.CountsAsClass(DamageClasses.Explosive)) {
+						if (item.useAmmo == ModContent.ItemType<Resizable_Mine_One>()) {
+							weaponType = WeaponTypes.CanisterLauncher;
+						} else if (item.CountsAsClass(DamageClasses.ThrownExplosive)) {
+							weaponType = WeaponTypes.ThrownExplosive;
+						} else {
+							weaponType = WeaponTypes.OtherExplosive;
+						}
+					}
+					if (weaponType == WeaponTypes.None && item.shoot > ProjectileID.None) {
+						switch (ContentSamples.ProjectilesByType[item.shoot].aiStyle) {
+							case ProjAIStyleID.Boomerang:
+							weaponType = WeaponTypes.Boomerang;
+							break;
+
+							case ProjAIStyleID.Yoyo:
+							weaponType = WeaponTypes.Yoyo;
+							break;
+						}
+						if (ItemID.Sets.Spears[item.type]) {
+							weaponType = WeaponTypes.Spear;
+						}
+
+						if (item.CountsAsClass(DamageClass.Summon) && !types.Any(t => t.ToString() == "Incantation")) {
+							if (Origins.ArtifactMinion[item.shoot]) weaponType = WeaponTypes.Artifact;
+							if (ProjectileID.Sets.IsAWhip[item.shoot]) {
+								weaponType = WeaponTypes.Whip;
+							} else if (item.CountsAsClass<Incantation>()) {
+								weaponType = WeaponTypes.Incantation;
+							} else {
+								Projectile proj = ContentSamples.ProjectilesByType[item.shoot];
+								if (proj.minion) weaponType = WeaponTypes.Minion;
+								else if (proj.sentry) weaponType = WeaponTypes.Sentry;
+								else weaponType = WeaponTypes.OtherSummon;
+							}
+						}
+					}
+					if (weaponType == WeaponTypes.None && item.CountsAsClass(DamageClass.Magic)) {
+						if (Item.staff[item.type]) weaponType = WeaponTypes.Wand;
+						else weaponType = WeaponTypes.OtherMagic;
+					}
+					if (weaponType == WeaponTypes.None && item.CountsAsClass(DamageClass.Melee)) weaponType = WeaponTypes.OtherMelee;
+					if (weaponType == WeaponTypes.None) {
+						switch (item.useAmmo) {
+							case ItemID.WoodenArrow:
+							weaponType = WeaponTypes.Bow;
+							break;
+							case ItemID.MusketBall:
+							weaponType = WeaponTypes.Gun;
+							break;
+
+							default:
+							if (item.useAmmo == ModContent.ItemType<Metal_Slug>()) {
+								weaponType = WeaponTypes.Handcannon;
+							} else if (item.useAmmo == ModContent.ItemType<Harpoon>()) {
+								weaponType = WeaponTypes.HarpoonGun;
+							} else if (item.useAmmo == AmmoID.Rocket) {
+								weaponType = WeaponTypes.RocketLauncher;
+							} else if (item.useAmmo == ModContent.ItemType<Resizable_Mine_One>()) {
+								weaponType = WeaponTypes.CanisterLauncher;
+							}
+							break;
+						}
+					}
+					if (weaponType == WeaponTypes.None && item.CountsAsClass(DamageClass.Ranged)) {
+						weaponType = WeaponTypes.OtherRanged;
+					}
+					if (weaponType == WeaponTypes.None && !item.noMelee && item.useStyle == ItemUseStyleID.Swing) weaponType = WeaponTypes.Sword;
+				}
+				if (weaponType != WeaponTypes.None && !types.Any(t => t.ToString() == weaponType.ToString())) types.Add(weaponType.ToString());
+			}
+			switch (item.ammo) {
+				case ItemID.WoodenArrow:
+				types.Add("Arrow");
+				break;
+				case ItemID.MusketBall:
+				types.Add("Bullet");
+				break;
+
+				default:
+				if (item.ammo == ModContent.ItemType<Harpoon>()) types.Add("Harpoon");
+				break;
+			}
+			if (customStat?.Hardmode ?? (!item.consumable && item.rare > ItemRarityID.Orange)) types.Add("Hardmode");
+
+			if (ItemID.Sets.IsFood[item.type]) types.Add("Food");
+			if (item.ammo != 0 && item.ammo != ItemID.CopperOre) types.Add("Ammo");
+			if (item.headSlot != -1 || item.bodySlot != -1 || item.legSlot != -1) types.Add("Armor");
+			if (item.createTile != -1) {
+				types.Add("Tile");
+				if (TileID.Sets.Torch[item.createTile]) types.Add("Torch");
+				if (TileID.Sets.Ore[item.createTile]) types.Add("Ore");
+			}
+			if (item.expert) types.Add("Expert");
+			if (item.master) types.Add("Master");
+			data.Add("Types", types);
+
+			data.AppendStat("PickPower", item.pick, 0);
+			data.AppendStat("AxePower", item.axe * 5, 0);
+			data.AppendStat("HammerPower", item.hammer, 0);
+			data.AppendStat("FishPower", item.fishingPole, 0);
+			data.AppendStat("BaitPower", item.bait, 0);
+			data.AppendStat("ReachBonus", item.tileBoost, 0);
+
+			if (item.createTile != -1) {
+				ModTile tile = TileLoader.GetTile(item.createTile);
+				if (tile is not null) {
+					data.AppendStat(Main.tileHammer[item.createTile] ? "HammerReq" : "PickReq", tile.MinPick, 0);
+				}
+				int width = 1, height = 1;
+				if (TileObjectData.GetTileData(item.createTile, item.placeStyle) is TileObjectData tileData) {
+					width = tileData.Width;
+					height = tileData.Height;
+				}
+				data.Add("PlacementSize", new JArray(width, height));
+			}
+			data.AppendStat("Defense", item.defense, 0);
+			if (item.headSlot != -1) {
+				data.Add("ArmorSlot", "Helmet");
+			} else if (item.bodySlot != -1) {
+				data.Add("ArmorSlot", "Shirt");
+			} else if (item.legSlot != -1) {
+				data.Add("ArmorSlot", "Pants");
+			}
+			data.AppendStat("ManaCost", item.mana, 0);
+			data.AppendStat("HealLife", item.healLife, 0);
+			data.AppendStat("HealMana", item.healMana, 0);
+			data.AppendStat("Damage", item.damage, -1);
+			data.AppendStat("ArmorPenetration", item.ArmorPenetration, 0);
+			if (item.damage > 0) {
+				string damageClass = item.DamageType.DisplayName.Value;
+				damageClass = damageClass.Replace(" damage", "").Trim();
+				damageClass = Regex.Replace(damageClass, "( |^)(\\w)", (match) => match.Groups[1].Value + match.Groups[2].Value.ToUpper());
+				data.Add("DamageClass", damageClass);
+			}
+			data.AppendStat("Knockback", item.knockBack, 0);
+			data.AppendStat("Crit", item.crit + 4, 4);
+			data.AppendStat("UseTime", item.useTime, 100);
+			data.AppendStat("Velocity", item.shootSpeed, 0);
+			if (item.ToolTip.Lines > 1) {
+				JArray itemTooltip = [];
+				for (int i = 0; i < item.ToolTip.Lines; i++) {
+					string line = item.ToolTip.GetLine(i);
+					if (!string.IsNullOrWhiteSpace(line)) itemTooltip.Add(line);
+				}
+				data.AppendJStat("Tooltip", itemTooltip, []);
+			} else if (item.ToolTip.Lines > 0) {
+				data.AppendStat("Tooltip", item.ToolTip.GetLine(0), string.Empty);
+			}
+			data.AppendStat("Rarity", WikiPageExporter.GetWikiItemRarity(item), "");
+			if (customStat?.Buyable ?? false) data.AppendStat("Buy", item.value, 0);
+			data.AppendStat("Sell", item.value / 5, 0);
+			data.AppendJStat("Drops", new JArray().FillWithLoot(Main.ItemDropsDB.GetRulesForItemID(item.type).GetDropRates()), []);
+			{
+				string baseKey = $"WikiGenerator.Stats.{modItem.Mod?.Name}.{modItem.Name}.";
+				string key = baseKey + "Source";
+				if (Language.Exists(key)) data.AppendStat("Source", Language.GetTextValue(key), key);
+				else {
+					GenerateSourceCache();
+					if (sourceCache.TryGetValue(item.type, out string source)) data.AppendStat("Source", source, "");
+				}
+
+				key = baseKey + "Effect";
+				if (Language.Exists(key)) data.AppendStat("Effect", Language.GetTextValue(key), key);
+
+			}
+			if (customStat is not null) {
+				string baseKey = $"WikiGenerator.Stats.{modItem.Mod?.Name}.{modItem.Name}.";
+				foreach (string stat in customStat.LocalizedStats) {
+					data.AppendStat(stat, Language.GetOrRegister(baseKey + stat).Value, "");
+				}
+				customStat?.ModifyWikiStats(data);
+			}
+			data.AppendStat("SpriteWidth", item.ModItem is null ? item.width : ModContent.Request<Texture2D>(item.ModItem.Texture).Width(), 0);
+			data.AppendStat("InternalName", item.ModItem?.Name, null);
+			yield return (customStat?.CustomStatPath ?? PageName(modItem), data);
+			if (item.makeNPC > 0 && NPCLoader.GetNPC(item.makeNPC) is ModNPC modNPC) {
+				yield return ((customStat?.CustomStatPath ?? PageName(modItem)) + "_NPC", NPCWikiProvider.GetNPCStats(modNPC));
+			}
+		}
+		public override IEnumerable<(string, (Texture2D, int)[])> GetAnimatedSprites(ModItem value) {
+			if (Main.itemAnimations[value.Type] is DrawAnimation animation) {
+				yield return (WikiPageExporter.GetWikiName(value), SpriteGenerator.GenerateAnimationSprite(TextureAssets.Item[value.Type].Value, animation));
+			}
+			yield break;
+		}
+	}
+	public enum WeaponTypes {
+		None,
+		Sword,
+		Spear,
+		Boomerang,
+		Yoyo,
+		Bow,
+		Gun,
+		HarpoonGun,
+		Wand,
+		MagicGun,
+		SpellBook,
+		Artifact,
+		Minion,
+		Sentry,
+		Whip,
+		Incantation,
+		ThrownExplosive,
+		Handcannon,
+		RocketLauncher,
+		CanisterLauncher,
+		OtherMelee,
+		OtherRanged,
+		OtherMagic,
+		OtherSummon,
+		OtherExplosive
+	}
+}
---
+++ Dev/NPCWikiProvider.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json.Linq;
+using Origins.NPCs;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Origins.Dev.WikiPageExporter;
+
+namespace Origins.Dev {
+	public class NPCWikiProvider : WikiProvider<ModNPC> {
+		public override string PageName(ModNPC modNPC) => WikiPageExporter.GetWikiName(modNPC);
+		public override (PageTemplate template, Dictionary<string, object> context) GetPage(ModNPC modNPC) {
+			NPC npc = modNPC.NPC;
+			Dictionary<string, object> context = new() {
+				["Name"] = WikiPageExporter.GetWikiName(modNPC),
+				["DisplayName"] = Lang.GetNPCNameValue(npc.type)
+			};
+
+			IEnumerable<(string name, LocalizedText text)> pageTexts;
+			if (modNPC is ICustomWikiStat wikiStats) {
+				context["PageTextMain"] = wikiStats.PageTextMain.Value;
+				pageTexts = wikiStats.PageTexts;
+			} else {
+				string key = $"WikiGenerator.{modNPC.Mod?.Name}.{modNPC.LocalizationCategory}.{context["Name"]}.MainText";
+				context["PageTextMain"] = Language.GetOrRegister(key).Value;
+				pageTexts = WikiPageExporter.GetDefaultPageTexts(modNPC);
+			}
+			foreach (var text in pageTexts) {
+				context[text.name] = text.text;
+			}
+			return (WikiTemplate, context);
+		}
+		public static JObject GetNPCStats(ModNPC modNPC) {
+			NPC npc = modNPC.NPC;
+			JObject data = [];
+			ICustomWikiStat customStat = modNPC as ICustomWikiStat;
+			data["Image"] = customStat?.CustomSpritePath ?? (WikiPageExporter.GetWikiName(modNPC));
+			data["Name"] = npc.TypeName;
+			JArray types = new("NPC");
+			if (npc.boss || NPCID.Sets.ShouldBeCountedAsBoss[npc.type]) types.Add("Boss");
+			if (customStat is not null) foreach (string cat in customStat.Categories) types.Add(cat);
+			data.Add("Types", types);
+
+			bool getGoodWorld = Main.getGoodWorld;
+			Main.getGoodWorld = false;
+			int gameMode = Main.GameMode;
+			JObject expertData = [];
+			JObject masterData = [];
+			NPC tempInstance = new();
+			bool dontDoHardmodeScaling = NPCID.Sets.DontDoHardmodeScaling[npc.type];
+			try {
+				NPCID.Sets.DontDoHardmodeScaling[npc.type] = true;
+				Main.GameMode = GameModeID.Normal;
+				tempInstance.SetDefaults(npc.netID, new NPCSpawnParams {
+					gameModeData = Main.GameModeInfo,
+					playerCountForMultiplayerDifficultyOverride = 1
+				});
+				data.AppendStat("MaxLife", tempInstance.lifeMax, 0);
+				data.AppendStat("Defense", tempInstance.defense, 0);
+				data.AppendStat("KBResist", 1 - tempInstance.knockBackResist, 0);
+				data.AppendJStat("Immunities", tempInstance.GetImmunities(), []);
+				data.AppendStat("Coins", tempInstance.value, 0);
+				data.Add("Drops", new JArray().FillWithLoot(Main.ItemDropsDB.GetRulesForNPCID(npc.type, false).GetDropRates()));
+
+				Main.GameMode = GameModeID.Expert;
+				_ = Main.expertMode;
+				tempInstance.SetDefaults(npc.netID, new NPCSpawnParams {
+					gameModeData = Main.GameModeInfo,
+					playerCountForMultiplayerDifficultyOverride = 1
+				});
+				expertData.AppendAltStat(data, "MaxLife", tempInstance.lifeMax);
+				expertData.AppendAltStat(data, "Defense", tempInstance.defense);
+				expertData.AppendAltStat(data, "KBResist", 1 - tempInstance.knockBackResist);
+				expertData.AppendAltStat(data, "Immunities", tempInstance.GetImmunities());
+				expertData.AppendAltStat(data, "Coins", tempInstance.value);
+				expertData.Add("Drops", new JArray().FillWithLoot(Main.ItemDropsDB.GetRulesForNPCID(npc.type, false).GetDropRates()));
+
+				Main.GameMode = GameModeID.Master;
+				tempInstance.SetDefaults(npc.netID, new NPCSpawnParams {
+					gameModeData = Main.GameModeInfo,
+					playerCountForMultiplayerDifficultyOverride = 1
+				});
+				masterData.AppendAltStat(data, "MaxLife", tempInstance.lifeMax);
+				masterData.AppendAltStat(data, "Defense", tempInstance.defense);
+				masterData.AppendAltStat(data, "KBResist", 1 - tempInstance.knockBackResist);
+				masterData.AppendAltStat(data, "Immunities", tempInstance.GetImmunities());
+				masterData.AppendAltStat(data, "Coins", tempInstance.value);
+				masterData.Add("Drops", new JArray().FillWithLoot(Main.ItemDropsDB.GetRulesForNPCID(npc.type, false).GetDropRates()));
+			} finally {
+				Main.GameMode = gameMode;
+				Main.getGoodWorld = getGoodWorld;
+				NPCID.Sets.DontDoHardmodeScaling[npc.type] = dontDoHardmodeScaling;
+			}
+			for (int i = 0; i < BiomeNPCGlobals.assimilationProviders.Count; i++) {
+				if (npc.TryGetGlobalNPC((GlobalNPC)BiomeNPCGlobals.assimilationProviders[i], out GlobalNPC gNPC) && gNPC is IAssimilationProvider assimilationProvider) {
+					AssimilationAmount amount = assimilationProvider.GetAssimilationAmount(npc);
+					if (amount != default) {
+						string assName = assimilationProvider.AssimilationName;
+						if (amount.Function is not null) {
+							data.Add(assName, "variable");
+							continue;
+						}
+						data.Add(assName, amount.ClassicAmount);
+						if (amount.ExpertAmount.HasValue) expertData.Add(assName, amount.ExpertAmount.Value);
+						if (amount.MasterAmount.HasValue) masterData.Add(assName, amount.MasterAmount.Value);
+					}
+				}
+			}
+			data.AppendJStat("Expert", expertData, []);
+			data.AppendJStat("Master", masterData, []);
+
+			customStat?.ModifyWikiStats(data);
+			if (!data.ContainsKey("SpriteWidth")) data.AppendStat("SpriteWidth", modNPC is null ? npc.width : ModContent.Request<Texture2D>(modNPC.Texture).Width(), 0);
+			if (!data.ContainsKey("InternalName")) data.AppendStat("InternalName", modNPC?.Name, null);
+			return data;
+		}
+		public override IEnumerable<(string, JObject)> GetStats(ModNPC modNPC) {
+			NPC npc = modNPC.NPC;
+			if (npc.catchItem > 0) yield break;
+			string segmentText = "";
+			if (modNPC is WormBody) {
+				segmentText = "_Body";
+			} else if (modNPC is WormTail) {
+				segmentText = "_Tail";
+			}
+			yield return (((modNPC as ICustomWikiStat)?.CustomStatPath ?? PageName(modNPC)) + segmentText, GetNPCStats(modNPC));
+		}
+		public override IEnumerable<(string, (Texture2D, int)[])> GetAnimatedSprites(ModNPC modNPC) {
+			if (modNPC is not IWikiNPC wikiNPC) yield break;
+			yield return (WikiPageExporter.GetWikiName(modNPC), SpriteGenerator.GenerateAnimationSprite(modNPC.NPC, wikiNPC.DrawRect, wikiNPC.AnimationFrames));
+		}
+	}
+}
