007ed267e063d13610e0ed73b6147b0ec26414b3
Tyfyter <tyfyter2616@gmail.com>
2025-02-08 16:37:22 -0700

    Create Rockstar_Collar.cs

---
+++ Items/Accessories/Rockstar_Collar.cs
@@ -0,0 +1,41 @@
+﻿using Origins.Dev;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+namespace Origins.Items.Accessories {
+	public class Rockstar_Collar : ModItem, ICustomWikiStat {
+		public string[] Categories => [
+			"Combat"
+		];
+		public override LocalizedText Tooltip => OriginExtensions.CombineTooltips(
+				Language.GetOrRegister("ItemTooltip.SharkToothNecklace"),
+				Language.GetOrRegister($"Mods.Origins.Items.{nameof(Lightning_Ring)}.Tooltip"),
+				Language.GetOrRegister($"Mods.Origins.Items.{nameof(Makeover_Choker)}.Tooltip")
+			);
+		public override void SetDefaults() {
+			Item.DefaultToAccessory(18, 22);
+			Item.value = Item.sellPrice(gold: 2);
+			Item.rare = ItemRarityID.Orange;
+		}
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+			player.OriginPlayer().lightningRing = true;
+			player.GetArmorPenetration(DamageClass.Generic) += 5;
+			player.longInvince = true;
+			Mysterious_Spray.EquippedEffect(player);
+			if (!hideVisual) {
+				Mysterious_Spray.VanityEffect(player);
+			}
+		}
+		public override void UpdateVanity(Player player) {
+			Mysterious_Spray.VanityEffect(player);
+		}
+		public override void AddRecipes() {
+			CreateRecipe()
+			.AddIngredient<Rockstar_Collar>()
+			.AddIngredient<Makeover_Choker>()
+			.AddTile(TileID.TinkerersWorkbench)
+			.Register();
+		}
+	}
+}
