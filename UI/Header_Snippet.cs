9d3b7e59af6869ae05fa2a3f789446257a242e20
Tyfyter <tyfyter2616@gmail.com>
2025-03-09 13:56:18 -0600

    for some reason they needed two spaces for a space

--- UI/Header_Snippet.cs
+++ UI/Header_Snippet.cs
@@ -3,9 +3,7 @@ using Terraria.UI.Chat;
 
 namespace Origins.UI {
 	public class Header_Snippet_Handler : ITagHandler {
-		public class Header_Snippet : TextSnippet {
-			public Header_Snippet(string text, Color color = default) : base(text, color, 1.1f) {}
-		}
+		public class Header_Snippet(string text, Color color = default) : TextSnippet(text.Replace(" ", "  "), color, 1.1f) { }
 		public TextSnippet Parse(string text, Color baseColor = default(Color), string options = null) {
 			return new Header_Snippet(text, baseColor);
 		}
