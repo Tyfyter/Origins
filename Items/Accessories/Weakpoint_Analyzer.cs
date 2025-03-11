0218240f07b342f3ecdd43ee0aeac7011b325ba3
chrersis <66661847+chrersis@users.noreply.github.com>
2025-03-11 11:36:17 -0700

    Journal Image Improvements 2/2

--- Items/Accessories/Weakpoint_Analyzer.cs
+++ Items/Accessories/Weakpoint_Analyzer.cs
@@ -1,13 +1,18 @@
 ﻿using Origins.Dev;
+using Origins.Journal;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 namespace Origins.Items.Accessories {
-	public class Weakpoint_Analyzer : ModItem, ICustomWikiStat {
+	public class Weakpoint_Analyzer : ModItem, ICustomWikiStat, IJournalEntrySource {
 		public string[] Categories => [
 			"Combat",
 			"RangedBoostAcc"
 		];
+		public string EntryName => "Origins/" + typeof(Weakpoint_Analyzer_Entry).Name;
+		public class Weakpoint_Analyzer_Entry : JournalEntry {
+			public override string TextKey => "Weakpoint_Analyzer";
+		}
 		public override void SetDefaults() {
 			Item.DefaultToAccessory(14, 28);
 			Item.value = Item.sellPrice(gold: 2);
