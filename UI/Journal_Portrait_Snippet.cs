7b20a826858809500e7c5c450fec6495dcb46b41
Tyfyter <tyfyter2616@gmail.com>
2025-03-10 21:08:09 -0600

    snippet improvements

--- UI/Journal_Portrait_Snippet.cs
+++ UI/Journal_Portrait_Snippet.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using PegasusLib.Graphics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System;
 using System.Text.RegularExpressions;
 using Terraria;
 using Terraria.GameContent.Bestiary;
@@ -80,35 +83,51 @@ namespace Origins.UI {
 			}
 		}
 		public record struct Options(bool Sketch = false, float Sharpness = 1, Color Color = default, float Width = 230f, float Height = 112f);
-		public TextSnippet Parse(string text, Color baseColor = default, string options = null) {
-			Regex regex = new("(?:(s[\\d\\.]+)|(d)|(w[\\d\\.]+)|(h[\\d\\.]+))+");
-			Options settings = new(Color: baseColor);
-			bool first = true;
-			foreach (Group group in regex.Match(options).Groups.Values) {
-				if (first) {
-					first = false;
-					continue;
-				}
-				if (group.Value.Length <= 0) continue;
-				switch (group.Value[0]) {
-					///sharpness
-					case 's':
-					settings.Sharpness = float.Parse(group.Value[1..]);
-					break;
-					///sketch instead of full color
-					case 'd':
-					settings.Sketch = true;
-					break;
-					///width
-					case 'w':
-					settings.Width = float.Parse(group.Value[1..]);
-					break;
-					///height
-					case 'h':
-					settings.Height = float.Parse(group.Value[1..]);
-					break;
-				}
+		record struct SnippetOption(string Name, [StringSyntax(StringSyntaxAttribute.Regex)] string Data, Action<string> Action) {
+			public readonly string Pattern => Name + Data;
+			public static SnippetOption CreateColorOption(string name, Action<Color> setter) {
+				return new(name, "[\\da-fA-F]{3,8}", match => {
+					int Parse(int index, int size) {
+						int startIndex = (index * size);
+						return Convert.ToInt32(match[startIndex..(startIndex + size)], 16);
+					}
+					switch (match.Length) {
+						case 8:
+						setter(new Color(Parse(0, 2), Parse(1, 2), Parse(2, 2), Parse(3, 2)));
+						break;
+						case 6:
+						setter(new Color(Parse(0, 2), Parse(1, 2), Parse(2, 2)));
+						break;
+						case 4:
+						setter(new Color(Parse(0, 1) * 16 - 1, Parse(1, 1) * 16 - 1, Parse(2, 1) * 16 - 1, Parse(3, 1) * 16 - 1));
+						break;
+						case 3:
+						setter(new Color(Parse(0, 1) * 16 - 1, Parse(1, 1) * 16 - 1, Parse(2, 1) * 16 - 1));
+						break;
+						default:
+						throw new FormatException($"Malformed color code {match}");
+					}
+				});
 			}
+		}
+		static void ParseOptions(string optionsText, params SnippetOption[] options) {
+			Regex regex = new($"(?:{string.Join("|", options.Select(so => $"({so.Pattern})"))})+");
+			GroupCollection groups = regex.Match(optionsText).Groups;
+			for (int i = 0; i < groups.Count - 1; i++) {
+				string match = groups[i + 1].Value;
+				if (match.Length <= 0) continue;
+				options[i].Action(match[options[i].Name.Length..]);
+			}
+		}
+		public TextSnippet Parse(string text, Color baseColor = default, string options = null) {
+			Options settings = new(Color: baseColor);
+			ParseOptions(options,
+				new SnippetOption("w", "[\\d\\.]+", match => settings.Width = float.Parse(match)),
+				new SnippetOption("h", "[\\d\\.]+", match => settings.Height = float.Parse(match)),
+				new SnippetOption("s", "[\\d\\.]+", match => settings.Sharpness = float.Parse(match)),
+				new SnippetOption("d", "", match => settings.Sketch = true),
+				SnippetOption.CreateColorOption("c", value => settings.Color = value)
+			);
 			return new Journal_Portrait_Snippet(text, settings);
 		}
 	}
