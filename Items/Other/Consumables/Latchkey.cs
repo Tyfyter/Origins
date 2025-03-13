3a7718131ca06fde84e815b7199896c9d33cd8ad
Tyfyter <tyfyter2616@gmail.com>
2025-03-13 13:49:57 -0600

    Latchkey now smears you

--- Items/Other/Consumables/Latchkey.cs
+++ Items/Other/Consumables/Latchkey.cs
@@ -1,16 +1,23 @@
-﻿using Origins.Items.Accessories;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Origins.Items.Accessories;
+using Origins.Items.Other.Dyes;
 using Origins.Tiles;
+using PegasusLib.Graphics;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.Items.Other.Consumables {
 	public class Latchkey : ModItem {
-        public string[] Categories => [
-            "ExpendableTool"
-        ];
-        public override void SetStaticDefaults() {
+		public string[] Categories => [
+			"ExpendableTool"
+		];
+		public override void SetStaticDefaults() {
 			Item.ResearchUnlockCount = 99;
 		}
 		public override void SetDefaults() {
@@ -116,7 +123,7 @@ namespace Origins.Items.Other.Consumables {
 			for (int i = 0; i <= maxX; i++) {
 				for (int j = 0; j <= maxY; j++) {
 					Tile tile = Main.tile[i + minX, j + minY];
-					if (tile != null && tile.HasSolidTile()) {
+					if (tile != null && tile.HasFullSolidTile()) {
 						checkArea.X = i * -16 + cornerX;
 						checkArea.Y = j * -16 + cornerY;
 						int value = TileLoader.GetTile(tile.TileType) is IDefiledTile ? 1 : 2;
@@ -130,6 +137,75 @@ namespace Origins.Items.Other.Consumables {
 				}
 			}
 			return highestValue;
+		}
+	}
+	public class Latchkey_Visuals : ModSystem {
+		internal RenderTarget2D renderTarget;
+		internal RenderTarget2D oldRenderTarget;
+		bool oldHadLatchkeys = false;
+		public override void PostDrawTiles() {
+			int type = ModContent.ProjectileType<Latchkey_P>();
+			List<Projectile> latchkeys = [];
+			foreach (Projectile projectile in Main.ActiveProjectiles) {
+				if (projectile.type != type) continue;
+				latchkeys.Add(projectile);
+			}
+			if (latchkeys.Count > 0) {
+				RenderTargetBinding[] oldRenderTargets = Main.graphics.GraphicsDevice.GetRenderTargets();
+				Utils.Swap(ref renderTarget, ref oldRenderTarget);
+				Main.graphics.GraphicsDevice.SetRenderTarget(renderTarget);
+				Main.graphics.GraphicsDevice.Clear(Color.Transparent);
+				Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+				if (oldHadLatchkeys) Main.spriteBatch.Draw(oldRenderTarget, Main.screenLastPosition - Main.screenPosition, null, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+				for (int i = 0; i < latchkeys.Count; i++) {
+					Player player = Main.player[latchkeys[i].owner];
+					Main.PlayerRenderer.DrawPlayer(
+						Main.Camera,
+						player,
+						player.position + new Vector2(0, player.gfxOffY),
+						player.fullRotation,
+						player.fullRotationOrigin,
+						scale: 1
+					);
+				}
+
+				Main.spriteBatch.End();
+				Main.graphics.GraphicsDevice.UseOldRenderTargets(oldRenderTargets);
+				Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+				Main.spriteBatch.Draw(renderTarget, Vector2.Zero, null, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+				DrawData data = new(renderTarget, Vector2.Zero, null, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None);
+				ArmorShaderData shader = GameShaders.Armor.GetSecondaryShader(Shimmer_Dye.ShaderID, null); // temp shader choice
+				shader.Apply(Main.LocalPlayer, data);
+				data.Draw(Main.spriteBatch);
+				Main.spriteBatch.End();
+
+				oldHadLatchkeys = true;
+			} else {
+				oldHadLatchkeys = false;
+			}
+		}
+		public override void Load() {
+			if (Main.dedServ) return;
+			Main.QueueMainThreadAction(SetupRenderTargets);
+			Main.OnResolutionChanged += Resize;
+		}
+		public void Resize(Vector2 _) {
+			if (Main.dedServ) return;
+			renderTarget.Dispose();
+			oldRenderTarget.Dispose();
+			SetupRenderTargets();
+		}
+		void SetupRenderTargets() {
+			if (renderTarget is not null && !renderTarget.IsDisposed) return;
+			renderTarget = new RenderTarget2D(Main.instance.GraphicsDevice, Main.screenWidth, Main.screenHeight, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.PreserveContents);
+			oldRenderTarget = new RenderTarget2D(Main.instance.GraphicsDevice, Main.screenWidth, Main.screenHeight, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.PreserveContents);
+		}
+		public override void Unload() {
+			Main.QueueMainThreadAction(() => {
+				renderTarget.Dispose();
+				oldRenderTarget.Dispose();
+			});
+			Main.OnResolutionChanged -= Resize;
 		}
 	}
 }
