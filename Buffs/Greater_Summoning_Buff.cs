91576e363f9a5949938e97b2d65546e634ae9f74
Tyfyter <tyfyter2616@gmail.com>
2025-03-24 19:59:44 -0600

    Greater Summoning Potion

---
+++ Buffs/Greater_Summoning_Buff.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Origins.Buffs {
+	public class Greater_Summoning_Buff : ModBuff {
+		public static int ID { get; private set; }
+		public string[] Categories => [
+			"SummonBoostBuff"
+		];
+		public override void SetStaticDefaults() {
+			ID = Type;
+		}
+		public override void Update(Player player, ref int buffIndex) {
+			player.maxMinions += 2;
+		}
+	}
+}
--- Items/Other/Consumables/Greater_Summoning_Potion.cs
+++ Items/Other/Consumables/Greater_Summoning_Potion.cs
@@ -6,7 +6,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.Items.Other.Consumables {
-	public class Focus_Potion : ModItem {
+	public class Greater_Summoning_Potion : ModItem {
 		public const float bonus_multiplicative = 0.15f;
 		public const float bonus_additive = 5f;
 		public string[] Categories => [
@@ -17,18 +17,16 @@ namespace Origins.Items.Other.Consumables {
 		}
 		public override void SetDefaults() {
 			Item.CloneDefaults(ItemID.WrathPotion);
-			Item.buffType = Focus_Buff.ID;
+			Item.buffType = Greater_Summoning_Buff.ID;
 			Item.value = Item.sellPrice(silver: 2);
 		}
 		public override void AddRecipes() {
 			Recipe.Create(Type)
 			.AddIngredient(ItemID.BottledWater)
-			.AddIngredient(ItemID.FallenStar)
-			.AddIngredient(ModContent.ItemType<Brineglow_Item>())
 			.AddIngredient(ModContent.ItemType<Toadfish>())
+			.AddIngredient(ItemID.Moonglow)
 			.AddTile(TileID.Bottles)
 			.Register();
 		}
-		public static int GetManaCost(Item item) => (int)(15 * (item.useAnimation / 60f)) + 1;
 	}
 }
