f03de62a682f72aef226dc444497adb2e75f2b89
Tyfyter <tyfyter2616@gmail.com>
2025-04-01 18:45:15 -0600

    Limestone Piles

---
+++ Tiles/Limestone/Limestone_Pile_Medium.cs
@@ -0,0 +1,34 @@
+﻿using Origins.Items.Other.Testing;
+using Origins.Tiles.Brine;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Origins.Tiles.Limestone {
+	public class Limestone_Pile_Medium : ModTile {
+		public override void SetStaticDefaults() {
+			Main.tileFrameImportant[Type] = true;
+			Main.tileCut[Type] = false;
+			Main.tileNoFail[Type] = true;
+			AddMapEntry(new(203, 194, 149));
+
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+			TileObjectData.newTile.Origin = new Point16(0, 2);
+			if (!Name.EndsWith("_Fake")) TileObjectData.newTile.RandomStyleRange = 3;
+			TileObjectData.addTile(Type);
+
+			DustType = DustID.Sand;
+		}
+	}
+	public class Limestone_Pile_Medium_Fake : Limestone_Pile_Medium {
+		public override string Texture => base.Texture[..^"_Fake".Length];
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			FlexibleTileWand.RubblePlacementMedium.AddVariations(ModContent.ItemType<Limestone_Item>(), Type, 0, 1, 2);
+			RegisterItemDrop(ModContent.ItemType<Limestone_Item>());
+		}
+	}
+}
---
+++ Tiles/Limestone/Limestone_Stalactite.cs
@@ -0,0 +1,83 @@
+﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Origins.Tiles.Limestone {
+	public class Limestone_Stalactite : ModTile {
+		public override void SetStaticDefaults() {
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoFail[Type] = true;
+			AddMapEntry(new(203, 194, 149));
+
+			TileObjectData.newTile.Width = 1;
+			TileObjectData.newTile.Height = 3;
+			TileObjectData.newTile.UsesCustomCanPlace = true;
+			TileObjectData.newTile.LavaDeath = true;
+			TileObjectData.newTile.CoordinateHeights = new int[3] {
+				16,
+				16,
+				16
+			};
+
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.Direction = TileObjectDirection.None;
+			TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidBottom, TileObjectData.newTile.Width, 0);
+			if (!Name.EndsWith("_Fake")) TileObjectData.newTile.RandomStyleRange = 3;
+			TileObjectData.newTile.Origin = new();
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleMultiplier = 1;
+			TileObjectData.addTile(Type);
+
+			DustType = DustID.Sand;
+		}
+	}
+	public class Limestone_Stalagmite : ModTile {
+		public override void SetStaticDefaults() {
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoFail[Type] = true;
+			AddMapEntry(new(203, 194, 149));
+
+			TileObjectData.newTile.Width = 1;
+			TileObjectData.newTile.Height = 3;
+			TileObjectData.newTile.Origin = new Point16(0, 2);
+			TileObjectData.newTile.UsesCustomCanPlace = true;
+			TileObjectData.newTile.LavaDeath = true;
+			TileObjectData.newTile.CoordinateHeights = new int[3] {
+				16,
+				16,
+				16
+			};
+
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.Direction = TileObjectDirection.None;
+			if (!Name.EndsWith("_Fake")) TileObjectData.newTile.RandomStyleRange = 3;
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleMultiplier = 1;
+			TileObjectData.addTile(Type);
+
+			DustType = DustID.Sand;
+		}
+	}
+	public class Limestone_Stalactite_Fake : Limestone_Stalactite {
+		public override string Texture => base.Texture[..^"_Fake".Length];
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			FlexibleTileWand.RubblePlacementMedium.AddVariations(ModContent.ItemType<Limestone_Item>(), Type, 0, 1, 2);
+			RegisterItemDrop(ModContent.ItemType<Limestone_Item>());
+		}
+	}
+	public class Limestone_Stalagmite_Fake : Limestone_Stalagmite {
+		public override string Texture => base.Texture[..^"_Fake".Length];
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			FlexibleTileWand.RubblePlacementMedium.AddVariations(ModContent.ItemType<Limestone_Item>(), Type, 0, 1, 2);
+			RegisterItemDrop(ModContent.ItemType<Limestone_Item>());
+		}
+	}
+}
