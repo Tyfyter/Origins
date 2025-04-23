7c6f760c530d6f4770cf0e67c913dcac515f007d
Tyfyter <tyfyter2616@gmail.com>
2025-04-22 18:13:15 -0600

    Fixed repeat attack protection misalignment

--- NPCs/Defiled/Boss/Defiled_Amalgamation.cs
+++ NPCs/Defiled/Boss/Defiled_Amalgamation.cs
@@ -266,13 +266,13 @@ namespace Origins.NPCs.Defiled.Boss {
 									new(0, 0f),
 									new(state_single_dash, 0.9f),
 									new(state_projectiles, 1f),
-									new(state_laser_rotate, MathHelper.Lerp(0.1f,1f,NPC.life / (float)(NPC.lifeMax)) * difficultyMult)
 									new(state_triple_dash, 0.35f),
 									new(state_sidestep_dash, 0.45f + (0.05f * difficultyMult)),
 									new(state_summon_roar, 0f),
 									new(state_ground_spikes, 1f),
 									new(state_magic_missile, 1f),
-									new(state_split_amalgamation_start,1f)
+									new(state_laser_rotate, MathHelper.Lerp(0.1f,1f,NPC.life / (float)(NPC.lifeMax)) * difficultyMult),
+									new(state_split_amalgamation_start, 1f)
 									]
 								);
 								int lastUsedAttack = -AIState;
