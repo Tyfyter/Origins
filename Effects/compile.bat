4e9219869c838ebfd5872e520dc4bc6208d768cf
Tyfyter <tyfyter2616@gmail.com>
2025-04-25 14:57:39 -0600

    Update compile.bat

--- Effects/compile.bat
+++ Effects/compile.bat
@@ -1,4 +1,4 @@
 @echo off
 fxcompiler.exe>log.log
-powershell -Command "(gc temp.log) -replace 'C:\\Users\\%USERNAME%\\Documents\\My Games\\Terraria\\tModLoader\\ModSources\\Origins\\Effects\\', '' | Out-File -encoding ASCII log.log"
+powershell -Command "(gc log.log) -replace 'C:\\Users\\%USERNAME%\\Documents\\My Games\\Terraria\\tModLoader\\ModSources\\Origins\\Effects\\', '' | Out-File -encoding ASCII log.log"
 log.log
