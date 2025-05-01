0c9783518373f119faf7c99ab08e5c676ed8e5e1
Tyfyter <tyfyter2616@gmail.com>
2025-05-01 12:44:24 -0600

    Fountain tooltips no longer need to be added manually

--- CrossMod/Fargos/Items/OFargosGlobalNPC.cs
+++ CrossMod/Fargos/Items/OFargosGlobalNPC.cs
@@ -1,4 +1,5 @@
 ﻿using Origins.Tiles.Other;
+using Origins.World.BiomeData;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.Localization;
@@ -8,20 +9,21 @@ using static Terraria.ModLoader.ModContent;
 namespace Origins.CrossMod.Fargos.NPCs {
 	[ExtendsFromMod(nameof(Fargowiltas))]
 	public class OFargosGlobalItem : GlobalItem {
-		private static string ExpandedTooltipLoc(string line) => Language.GetOrRegister($"Mods.Origins.CrossMod.ExpandedTooltips.{line}", () => line).Value;
-		private TooltipLine FountainTooltip(string biome) {
-			return new TooltipLine(Mod, "Tooltip0", $"[i:909] [c/AAAAAA:{ExpandedTooltipLoc($"Fountain{biome}")}]");
+		private static string ExpandedTooltipLoc<TBiome>() where TBiome : ModBiome =>
+			Language.GetOrRegister("Mods.Origins.CrossMod.ExpandedTooltips.Fountain").Format(GetInstance<TBiome>().DisplayName);
+		private TooltipLine FountainTooltip<TBiome>() where TBiome : ModBiome {
+			return new TooltipLine(Mod, "Tooltip0", $"[i:909] [c/AAAAAA:{ExpandedTooltipLoc<TBiome>()}]");
 		}
 		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
 			int tipLine = tooltips.FindIndex((line) => line.Mod == nameof(Fargowiltas) && line.Name == "TooltipNPCSold") + 1;
 			if (item.type == ItemType<Defiled_Fountain_Item>()) {
-				tooltips.Insert(tipLine, FountainTooltip("Defiled"));
+				tooltips.Insert(tipLine, FountainTooltip<Defiled_Wastelands>());
 			}
 			if (item.type == ItemType<Riven_Fountain_Item>()) {
-				tooltips.Insert(tipLine, FountainTooltip("Riven"));
+				tooltips.Insert(tipLine, FountainTooltip<Riven_Hive>());
 			}
 			if (item.type == ItemType<Brine_Fountain_Item>()) {
-				tooltips.Insert(tipLine, FountainTooltip("Brine"));
+				tooltips.Insert(tipLine, FountainTooltip<Brine_Pool>());
 			}
 		}
 	}
