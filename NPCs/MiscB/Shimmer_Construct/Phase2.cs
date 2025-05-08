6eeb2cc9beeb29dc8a677311cf431b52cf7c0546
Tyfyter <tyfyter2616@gmail.com>
2025-05-08 13:55:12 -0600

    Shimmer Construct phases are in separate files

---
+++ NPCs/MiscB/Shimmer_Construct/Phase2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria.ID;
+using Terraria;
+using static Origins.NPCs.MiscB.Shimmer_Construct.Shimmer_Construct;
+using PegasusLib;
+using Terraria.ModLoader;
+
+namespace Origins.NPCs.MiscB.Shimmer_Construct {
+	public class PhaseTwoIdleState : AIState {
+		public static List<AIState> aiStates = [];
+		public override void SetStaticDefaults() {
+			aiStates.Add(ModContent.GetInstance<SpawnCloudsState>());
+		}
+		public override void StartAIState(Shimmer_Construct boss) {
+			NPC npc = boss.NPC;
+			npc.ai[0] = 0;
+			npc.ai[1] = 0;
+			npc.ai[2] = 0;
+			npc.ai[3] = 0;
+		}
+		public override void DoAIState(Shimmer_Construct boss) {
+			NPC npc = boss.NPC;
+			npc.TargetClosest();
+			npc.velocity *= 0.97f;
+			if (++npc.ai[0] > (60 - ContentExtensions.DifficultyDamageMultiplier * 10) && Main.netMode != NetmodeID.MultiplayerClient) {
+				if (aiStates.Select(state => state.Index).All(boss.previousStates.Contains)) Array.Fill(boss.previousStates, Index);
+				SelectAIState(boss, aiStates);
+			}
+		}
+		public override void TrackState(int[] previousStates) { }
+	}
+	public class FastDashState : DashState {
+		public override void Load() {
+			PhaseTwoIdleState.aiStates.Add(this);
+		}
+		public override void StartAIState(Shimmer_Construct boss) {
+			base.StartAIState(boss);
+			NPC npc = boss.NPC;
+			npc.ai[3] *= 1.5f;
+			npc.ai[1] *= 1.5f;
+			npc.ai[2] *= 1.5f;
+		}
+	}
+	public class FastCircleState : CircleState {
+		public override void Load() {
+			PhaseTwoIdleState.aiStates.Add(this);
+		}
+		public override void StartAIState(Shimmer_Construct boss) {
+			NPC npc = boss.NPC;
+			npc.ai[3] = 11;
+		}
+	}
+	public class MagicMissilesState : AIState {
+		public override void Load() {
+			PhaseTwoIdleState.aiStates.Add(this);
+		}
+		public override void DoAIState(Shimmer_Construct boss) {
+			NPC npc = boss.NPC;
+			npc.SpawnProjectile(null,
+				npc.Center,
+				Vector2.UnitY.RotatedByRandom(1) * -7,
+				ProjectileID.FairyQueenSunDance,
+				1,
+				1
+			);
+			if (--npc.ai[0] <= 0) SetAIState(boss, StateIndex<PhaseOneIdleState>());
+		}
+		public override void StartAIState(Shimmer_Construct boss) {
+			NPC npc = boss.NPC;
+			npc.ai[0] = 5 + Main.rand.RandomRound(ContentExtensions.DifficultyDamageMultiplier);
+		}
+	}
+}
--- NPCs/MiscB/Shimmer_Construct/Shimmer_Construct.cs
+++ NPCs/MiscB/Shimmer_Construct/Shimmer_Construct.cs
@@ -19,15 +19,16 @@ namespace Origins.NPCs.MiscB.Shimmer_Construct {
 	public class Shimmer_Construct : ModNPC {
 		protected readonly static List<AIState> aiStates = [];
 		public override string Texture => "Terraria/Images/NPC_" + NPCID.EyeofCthulhu;
-		readonly int[] previousStates = new int[5];
+		public readonly int[] previousStates = new int[5];
 		public override void SetStaticDefaults() {
 			Main.npcFrameCount[Type] = 6;
+			for (int i = 0; i < aiStates.Count; i++) aiStates[i].SetStaticDefaults();
 		}
 		public override void SetDefaults() {
 			base.SetDefaults();
 			NPC.width = 100;
 			NPC.height = 110;
-			NPC.lifeMax = 1;
+			NPC.lifeMax = 100;
 			NPC.damage = 1;
 			NPC.boss = true;
 			NPC.noGravity = true;
@@ -59,10 +60,12 @@ namespace Origins.NPCs.MiscB.Shimmer_Construct {
 			npc.netUpdate = true;
 		}
 		public static int StateIndex<TState>() where TState : AIState => ModContent.GetInstance<TState>().Index;
-		public static void SelectAIState(Shimmer_Construct boss, List<AIState> from) {
+		public static void SelectAIState(Shimmer_Construct boss, params List<AIState>[] from) {
 			WeightedRandom<int> states = new(Main.rand);
-			for (int i = 0; i < from.Count; i++) {
-				states.Add(from[i].Index, from[i].GetWeight(boss, boss.previousStates));
+			for (int j = 0; j < from.Length; j++) {
+				for (int i = 0; i < from[j].Count; i++) {
+					states.Add(from[j][i].Index, from[j][i].GetWeight(boss, boss.previousStates));
+				}
 			}
 			SetAIState(boss, states);
 		}
@@ -74,6 +77,7 @@ namespace Origins.NPCs.MiscB.Shimmer_Construct {
 				Load();
 			}
 			public virtual void Load() { }
+			public virtual void SetStaticDefaults() { }
 			public abstract void DoAIState(Shimmer_Construct boss);
 			public virtual void StartAIState(Shimmer_Construct boss) { }
 			public virtual double GetWeight(Shimmer_Construct boss, int[] previousStates) {
@@ -83,310 +87,6 @@ namespace Origins.NPCs.MiscB.Shimmer_Construct {
 			}
 			public virtual void TrackState(int[] previousStates) => previousStates.Roll(Index);
 			public void Unload() { }
-		}
-		public class PhaseOneIdleState : AIState {
-			internal static List<AIState> aiStates = [];
-			public override void StartAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				npc.ai[0] = 0;
-				npc.ai[1] = 0;
-				npc.ai[2] = 0;
-				npc.ai[3] = 0;
-			}
-			public override void DoAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				npc.TargetClosest();
-				npc.velocity *= 0.97f;
-				if (++npc.ai[0] > 60 && Main.netMode != NetmodeID.MultiplayerClient) {
-					if (aiStates.Select(state => state.Index).All(boss.previousStates.Contains)) Array.Fill(boss.previousStates, Index);
-					SelectAIState(boss, aiStates);
-				}
-			}
-			public override void TrackState(int[] previousStates) { }
-		}
-		public class DashState : AIState {
-			public override void Load() {
-				PhaseOneIdleState.aiStates.Add(this);
-			}
-			public override void DoAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				npc.velocity.X = npc.ai[1];
-				npc.velocity.Y = npc.ai[2];
-				npc.rotation = npc.velocity.ToRotation();
-				if ((++npc.ai[0]) * npc.ai[3] > 16 * 40) SetAIState(boss, StateIndex<PhaseOneIdleState>());
-			}
-			public override void StartAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				npc.ai[3] = 6 + ContentExtensions.DifficultyDamageMultiplier;
-				Vector2 direction = npc.DirectionTo(npc.GetTargetData().Center) * npc.ai[3];
-				npc.ai[1] = direction.X;
-				npc.ai[2] = direction.Y;
-			}
-		}
-		public class CircleState : AIState {
-			public override void Load() {
-				PhaseOneIdleState.aiStates.Add(this);
-			}
-			public override void DoAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				Vector2 diff = npc.GetTargetData().Center - npc.Center;
-				Vector2 direction = diff.SafeNormalize(Vector2.UnitY);
-				Vector2 targetDiff = direction.RotatedBy(npc.direction) * 16 * 30;
-				npc.velocity = diff.DirectionFrom(targetDiff) * (6.5f + ContentExtensions.DifficultyDamageMultiplier * 0.5f);
-				npc.rotation = npc.velocity.ToRotation();
-				int shotsToHaveFired = (int)((++npc.ai[0]) / npc.ai[3]);
-				if (shotsToHaveFired > npc.ai[1]) {
-					npc.ai[1]++;
-					npc.SpawnProjectile(null,
-						npc.Center,
-						direction * 7,
-						ProjectileID.BulletSnowman,
-						1,
-						1
-					);
-				}
-				if (npc.ai[0] > 120) SetAIState(boss, StateIndex<PhaseOneIdleState>());
-			}
-			public override void StartAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				npc.ai[3] = 15;
-			}
-		}
-		public class SpawnCloudsState : AIState {
-			public override void Load() {
-				PhaseOneIdleState.aiStates.Add(this);
-				if (Main.dedServ) return;
-				On_Main.DrawProjectiles += (orig, self) => {
-					orig(self);
-					if (Main.dedServ) return;
-					if (cachedRain.Count == 0 && cachedClouds.Count == 0) return;
-					try {
-						GraphicsUtils.drawingEffect = true;
-						Origins.shaderOroboros.Capture();
-						while (cachedRain.Count != 0) {
-							self.DrawProj(cachedRain.Pop());
-						}
-						while (cachedClouds.Count != 0) {
-							self.DrawProj(cachedClouds.Pop());
-						}
-						ArmorShaderData shader = GameShaders.Armor.GetSecondaryShader(Shimmer_Dye.ShaderID, null);
-						Origins.shaderOroboros.Stack(shader);
-						Origins.shaderOroboros.Release();
-					} finally {
-						GraphicsUtils.drawingEffect = false;
-					}
-				};
-			}
-			internal static Stack<int> cachedClouds = [];
-			internal static Stack<int> cachedRain = [];
-			public override void DoAIState(Shimmer_Construct boss) {
-				NPC npc = boss.NPC;
-				Vector2 targetPos = npc.GetTargetData().Center;
-				Vector2 unfurlPos = targetPos - new Vector2(-35, 25) * 16;
-				npc.SpawnProjectile(null,
-					npc.Center,
-					npc.DirectionTo(unfurlPos) * 7,
-					ModContent.ProjectileType<Shimmer_Construct_Cloud_Ball>(),
-					1,
-					1,
-					unfurlPos.X,
-					unfurlPos.Y
-				);
-				unfurlPos = targetPos - new Vector2(35, 25) * 16;
-				npc.SpawnProjectile(null,
-					npc.Center,
-					npc.DirectionTo(targetPos - new Vector2(35, 25) * 16) * 7,
-					ModContent.ProjectileType<Shimmer_Construct_Cloud_Ball>(),
-					1,
-					1,
-					unfurlPos.X,
-					unfurlPos.Y
-				);
-				SetAIState(boss, StateIndex<PhaseOneIdleState>());
-			}
-			public override double GetWeight(Shimmer_Construct boss, int[] previousStates) {
-				int cloudType = ModContent.ProjectileType<Shimmer_Construct_Cloud_P>();
-				int ballType = ModContent.ProjectileType<Shimmer_Construct_Cloud_Ball>();
-				int count = 0;
-				foreach (Projectile proj in Main.ActiveProjectiles) {
-					if ((proj.type == cloudType || proj.type == ballType) && ++count >= 2) return 0;
-				}
-				return Math.Max(base.GetWeight(boss, previousStates) - 0.5f, 0);
-			}
-
-			public class Shimmer_Construct_Cloud_Ball : ModProjectile {
-				public override string Texture => typeof(Shimmer_Cloud_Ball).GetDefaultTMLName();
-				public override void SetStaticDefaults() {
-					Main.projFrames[Type] = 4;
-				}
-				public override void SetDefaults() {
-					Projectile.CloneDefaults(ProjectileID.RainCloudMoving);
-					Projectile.aiStyle = 0;
-					Projectile.timeLeft = 18000;
-					Projectile.tileCollide = false;
-				}
-				public Vector2 TargetPos {
-					get => new(Projectile.ai[0], Projectile.ai[1]);
-					set {
-						Projectile.ai[0] = value.X;
-						Projectile.ai[1] = value.Y;
-					}
-				}
-				public override void AI() {
-					if (TargetPos != default) {
-						Vector2 combined = Projectile.velocity * (TargetPos - Projectile.Center);
-						if (Projectile.owner == Main.myPlayer && combined.X <= 0 && combined.Y <= 0) {
-							Projectile.NewProjectile(
-								Projectile.GetSource_FromAI(),
-								TargetPos,
-								default,
-								ModContent.ProjectileType<Shimmer_Construct_Cloud_P>(),
-								Projectile.damage,
-								Projectile.knockBack,
-								Projectile.owner,
-								Projectile.ai[2]
-							);
-							Projectile.Kill();
-							OriginExtensions.FadeOutOldProjectilesAtLimit([ModContent.ProjectileType<Shimmer_Construct_Cloud_P>(), ModContent.ProjectileType<Shimmer_Construct_Cloud_Ball>()], 3, 52);
-							return;
-						}
-					}
-					float inShimmer = Collision.WetCollision(Projectile.position, Projectile.width, Projectile.height) && Collision.shimmer ? 1 : 0;
-					if (Projectile.localAI[0] != inShimmer) {
-						Projectile.localAI[0] = inShimmer;
-						if (inShimmer == 1) {
-							Projectile.velocity.Y = -Projectile.velocity.Y;
-							Projectile.ai[1] += (Projectile.Center.Y - Projectile.ai[1]) * 2;
-							Projectile.ai[2] = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
-						}
-					}
-
-					Projectile.rotation += Projectile.velocity.X * 0.02f;
-					Projectile.frameCounter++;
-					if (Projectile.frameCounter > 4) {
-						Projectile.frameCounter = 0;
-						Projectile.frame++;
-						if (Projectile.frame > 3)
-							Projectile.frame = 0;
-					}
-				}
-				public override bool PreDraw(ref Color lightColor) {
-					if (!GraphicsUtils.drawingEffect) {
-						cachedClouds.Push(Projectile.whoAmI);
-						return false;
-					}
-					lightColor = Color.LightGray;
-					return true;
-				}
-				public override bool OnTileCollide(Vector2 oldVelocity) {
-					TargetPos = Projectile.Center;
-					Projectile.velocity = default;
-					return false;
-				}
-			}
-			public class Shimmer_Construct_Cloud_P : ModProjectile {
-				public override string Texture => typeof(Shimmer_Cloud_P).GetDefaultTMLName();
-				public override void SetStaticDefaults() {
-					Main.projFrames[Type] = 6;
-				}
-				public override void SetDefaults() {
-					Projectile.CloneDefaults(ProjectileID.RainCloudRaining);
-					Projectile.aiStyle = 0;
-					Projectile.timeLeft = 60 * 200;
-					Projectile.width = 28;
-					Projectile.height = 28;
-					Projectile.tileCollide = false;
-				}
-				public override void AI() {
-					Projectile.rotation = Projectile.ai[0];
-					if (++Projectile.ai[1] % 10f < 1) {
-						Vector2 unit = new Vector2(0, 1).RotatedBy(Projectile.rotation);
-						Vector2 perp = new(unit.Y, -unit.X);
-						Projectile.NewProjectile(
-							Projectile.GetSource_FromAI(),
-							Projectile.Center + unit * 24 + perp * Main.rand.Next(-14, 15),
-							unit * 5,
-							Shimmer_Construct_Cloud_Rain.ID,
-							Projectile.damage,
-							Projectile.knockBack,
-							Projectile.owner,
-							Projectile.ai[2]
-						);
-					}
-					Projectile.frameCounter++;
-					if (Projectile.frameCounter > 4) {
-						Projectile.frameCounter = 0;
-						Projectile.frame++;
-						if (Projectile.frame > 5)
-							Projectile.frame = 0;
-					}
-				}
-				public override bool PreDraw(ref Color lightColor) {
-					if (!GraphicsUtils.drawingEffect) {
-						cachedClouds.Push(Projectile.whoAmI);
-						return false;
-					}
-					lightColor = Color.LightGray;
-					Rectangle frame = TextureAssets.Projectile[Type].Value.Frame(verticalFrames: 6, frameY: Projectile.frame);
-					float timeFactor = Math.Min(Projectile.timeLeft / 52f, 1);
-					Main.spriteBatch.Draw(
-						TextureAssets.Projectile[Type].Value,
-						Projectile.Center - Main.screenPosition,
-						frame,
-						lightColor * timeFactor,
-						Projectile.rotation,
-						frame.Size() * 0.5f,
-						Projectile.scale,
-						0,
-					0);
-					return false;
-				}
-			}
-			public class Shimmer_Construct_Cloud_Rain : ModProjectile {
-				public override string Texture => typeof(Shimmer_Cloud_Rain).GetDefaultTMLName();
-				public static int ID { get; private set; }
-				public override void SetStaticDefaults() {
-					Origins.HomingEffectivenessMultiplier[Type] = 2;
-					ID = Type;
-				}
-				public override void SetDefaults() {
-					Projectile.CloneDefaults(ProjectileID.RainFriendly);
-					Projectile.friendly = true;
-					Projectile.hostile = true;
-					Projectile.aiStyle = 0;
-					Projectile.width = 4;
-					Projectile.height = 4;
-					Projectile.tileCollide = false;
-				}
-				public override void AI() {
-					Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
-					if (Collision.WetCollision(Projectile.position, Projectile.width, Projectile.height) && Collision.shimmer) {
-						Projectile.velocity.Y -= 0.2f;
-					}
-					if (!Projectile.tileCollide && !Projectile.Hitbox.OverlapsAnyTiles()) {
-						Projectile.tileCollide = true;
-					}
-				}
-				public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) {
-					if (projHitbox.Intersects(targetHitbox)) return true;
-					const float factor = 1.5f;
-					Rectangle hitbox = projHitbox;
-					for (int i = 0; i < 7; i++) {
-						hitbox.X = projHitbox.X - (int)(Projectile.velocity.X * i * factor);
-						hitbox.Y = projHitbox.Y - (int)(Projectile.velocity.Y * i * factor);
-						if (hitbox.Intersects(targetHitbox)) return true;
-					}
-					return false;
-				}
-				public override bool PreDraw(ref Color lightColor) {
-					if (!GraphicsUtils.drawingEffect) {
-						cachedRain.Push(Projectile.whoAmI);
-						return false;
-					}
-					lightColor = new Color(1f, 0, 0.08f, 0.3f);
-					return true;
-				}
-			}
 		}
 	}
 }
