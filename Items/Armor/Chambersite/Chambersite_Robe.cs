c4933ca8487d8719d6098f975e9eaeed155754a2
Tyfyter <tyfyter2616@gmail.com>
2025-05-12 13:57:02 -0600

    Chambersite Robe fixes

--- Items/Armor/Chambersite/Chambersite_Robe.cs
+++ Items/Armor/Chambersite/Chambersite_Robe.cs
@@ -8,14 +8,10 @@ using Terraria.ModLoader;
 
 namespace Origins.Items.Armor.Chambersite {
 	[AutoloadEquip(EquipType.Body)]
-	public class Chambersite_Robe : ModItem, INoSeperateWikiPage {
-		private int ManaToRemove = 80;
-		public string[] Categories => [
-			"ArmorSet",
-			"MagicBoostGear"
-		];
-		public override void SetStaticDefaults() {
-			ArmorIDs.Body.Sets.HidesBottomSkin[Item.bodySlot] = true;
+	public class Chambersite_Robe : ModItem {
+		int legsID;
+		public override void Load() {
+			legsID = EquipLoader.AddEquipTexture(Mod, $"{Texture}_{EquipType.Legs}", EquipType.Legs, name: $"{Name}_{EquipType.Legs}");
 		}
 		public override void SetDefaults() {
 			Item.defense = 9;
@@ -23,9 +19,8 @@ namespace Origins.Items.Armor.Chambersite {
 			Item.rare = ItemRarityID.Lime;
 		}
 		public override void UpdateEquip(Player player) {
-			player.statManaMax2 = Math.Max(player.statManaMax2 - ManaToRemove, 20);
-			ManaToRemove = 80;
-			player.manaCost = 0.2f;
+			player.statManaMax2 = Math.Max(player.statManaMax2 - (player.armor[0].type == ItemID.MagicHat ? 20 : 80), 20);
+			player.manaCost *= 0.2f;
 		}
 		public override bool IsArmorSet(Item head, Item body, Item legs) {
 			return head.type == ItemID.MagicHat || head.type == ItemID.WizardHat;
@@ -33,12 +28,15 @@ namespace Origins.Items.Armor.Chambersite {
 		public override void UpdateArmorSet(Player player) {
 			if (player.armor[0].type == ItemID.MagicHat) {
 				player.setBonus = Language.GetTextValue("Mods.Origins.SetBonuses.ChambersiteMagicHat");
-				ManaToRemove = 20;
 			}
 			if (player.armor[0].type == ItemID.WizardHat) {
 				player.setBonus = Language.GetTextValue("Mods.Origins.SetBonuses.ChambersiteWizardHat");
 				player.GetCritChance(DamageClass.Magic) += 20;
 			}
+		}
+		public override void SetMatch(bool male, ref int equipSlot, ref bool robes) {
+			robes = true;
+			equipSlot = legsID;
 		}
 		public override void AddRecipes() {
 			CreateRecipe()
