69e54d3e23d35b0e5a87fbfb434a7bb40f2874c9
R31v4x-x <amandagraham232@gmail.com>
2025-05-29 14:39:09 -0400

    caged

---
+++ Tiles/Other/Amoeba_Buggy_Cage.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Origins.Items.Other.Critters;
+using Terraria.ID;
+
+namespace Origins.Tiles.Other {
+	public class Amoeba_Buggy_Cage : CageBase {
+		public override int LidType => 0;
+		public override int[] FrameIndexArray => Main.snailCageFrame;
+		public override CageKinds CageKind => CageKinds.SmallCage;
+		public override void OnLoad() {
+			item.OnAddRecipes += (item) => {
+				Recipe.Create(item.type)
+				.AddIngredient<Amoeba_Buggy_Item>()
+				.AddIngredient(ItemID.Terrarium)
+				.Register();
+			};
+		}
+	}
+}
---
+++ Tiles/Other/Cage_Cage.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Origins.Items.Other.Critters;
+using Terraria.ID;
+
+namespace Origins.Tiles.Other {
+	public class Cage_Cage : CageBase {
+		public override string Texture => "Origins/Tiles/Other/Amoeba_Buggy_Cage";
+		public override int LidType => 0;
+		public override int[] FrameIndexArray => Main.snailCageFrame;
+		public override CageKinds CageKind => CageKinds.SmallCage;
+		public override void OnLoad() {
+			item.OnAddRecipes += (item) => {
+				Recipe.Create(item.type)
+				.AddIngredient<Bug_Item>()
+				.AddIngredient(ItemID.Terrarium)
+				.Register();
+			};
+		}
+	}
+}
