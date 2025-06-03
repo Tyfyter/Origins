182fa194fe67cc0f22a65c88c4b686fab5decdf5
Tyfyter <tyfyter2616@gmail.com>
2025-06-03 15:09:41 -0600

    remembered what was forgotten
    Ain't like we can't change the crafting station 'f I'm missing somethin'

--- Items/Other/Consumables/Super_Mojo_Flask.cs
+++ Items/Other/Consumables/Super_Mojo_Flask.cs
@@ -1,4 +1,7 @@
-﻿using Terraria;
+﻿using Origins.Tiles.Brine;
+using Origins.Tiles.Other;
+using Terraria;
+using Terraria.ID;
 
 namespace Origins.Items.Other.Consumables {
 	public class Super_Mojo_Flask : Mojo_Flask {
@@ -7,5 +10,11 @@ namespace Origins.Items.Other.Consumables {
 			base.SetDefaults();
 			Item.buffTime = 25;
 		}
+		public override void AddRecipes() => CreateRecipe()
+			.AddIngredient<Mojo_Flask>()
+			.AddIngredient(ItemID.SoulofLight, 8)
+			.AddIngredient<Brineglow_Item>(8)
+			.AddTile<Cleansing_Station>()
+			.Register();
 	}
 }
