d9a7de921c59146cccae3c1c1a1ed7b3c2d4383b
Tyfyter <tyfyter2616@gmail.com>
2025-06-26 15:24:31 -0600

    Others may see me move as a wheel does, and despise me

--- NPCs/Riven/Single_Cellular_Nautilus.cs
+++ NPCs/Riven/Single_Cellular_Nautilus.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Origins.Dev;
 using Origins.Items.Armor.Riven;
 using Origins.Items.Materials;
 using Origins.World.BiomeData;
 using System;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
@@ -28,8 +31,8 @@ namespace Origins.NPCs.Riven {
 			NPC.lifeMax = 110;
 			NPC.defense = 31;
 			NPC.damage = 28;
-			NPC.width = 20;
-			NPC.height = 24;
+			NPC.width = 30;
+			NPC.height = 30;
 			NPC.friendly = false;
 			NPC.knockBackResist = 1f;
 			NPC.frame.Height = 30;
@@ -66,5 +69,24 @@ namespace Origins.NPCs.Riven {
 				for (int i = 0; i < 6; i++) Origins.instance.SpawnGoreByName(NPC.GetSource_Death(), NPC.position + new Vector2(Main.rand.Next(NPC.width), Main.rand.Next(NPC.height)), NPC.velocity, "Gores/NPCs/R_Effect_Meat1");
 			}
 		}
+		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
+			SpriteEffects effects = NPC.spriteDirection == -1 ? SpriteEffects.FlipVertically : SpriteEffects.None;
+			DrawData data = new(
+				TextureAssets.Npc[NPC.type].Value,
+				NPC.Center - screenPos,
+				NPC.frame,
+				drawColor,
+				NPC.rotation,
+				new Vector2(22, 15).Apply(effects, NPC.frame.Size()),
+				NPC.scale,
+				effects
+			);
+			data.Draw(spriteBatch);
+			data.texture = GlowTexture;
+			data.color = Riven_Hive.GetGlowAlpha(drawColor);
+			data.Draw(spriteBatch);
+			return false;
+		}
+		public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) { }
 	}
 }
