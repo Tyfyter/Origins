2cd33fae184297f427f744cc2909787ce0fdf1d2
R31v4x-x <amandagraham232@gmail.com>
2025-07-14 05:54:59 -0400

    finished aether orb

--- CrossMod/Fargos/Items/Aether_Orb.cs
+++ CrossMod/Fargos/Items/Aether_Orb.cs
@@ -17,14 +17,19 @@ namespace Origins.CrossMod.Fargos.Items {
 			Item.consumable = false;
 		}
 		public override void Update(ref float gravity, ref float maxFallSpeed) {
-			if (Item.shimmerWet && !Item.shimmered && Main.netMode != NetmodeID.MultiplayerClient && !NPC.AnyNPCs(ModContent.NPCType<Shimmer_Construct>())) {
-				SoundEngine.PlaySound(SoundID.Roar, Item.Center);
-				NPC.SpawnBoss((int)Item.Center.X, (int)Item.Center.Y, ModContent.NPCType<Shimmer_Construct>(), Main.myPlayer);
-				Item.stack--;
-				if (Item.stack <= 0) Item.active = false;
-				if (Item.active) {
+			if (!NetmodeActive.MultiplayerClient && Item.shimmerWet && !Item.shimmered) {
+				if (!NPC.AnyNPCs(ModContent.NPCType<Shimmer_Construct>())) {
+					Item.shimmerTime += 0.02f;
+					if (Item.shimmerTime > 0.9f) {
+						Item.shimmerTime = 0.9f;
+						SoundEngine.PlaySound(SoundID.Roar, Item.Center);
+						NPC.SpawnBoss((int)Item.Center.X, (int)Item.Center.Y, ModContent.NPCType<Shimmer_Construct>(), Main.myPlayer);
+						Item.stack--;
+						if (Item.stack <= 0) Item.active = false;
+					}
+				}
+				if (Item.active && NPC.AnyNPCs(ModContent.NPCType<Shimmer_Construct>())) {
 					Item.shimmered = true;
-					Item.shimmerTime = 1;
 				}
 			}
 		}
