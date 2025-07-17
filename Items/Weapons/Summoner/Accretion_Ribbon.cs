f9309a0484d0457f719d3075047502c7260d306a
Tyfyter <tyfyter2616@gmail.com>
2025-07-17 14:44:17 -0600

    AR fixes, buff, & pole

--- Items/Weapons/Summoner/Accretion_Ribbon.cs
+++ Items/Weapons/Summoner/Accretion_Ribbon.cs
@@ -19,7 +19,6 @@ namespace Origins.Items.Weapons.Summoner {
 		}
 		public override void SetDefaults() {
 			Item.DefaultToWhip(ModContent.ProjectileType<Accretion_Ribbon_P>(), 34, 5, 1, 28);
-			Item.DamageType = DamageClass.SummonMeleeSpeed;
 			Item.value = Item.sellPrice(gold: 4, silver: 60);
 			Item.rare = ItemRarityID.LightRed;
 		}
@@ -50,39 +49,48 @@ namespace Origins.Items.Weapons.Summoner {
 			Projectile.DefaultToWhip();
 			Projectile.extraUpdates = 0;
 			Projectile.aiStyle = -1;
-			Projectile.width = 10;
-			Projectile.height = 10;
+			Projectile.width = 0;
+			Projectile.height = 0;
 			Projectile.ContinuouslyUpdateDamageStats = true;
 			Projectile.friendly = true;
 			Projectile.hide = true;
 			Projectile.manualDirectionChange = true;
 			Projectile.localNPCHitCooldown = 10;
 		}
+		public override bool ShouldUpdatePosition() => false;
 		public override void OnSpawn(IEntitySource source) {
 			if (source is EntitySource_ItemUse itemUse) {
 				Projectile.scale *= itemUse.Item.scale;
+				Projectile.localAI[0] = itemUse.Item.prefix;
 			}
 		}
 		public override void AI() {
+			float max_dist = 162 * Projectile.scale;
 			Player player = Main.player[Projectile.owner];
-			if (!player.active || player.dead || player.HeldItem.ModItem is not Accretion_Ribbon) {
+			if (Projectile.owner == Main.myPlayer && (!player.active || player.dead || player.HeldItem.ModItem is not Accretion_Ribbon || Projectile.localAI[0] != player.HeldItem.prefix)) {
 				Projectile.Kill();
 				return;
 			}
 			Projectile.timeLeft = 2;
-			if (player.altFunctionUse == 2) {
+			Projectile.extraUpdates = 0;
+			if (player.altFunctionUse == 2 || player.controlUseTile) {
+				Projectile.extraUpdates = 2;
 				float maxXVel = Math.Max(Math.Abs(player.velocity.X), Math.Max(player.maxRunSpeed * 2.5f, player.accRunSpeed * 2f));
+				bool first = false;
 				if (Projectile.direction == 0) {
+					first = true;
 					Projectile.direction = player.direction;
 					if (player.velocity.Y == 0) {
 						player.velocity += new Vector2(Projectile.direction * 2, -1) * (5.01f + player.jumpSpeedBoost);
 					}
 				}
-				player.velocity.X = float.Clamp(player.velocity.X + Projectile.direction * 0.2f, -maxXVel, maxXVel);
-				Projectile.velocity = Projectile.velocity.RotatedBy(0.35f * Projectile.direction);
-			} else {
+				player.velocity.X = float.Clamp(player.velocity.X + Projectile.direction * 0.07f, -maxXVel, maxXVel);
+				float compensation = first ? 3 : 1;
+				Projectile.velocity = Projectile.velocity.RotatedBy((MathHelper.PiOver2 * Projectile.direction * compensation / 0.75f) / player.itemAnimationMax);
+				if (player.altFunctionUse != 2) Projectile.direction = 0;
+			} else if (Projectile.owner == Main.myPlayer) {
 				Vector2 diff = Main.MouseWorld - player.MountedCenter;
-				Projectile.ai[0] = Math.Min(diff.Length(), 162);
+				Projectile.ai[0] = Math.Min(diff.Length(), max_dist);
 				Vector2 velocity = Projectile.velocity;
 				MathUtils.LinearSmoothing(ref velocity, diff.SafeNormalize(default), 1);
 				if (Projectile.velocity != velocity) {
@@ -94,28 +102,37 @@ namespace Origins.Items.Weapons.Summoner {
 					Projectile.velocity = Projectile.velocity.RotatedBy(0.4f * MathF.Sin((player.itemAnimation / (float)player.itemAnimationMax) * MathHelper.TwoPi + MathHelper.Pi));
 				}
 			}
-			Projectile.position = player.MountedCenter + (Projectile.velocity * Projectile.ai[0]).Floor();
-			Projectile.rotation = Projectile.velocity.ToRotation();
+			Vector2 centerDiff = player.MountedCenter - player.position;
+			Projectile.position = Vector2.Lerp(player.oldPosition, player.position, 1 - (Projectile.numUpdates + 1) / (float)Projectile.MaxUpdates) + centerDiff + (Projectile.velocity * Projectile.ai[0]).Floor();
+			Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
+			player.SetCompositeArmFront(true, (Projectile.ai[0] * 4 / max_dist) switch {
+				<= 1 => Player.CompositeArmStretchAmount.None,
+				<= 2 => Player.CompositeArmStretchAmount.Quarter,
+				<= 3 => Player.CompositeArmStretchAmount.ThreeQuarters,
+				_ => Player.CompositeArmStretchAmount.Full
+			}, Projectile.rotation);
 			player.heldProj = Projectile.whoAmI;
 			for (int i = 0; i < partialImmunity.Length; i++) {
-				if (partialImmunity[i]) {
+				if (partialImmunity[i] > 0) {
 					NPC npc = Main.npc[i];
 					if (!npc.active || !Projectile.Colliding(default, npc.Hitbox)) {
-						partialImmunity[i] = false;
+						partialImmunity[i] = 0;
+					} else {
+						partialImmunity[i]--;
 					}
 				}
 			}
 		}
-		readonly bool[] partialImmunity = new bool[Main.maxNPCs];
+		readonly int[] partialImmunity = new int[Main.maxNPCs];
 		public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers) {
 			modifiers.HitDirectionOverride = Math.Sign(target.Center.X - Main.player[Projectile.owner].Center.X);
-			if (partialImmunity[target.whoAmI]) {
+			if (partialImmunity[target.whoAmI] > 0) {
 				modifiers.SourceDamage *= 0.1f;
 				modifiers.Knockback *= 0.5f;
 			}
 		}
 		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
-			partialImmunity[target.whoAmI] = true;
+			if (partialImmunity[target.whoAmI] <= 0) partialImmunity[target.whoAmI] = ProjectileID.Sets.TrailCacheLength[Type];
 			if (target.life > 0 && target.CanBeChasedBy()) Main.player[Projectile.owner].MinionAttackTargetNPC = target.whoAmI;
 			target.AddBuff(Accretion_Ribbon_Buff.ID, 240);
 		}
@@ -123,6 +140,7 @@ namespace Origins.Items.Weapons.Summoner {
 			float size = 4 * Projectile.scale;
 			float _ = 0;
 			for (int i = 1; i < Projectile.oldPos.Length; i++) {
+				if (Projectile.oldPos[i] == default) break;
 				if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.oldPos[i - 1], Projectile.oldPos[i], size, ref _)) return true;
 			}
 			return false;
@@ -148,7 +166,8 @@ namespace Origins.Items.Weapons.Summoner {
 			}
 			oldRot[0] = oldRot[1];
 			miscShaderData.Apply();
-			_vertexStrip.PrepareStrip(Projectile.oldPos, oldRot, (GetLightColor) => new(120, 80, 225), _ => 4, -Main.screenPosition, Projectile.oldPos.Length, includeBacksides: true);
+			float size = 4 * Projectile.scale;
+			_vertexStrip.PrepareStrip(Projectile.oldPos, oldRot, (GetLightColor) => new(120, 80, 225), _ => size, -Main.screenPosition, Projectile.oldPos.Length, includeBacksides: true);
 			_vertexStrip.DrawTrail();
 
 			Origins.shaderOroboros.DrawContents(renderTarget, Color.White, Main.GameViewMatrix.EffectMatrix);
@@ -164,6 +183,17 @@ namespace Origins.Items.Weapons.Summoner {
 				Vector2.One / Main.GameViewMatrix.Zoom,
 				SpriteEffects.None
 			));
+			Texture2D texture = TextureAssets.Projectile[Type].Value;
+			Main.EntitySpriteDraw(
+				texture,
+				Projectile.position - Main.screenPosition,
+				null,
+				lightColor,
+				Projectile.rotation,
+				texture.Size() - (Vector2.One * 3),
+				Projectile.scale,
+				SpriteEffects.None
+			);
 			return false;
 		}
 		public override void OnKill(int timeLeft) {
