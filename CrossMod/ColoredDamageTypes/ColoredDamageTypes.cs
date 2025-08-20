54ccc11cc2dad3695196bd8defe48d6da6c6c4b2
R31v4x-x <amandagraham232@gmail.com>
2025-08-20 03:23:33 -0400

    forgot to commit

--- CrossMod/ColoredDamageTypes/ColoredDamageTypes.cs
+++ CrossMod/ColoredDamageTypes/ColoredDamageTypes.cs
@@ -2,6 +2,7 @@
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using Terraria;
@@ -14,9 +15,13 @@ namespace Origins.CrossMod.ColoredDamageTypes {
 		private static DamageClass explosiveSentry;
 		public static DamageClass ExplosiveSentry => explosiveSentry ??= ModContent.GetInstance<SentryExplosive>();
 		public static Mod ColoredTypesMod;
-		public static Color explColor = new(234, 56, 103);
-		public static Color explCritColor = new(235, 0, 59);
-		public static Dictionary<DamageClass, (Color dmgColor, Color critColor)> colors = [];
+		public static Color novaTipColor = new(234, 56, 103);
+		public static Color novaDmgColor = new(234, 56, 103);
+		public static Color novaCritColor = new(235, 0, 59);
+		public static Color explTipColor = new(234, 105, 91);
+		public static Color explDmgColor = new(234, 105, 91);
+		public static Color explCritColor = new(173, 78, 67);
+		public static Dictionary<DamageClass, (Color tipColor, Color dmgColor, Color critColor)> colors = [];
 
 		public override bool IsLoadingEnabled(Mod mod) {
 			return ModLoader.HasMod("ColoredDamageTypes");
@@ -51,14 +56,28 @@ namespace Origins.CrossMod.ColoredDamageTypes {
 		}
 		public override void PostSetupContent() {
 			Type DamageConfig = ColoredTypesMod.Code.GetType("ColoredDamageTypes.DamageConfig");
+			Type TooltipConfig = ColoredTypesMod.Code.GetType("ColoredDamageTypes.TooltipsConfig");
+
 			FieldInfo VanillaDmg = DamageConfig.GetField("VanillaDmg");
+			FieldInfo VanillaTT = TooltipConfig.GetField("VanillaTT");
+
 			object VanillaInstance = VanillaDmg.GetValue(DamageConfig.GetField("Instance").GetValue(null));
+			object VanillaTTInstance = VanillaTT.GetValue(TooltipConfig.GetField("Instance").GetValue(null));
+
 			FieldInfo VanillaMeleeDmg = VanillaDmg.FieldType.GetField("MeleeDmg");
 			FieldInfo VanillaRangeDmg = VanillaDmg.FieldType.GetField("RangedDmg");
 			FieldInfo VanillaMagicDmg = VanillaDmg.FieldType.GetField("MagicDmg");
 			FieldInfo VanillaThrowDmg = VanillaDmg.FieldType.GetField("ThrowingDmg");
 			FieldInfo VanillaSummonDmg = VanillaDmg.FieldType.GetField("SummonDmg");
 			FieldInfo VanillaSentryDmg = VanillaDmg.FieldType.GetField("SentryDmg");
+
+			Color MeleeTTColor = (Color)VanillaTT.FieldType.GetField("TooltipMelee").GetValue(VanillaTTInstance);
+			Color RangeTTColor = (Color)VanillaTT.FieldType.GetField("TooltipRanged").GetValue(VanillaTTInstance);
+			Color MagicTTColor = (Color)VanillaTT.FieldType.GetField("TooltipMagic").GetValue(VanillaTTInstance);
+			Color ThrowTTColor = (Color)VanillaTT.FieldType.GetField("TooltipThrowing").GetValue(VanillaTTInstance);
+			Color SummonTTColor = (Color)VanillaTT.FieldType.GetField("TooltipSummon").GetValue(VanillaTTInstance);
+			Color SentryTTColor = (Color)VanillaTT.FieldType.GetField("TooltipSentry").GetValue(VanillaTTInstance);
+
 			Color MeleeColor = (Color)VanillaMeleeDmg.FieldType.GetField("MeleeDamage").GetValue(VanillaMeleeDmg.GetValue(VanillaInstance));
 			Color MeleeCritColor = (Color)VanillaMeleeDmg.FieldType.GetField("MeleeDamageCrit").GetValue(VanillaMeleeDmg.GetValue(VanillaInstance));
 			Color RangeColor = (Color)VanillaRangeDmg.FieldType.GetField("RangedDamage").GetValue(VanillaRangeDmg.GetValue(VanillaInstance));
@@ -73,22 +92,40 @@ namespace Origins.CrossMod.ColoredDamageTypes {
 			Color SentryCritColor = (Color)VanillaSentryDmg.FieldType.GetField("SentryDamageCrit").GetValue(VanillaSentryDmg.GetValue(VanillaInstance));
 
 			DamageClass sentryClass = ColoredTypesMod.Find<DamageClass>("SentryClass");
-			colors.Add(DamageClass.Melee, (MeleeColor, MeleeCritColor));
-			colors.Add(DamageClass.MeleeNoSpeed, (MeleeColor, MeleeCritColor));
-			colors.Add(DamageClass.Ranged, (RangeColor, RangeCritColor));
-			colors.Add(DamageClass.Magic, (MagicColor, MagicCritColor));
-			colors.Add(DamageClass.Throwing, (ThrowColor, ThrowCritColor));
-			colors.Add(DamageClass.Summon, (SummonColor, SummonCritColor));
-			colors.Add(DamageClass.SummonMeleeSpeed, (SummonColor, SummonCritColor));
-			colors.Add(sentryClass, (SentryColor, SentryCritColor));
+			colors.Add(DamageClass.Melee, (MeleeTTColor, MeleeColor, MeleeCritColor));
+			colors.Add(DamageClass.MeleeNoSpeed, (MeleeTTColor, MeleeColor, MeleeCritColor));
+			colors.Add(DamageClass.Ranged, (RangeTTColor, RangeColor, RangeCritColor));
+			colors.Add(DamageClass.Magic, (MagicTTColor, MagicColor, MagicCritColor));
+			colors.Add(DamageClass.Throwing, (ThrowTTColor, ThrowColor, ThrowCritColor));
+			colors.Add(DamageClass.Summon, (SummonTTColor, SummonColor, SummonCritColor));
+			colors.Add(DamageClass.SummonMeleeSpeed, (SummonTTColor, SummonColor, SummonCritColor));
+			colors.Add(sentryClass, (SentryTTColor, SentryColor, SentryCritColor));
+
+			Type CustomConfig = ColoredTypesMod.Code.GetType("ColoredDamageTypes.zCrossModConfig");
+			FieldInfo CrossDmg = CustomConfig.GetField("CrossModDamageConfig");
+			IDictionary CustomInstance = (IDictionary)CrossDmg.GetValue(CustomConfig.GetField("Instance").GetValue(null));
+			Type DictValue = CrossDmg.FieldType.GenericTypeArguments[1];
+			FieldInfo TooltipColor = DictValue.GetField("TooltipColor");
+			FieldInfo DamageColor = DictValue.GetField("DamageColor");
+			FieldInfo CritDamageColor = DictValue.GetField("CritDamageColor");
+
+			foreach (DictionaryEntry obj in CustomInstance) {
+				DamageClass dClass = ModContent.Find<DamageClass>((string)obj.Key);
+				if (DamageClasses.ExplosiveVersion.ContainsKey(dClass)) {
+					Color tipColor = (Color)TooltipColor.GetValue(obj.Value);
+					Color dmgColor = (Color)DamageColor.GetValue(obj.Value);
+					Color critColor = (Color)CritDamageColor.GetValue(obj.Value);
+					colors.Add(dClass, (tipColor, dmgColor, critColor));
+				}
+			}
 
 			if (DamageClasses.ExplosiveVersion.ContainsKey(sentryClass)) DamageClasses.ExplosiveVersion[sentryClass] = ExplosiveSentry;
 			else DamageClasses.ExplosiveVersion.Add(sentryClass, ExplosiveSentry);
 
 			ColoredTypesMod.Call("AddDamageType",
 				DamageClasses.Explosive,
-				explColor,
-				explColor,
+				explTipColor,
+				explDmgColor,
 				explCritColor
 			);
 			ColoredTypesMod.Call("AddDamageType",
@@ -111,20 +148,16 @@ namespace Origins.CrossMod.ColoredDamageTypes {
 			);
 			ColoredTypesMod.Call("AddDamageType",
 				ModContent.GetInstance<Chambersite_Mine_Launcher_Damage>(),
-				colors[DamageClass.Ranged].dmgColor.MultiplyRGB(explColor),
-				colors[DamageClass.Ranged].dmgColor.MultiplyRGB(explColor),
+				colors[DamageClass.Ranged].dmgColor.MultiplyRGB(explTipColor),
+				colors[DamageClass.Ranged].dmgColor.MultiplyRGB(explDmgColor),
 				colors[DamageClass.Ranged].critColor.MultiplyRGB(explCritColor)
 			);
 
-			if (OriginsModIntegrations.Thorium is not null) {
-				if (OriginsModIntegrations.Thorium.TryFind("BardClass", out DamageClass bard)) colors.Add(bard, (new(), new()));
-			}
-
-			foreach (KeyValuePair<DamageClass, (Color dmgColor, Color critColor)> item in colors) {
+			foreach (KeyValuePair<DamageClass, (Color tipColor, Color dmgColor, Color critColor)> item in colors) {
 				ColoredTypesMod.Call("AddDamageType",
 					DamageClasses.ExplosiveVersion[item.Key],
-					item.Value.dmgColor.MultiplyRGB(explColor),
-					item.Value.dmgColor.MultiplyRGB(explColor),
+					item.Value.tipColor.MultiplyRGB(explTipColor),
+					item.Value.dmgColor.MultiplyRGB(explDmgColor),
 					item.Value.critColor.MultiplyRGB(explCritColor)
 				);
 			}
