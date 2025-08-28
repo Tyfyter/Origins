bcccc30bf3d0b61e1ec548c957340ff06f77e806
Tyfyter <tyfyter2616@gmail.com>
2025-08-28 16:33:18 -0600

    Alt glitch effect

--- UI/Glitch_Snippet.cs
+++ UI/Glitch_Snippet.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using Terraria;
+using Terraria.ModLoader;
 using Terraria.UI.Chat;
 using Terraria.Utilities;
 
@@ -10,11 +11,21 @@ namespace Origins.UI {
 		internal static Vector2 origin;
 		public class Glitch_Snippet(string text, string symbols, Color color = default, float scale = 1) : TextSnippet(text, color, scale) {
 			readonly StringBuilder DisplayedText = new(text);
+			string OriginalText = text;
 			public override void Update() {
+				if (string.IsNullOrEmpty(OriginalText)) OriginalText = Lang.GetItemNameValue(Main.rand.Next(ItemLoader.ItemCount));
+				if (!string.IsNullOrEmpty(symbols)) DisplayedText.Replace(Text, OriginalText);
 				UnifiedRandom rand = new((int)(Origins.gameFrameCount / 32));
 				rand = new((int)(Origins.gameFrameCount >> (2 + rand.Next(2))));
-				for (int i = rand.Next(Text.Length / 4); i > 0; i--) {
-					DisplayedText[rand.Next(DisplayedText.Length)] = symbols[rand.Next(symbols.Length)];
+				for (int i = rand.Next(Text.Length / 4 + 1); i > 0; i--) {
+					int index = rand.Next(DisplayedText.Length);
+					if (string.IsNullOrEmpty(symbols)) {
+						unchecked {
+							DisplayedText[index] = (char)(DisplayedText[index] + rand.Next(-4, 5));
+						}
+					} else {
+						DisplayedText[index] = symbols[rand.Next(symbols.Length)];
+					}
 				}
 				Text = DisplayedText.ToString();
 			}
