97aad58e2ae6ae349f1d6db9f0309e351a922ac4
Tyfyter <tyfyter2616@gmail.com>
2025-09-02 15:45:15 -0600

    Potato mine should work better now

---
+++ Reflection/WiringMethods.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable CS0649
+#pragma warning disable IDE0044
+using System;
+using Terraria;
+using PegasusLib.Reflection;
+
+namespace Origins.Reflection {
+	public class WiringMethods : ReflectionLoader {
+		[ReflectionParentType(typeof(Wiring)), ReflectionMemberName("HitWireSingle")]
+		static Action<int, int> _hitWireSingle;
+		public static void HitWireSingle(int i, int j) => _hitWireSingle(i, j);
+	}
+}
