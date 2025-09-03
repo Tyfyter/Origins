116cdf5a09a58af3a8ec60dbd836e290a08f0041
Tyfyter <tyfyter2616@gmail.com>
2025-09-03 17:43:48 -0600

    We don't need to reference it

--- CrossMod/LuiAFK/NPCs/OLuiAFKGlobalNPC.cs
+++ CrossMod/LuiAFK/NPCs/OLuiAFKGlobalNPC.cs
@@ -1,21 +1,19 @@
-﻿using miningcracks_take_on_luiafk.NPCs;
-using Origins.Items.Other.Consumables;
+﻿using Origins.Items.Other.Consumables;
 using Origins.Items.Tools;
 using Origins.Questing;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 
 namespace Origins.CrossMod.LuiAFK.NPCs {
-	[ExtendsFromMod(nameof(miningcracks_take_on_luiafk))]
+	[ExtendsFromMod("miningcracks_take_on_luiafk")]
 	public class OLuiAFKGlobalNPC : GlobalNPC {
 		public override void ModifyShop(NPCShop shop) {
-			if (shop.NpcType == NPCType<SkeletonMerchant>()) {
+			if (shop.Name == "LuiAFK Skeleton Merchant Shop") {
 				shop.Add(ItemID.BlackInk);
 				shop.Add<Trash_Lid>(Condition.MoonPhaseFull);
 			}
-			if (shop.NpcType == NPCType<MobileMerchant>()) {
+			if (shop.Name == "LuiAFK Fairy Merchant Shop") {
 				shop.Add<Blue_Bovine>(Quest.QuestCondition<Blue_Bovine_Quest>());
 				shop.Add<Lottery_Ticket>(Quest.QuestCondition<Lottery_Ticket_Quest>());
 			}
