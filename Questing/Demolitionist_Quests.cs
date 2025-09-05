3bfe89433aed58c4ebdd867f3db1f3c9ca2e9376
Tyfyter <tyfyter2616@gmail.com>
2025-09-05 17:05:31 -0600

    Update Demolitionist_Quests.cs

--- Questing/Demolitionist_Quests.cs
+++ Questing/Demolitionist_Quests.cs
@@ -53,8 +53,7 @@ namespace Origins.Questing {
 			return Quest.HasQuestButton(npc, player);
 		}
 	}
-	public class Peat_Moss_Quest : Quest, IBrokenContent {
-		public string BrokenReason => "Missing localizations";
+	public class Peat_Moss_Quest : Quest {
 		public const string lockey = "Mods.Origins.Quests.Demolitionist.Peat_Moss.";
 		public static IEnumerable<ShopItem> GetItems() {
 			yield return new ShopItem<Peatball>(15);
