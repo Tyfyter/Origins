0d42840e82103f1b09fbdb6006e19269183f9567
chrersis <66661847+chrersis@users.noreply.github.com>
2025-09-26 19:25:15 -0700

    Chambersite Ring sprite

--- CrossMod/Thorium/Items/Accessories/Chambersite_Ring.cs
+++ CrossMod/Thorium/Items/Accessories/Chambersite_Ring.cs
@@ -7,7 +7,6 @@ using ThoriumMod.Items.BasicAccessories;
 namespace Origins.Items.Accessories {
 	[ExtendsFromMod("ThoriumMod")]
 	public class Chambersite_Ring : GemRingBase/*, ICustomWikiStat*/ {
-		public override string Texture => typeof(Large_Chambersite).GetDefaultTMLName();
 		public override int PrimaryIngredientType => ModContent.ItemType<Chambersite_Item>();
 		public override int StatIncrease => 6;
         public override void UpdateAccessory(Player player, bool hideVisual) {
