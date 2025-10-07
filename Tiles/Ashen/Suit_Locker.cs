40991cb5971f10e342e9396f0a9c671ef2920775
Tyfyter <tyfyter2616@gmail.com>
2025-10-07 15:31:18 -0600

    Powered Suit Lockers

---
+++ Tiles/Ashen/Suit_Locker.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Origins.Buffs;
+using Origins.Graphics;
+using Origins.Items.Other.Testing;
+using Origins.Items.Weapons.Demolitionist;
+using Origins.Tiles.Other;
+using Origins.Tiles.Riven;
+using Origins.World.BiomeData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Origins.Tiles.Ashen {
+	public class Suit_Locker : OriginTile, IGlowingModTile {
+		public static int ID { get; private set; }
+		TileItem item;
+		public override void Load() {
+			this.SetupGlowKeys();
+			Mod.AddContent(item = new(this));
+		}
+		public void FancyLightingGlowColor(Tile tile, ref Vector3 color) {
+			if (tile.TileFrameX < 3 * 18 * 2) color = Vector3.Max(color, new Vector3(0.912f, 0.579f, 0f) * (tile.TileFrameY < 2 * 18 ? 1 : 0.125f));
+		}
+		public override void SetStaticDefaults() {
+			if (!Main.dedServ) GlowTexture = ModContent.Request<Texture2D>(Texture + "_Glow");
+			// Properties
+			TileID.Sets.CanBeSloped[Type] = false;
+			Main.tileLighted[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = false;
+			TileID.Sets.HasOutlines[Type] = false;
+			TileID.Sets.DisableSmartCursor[Type] = true;
+
+			// Names
+			AddMapEntry(new Color(220, 220, 220), CreateMapEntryName());
+
+			// Placement
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+			TileObjectData.newTile.Direction = TileObjectDirection.None;
+			TileObjectData.newTile.FlattenAnchors = true;
+			TileObjectData.newTile.RandomStyleRange = 3;
+			TileObjectData.addTile(Type);
+			ID = Type;
+			DustType = Ashen_Biome.DefaultTileDust;
+			RegisterItemDrop(item.Type);
+		}
+		public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) {
+			if (Main.tile[i, j].TileFrameX < 3 * 18 * 2) r = g = b = 0.01f;
+		}
+		public CustomTilePaintLoader.CustomTileVariationKey GlowPaintKey { get; set; }
+		public AutoCastingAsset<Texture2D> GlowTexture { get; private set; }
+		public Color GlowColor => Color.White;
+	}
+}
