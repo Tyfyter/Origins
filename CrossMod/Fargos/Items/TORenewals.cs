ce8f46bda273cc3df8aa24a70b60adba1160f793
R31v4x-x <amandagraham232@gmail.com>
2025-10-06 23:46:10 -0400

    update renewals

--- CrossMod/Fargos/Items/TORenewals.cs
+++ CrossMod/Fargos/Items/TORenewals.cs
@@ -1,16 +1,27 @@
 ﻿using AltLibrary.Common.AltBiomes;
 using AltLibrary.Core;
+using Origins.Items.Weapons.Ammo;
+using Origins.World.BiomeData;
 using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Origins.CrossMod.Fargos.Items {
-	public abstract class TORenewals<TMaterial, TSolution>(bool supreme = false) : ModItem where TMaterial : ModItem where TSolution : ModProjectile {
+	public abstract class TORenewals : ModItem {
 		public override bool IsLoadingEnabled(Mod mod) => ModLoader.HasMod("Fargowiltas");
+		protected override bool CloneNewInstances => true;
+		protected internal TORenewal_P Proj;
+		public virtual bool Supreme => false;
+		public abstract ModItem Material { get; }
+		public abstract AltBiome Biome { get; }
 
+		public override void Load() {
+			Mod.AddContent(Proj = new TORenewal_P(this));
+		}
 		public override void SetStaticDefaults() {
 			Item.ResearchUnlockCount = 10;
 		}
@@ -27,17 +38,18 @@ namespace Origins.CrossMod.Fargos.Items {
 			Item.value = Item.buyPrice(0, 0, 3);
 			Item.noUseGraphic = true;
 			Item.noMelee = true;
-			Item.shoot = 1;
+			if (Proj?.Projectile is not null) Item.shoot = Proj.Type;
 			Item.shootSpeed = 5f;
 		}
-		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-			Projectile.NewProjectile(player.GetSource_ItemUse(source.Item), position, velocity, ModContent.ProjectileType<TSolution>(), 0, 0f, Main.myPlayer, 0f, 0f, 0f);
-			return false;
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {/*
+			Projectile.NewProjectile(player.GetSource_ItemUse(source.Item), position, velocity, Proj.Type, 0, 0f, Main.myPlayer, 0f, 0f, 0f);
+			return false;*/
+			return true;
 		}
 		public override void AddRecipes() {
 			int amt;
 			Recipe recipe = Recipe.Create(Type);
-			if (supreme) {
+			if (Supreme) {
 				amt = 10;
 				recipe.AddIngredient(ItemID.ChlorophyteBar, 5);
 				recipe.AddTile(TileID.AlchemyTable);
@@ -46,13 +58,17 @@ namespace Origins.CrossMod.Fargos.Items {
 				recipe.AddIngredient(ItemID.Bottle);
 				recipe.AddTile(TileID.Bottles);
 			}
-			recipe.AddIngredient<TMaterial>(amt);
+			recipe.AddIngredient(Material, amt);
 			recipe.Register();
 		}
 	}
-	public abstract class TORenewal_P<TProj, TBiome>(bool supreme = false) : ModProjectile where TProj : ModProjectile where TBiome : AltBiome {
-
-		public override bool IsLoadingEnabled(Mod mod) => ModLoader.HasMod("Fargoswiltas");
+	[Autoload(false)]
+	public class TORenewal_P(TORenewals item) : ModProjectile {
+		protected override bool CloneNewInstances => true;
+		readonly TORenewals item = item;
+		public override string Name => item.Name + "_P";
+		public override LocalizedText DisplayName => item.DisplayName;
+		public override string Texture => item.Texture;
 
 		public override void SetDefaults() {
 			Projectile.width = 20;
@@ -68,34 +84,59 @@ namespace Origins.CrossMod.Fargos.Items {
 		}
 		public override void OnKill(int timeLeft) {
 			SoundEngine.PlaySound(in SoundID.Shatter, Projectile.Center);
-			int num = 150;
-			float[] array = [0f, 0f, 5f, 5f, 5f, -5f, -5f, -5f];
-			float[] array2 = [5f, -5f, 0f, 5f, -5f, 0f, 5f, -5f];
-			if (Main.netMode == 0) {
-				for (int i = 0; i < 8; i++) {
-					Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X, Projectile.Center.Y, array[i], array2[i], ModContent.ProjectileType<TProj>(), 0, 0f, Main.myPlayer);
+			int dst = 150;
+			Vector2[] vel = [new(0, 5), new(0, -5), new(5, 0), new(5), new(5, -5), new(-5, 0), new(-5, 5), new(-5)];
+
+			if (NetmodeActive.SinglePlayer) {
+				for (int i = 0; i < vel.Length; i++) {
+					Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, vel[i], item.Material.Item.shoot, 0, 0f, Projectile.owner);
 				}
 			}
 
-			if (supreme) {
-				for (int j = -Main.maxTilesX; j < Main.maxTilesX; j++) {
-					for (int k = -Main.maxTilesY; k < Main.maxTilesY; k++) {
-						int i2 = (int)(j + Projectile.Center.X / 16f);
-						int j2 = (int)(k + Projectile.Center.Y / 16f);
-						ALConvert.Convert<TBiome>(i2, j2, 1);
-					}
-				}
-
-				return;
+			int x = dst;
+			int y = dst;
+			if (item.Supreme) {
+				x = Main.maxTilesX;
+				y = Main.maxTilesY;
 			}
 
-			for (int l = -num; l <= num; l++) {
-				for (int m = -num; m <= num; m++) {
-					int i3 = (int)(l + Projectile.Center.X / 16f);
-					int j3 = (int)(m + Projectile.Center.Y / 16f);
-					if (Math.Sqrt(l * l + m * m) <= num + 0.5) ALConvert.Convert<TBiome>(i3, j3, 1);
+			for (int i = -x; i <= x; i++) {
+				for (int j = -y; j <= y; j++) {
+					int i3 = (int)(i + Projectile.Center.X / 16f);
+					int j3 = (int)(j + Projectile.Center.Y / 16f);
+					if (item.Supreme || Math.Sqrt(i * i + j * j) <= dst + 0.5) ALConvert.Convert(item.Biome, i3, j3, 1);
 				}
 			}
 		}
+	}
+
+	public class AshenRenewal : TORenewals {
+		public override ModItem Material => ModContent.GetInstance<Orange_Solution>();
+		public override AltBiome Biome => ModContent.GetInstance<Ashen_Alt_Biome>();
+	}
+	public class AshenSupremeRenewal : TORenewals {
+		public override bool Supreme => true;
+		public override ModItem Material => ModContent.GetInstance<AshenRenewal>();
+		public override AltBiome Biome => ModContent.GetInstance<Ashen_Alt_Biome>();
+	}
+
+	public class DefiledRenewal : TORenewals {
+		public override ModItem Material => ModContent.GetInstance<Gray_Solution>();
+		public override AltBiome Biome => ModContent.GetInstance<Defiled_Wastelands_Alt_Biome>();
+	}
+	public class DefiledSupremeRenewal : TORenewals {
+		public override bool Supreme => true;
+		public override ModItem Material => ModContent.GetInstance<DefiledRenewal>();
+		public override AltBiome Biome => ModContent.GetInstance<Defiled_Wastelands_Alt_Biome>();
+	}
+
+	public class RivenRenewal : TORenewals {
+		public override ModItem Material => ModContent.GetInstance<Teal_Solution>();
+		public override AltBiome Biome => ModContent.GetInstance<Riven_Hive_Alt_Biome>();
+	}
+	public class RivenSupremeRenewal : TORenewals {
+		public override bool Supreme => true;
+		public override ModItem Material => ModContent.GetInstance<RivenRenewal>();
+		public override AltBiome Biome => ModContent.GetInstance<Riven_Hive_Alt_Biome>();
 	}
 }
