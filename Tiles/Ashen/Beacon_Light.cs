77b1ae12788a9a988fa896e6de8d3704e7f49250
Tyfyter <tyfyter2616@gmail.com>
2025-10-07 20:51:48 -0600

    Beacon Light layering change

--- Tiles/Ashen/Beacon_Light.cs
+++ Tiles/Ashen/Beacon_Light.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Origins.Buffs;
+using Origins.Graphics;
 using Origins.Items.Other.Testing;
 using Origins.Items.Weapons.Demolitionist;
 using Origins.Tiles.Other;
 using Origins.Tiles.Riven;
 using Origins.World.BiomeData;
+using PegasusLib.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.Graphics.Effects;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
@@ -67,6 +71,10 @@ namespace Origins.Tiles.Ashen {
 	public class Beacon_Light_TE_System : TESystem {
 		HashSet<Point16> processedLocations;
 		List<Rectangle> hitboxes;
+		public const string biome_name = "Origins:BeaconLight";
+		public override void Load() {
+			Overlays.Scene[biome_name] = new Beacon_Light_Overlay();
+		}
 		public override void PreUpdateEntities() {
 			processedLocations ??= [];
 			hitboxes ??= [];
@@ -95,28 +103,60 @@ namespace Origins.Tiles.Ashen {
 			hitboxes.Clear();
 		}
 		public override void PostDrawTiles() {
-			Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
-			Color color = new(255, 123, 72);
-			const float range = 16 * 2640; // a mile
-			for (int i = 0; i < tileEntityLocations.Count; i++) {
-				Vector2 pos = tileEntityLocations[i].ToWorldCoordinates(0, 0);
-				float scaleFactor = float.Pow(1 - pos.Distance(pos.Clamp(Main.Camera.ScaledPosition, Main.Camera.ScaledPosition + Main.Camera.ScaledSize)) / range, 1.5f);
-				if (scaleFactor < 0.5f) scaleFactor *= scaleFactor + 0.5f;
-				if (scaleFactor < 0.1f) continue;
-				if (scaleFactor > 1) scaleFactor = 1;
-				Color currentColor = color * scaleFactor;
-				Flare_Launcher_Glow_P.DrawGlow(
-					pos,
-					currentColor,
-					scale: 5 * scaleFactor
-				);
+			bool inZone = tileEntityLocations.Count > 0;
+			if (inZone != (Overlays.Scene[biome_name].Mode != OverlayMode.Inactive)) {
+				if (inZone)
+					Overlays.Scene.Activate(biome_name, default);
+				else
+					Overlays.Scene[biome_name].Deactivate();
 			}
-			Main.spriteBatch.End();
 		}
 		internal static bool RegisterProjPosition(Point16 pos) {
 			Beacon_Light_TE_System instance = ModContent.GetInstance<Beacon_Light_TE_System>();
 			instance.processedLocations ??= [];
 			return instance.processedLocations.Add(pos);
+		}
+		public class Beacon_Light_Overlay() : Overlay(EffectPriority.High, RenderLayers.ForegroundWater) {
+			public override void Draw(SpriteBatch spriteBatch) {
+				List<Point16> tileEntityLocations = ModContent.GetInstance<Beacon_Light_TE_System>().tileEntityLocations;
+				SpriteBatchState state = spriteBatch.GetState();
+				spriteBatch.Restart(state, samplerState: SamplerState.LinearClamp);
+				Color color = new(255, 123, 72);
+				const float range = 16 * 2640; // a mile
+				for (int i = 0; i < tileEntityLocations.Count; i++) {
+					Vector2 pos = tileEntityLocations[i].ToWorldCoordinates(0, 0);
+					float obscureFactor = 1f;
+					foreach (Player player in Main.ActivePlayers) {
+						Rectangle rect = player.Hitbox;
+						rect.Inflate(-4, -4);
+						float distSQ = pos.Clamp(rect).DistanceSQ(pos);
+						if (distSQ <= 0) {
+							obscureFactor = 0;
+							break;
+						}
+						if (distSQ < 4 * 4) {
+							obscureFactor = Math.Min(float.Sqrt(distSQ) / 4, obscureFactor);
+						}
+					}
+					if (obscureFactor <= 0) continue;
+					float scaleFactor = float.Pow(1 - pos.Distance(pos.Clamp(Main.Camera.ScaledPosition, Main.Camera.ScaledPosition + Main.Camera.ScaledSize)) / range, 1.5f);
+					if (scaleFactor < 0.5f) scaleFactor *= scaleFactor + 0.5f;
+					if (scaleFactor < 0.1f) continue;
+					if (scaleFactor > 1) scaleFactor = 1;
+					scaleFactor *= obscureFactor;
+					Color currentColor = color * scaleFactor;
+					Flare_Launcher_Glow_P.DrawGlow(
+						pos,
+						currentColor,
+						scale: 5 * scaleFactor
+					);
+				}
+				spriteBatch.Restart(state);
+			}
+			public override void Update(GameTime gameTime) { }
+			public override void Activate(Vector2 position, params object[] args) { }
+			public override void Deactivate(params object[] args) { }
+			public override bool IsVisible() => true;
 		}
 	}
 }
