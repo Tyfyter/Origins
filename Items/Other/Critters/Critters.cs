bc07c9f8935887a49d8785d58671b16db0fe16ee
R31v4x-x <amandagraham232@gmail.com>
2025-10-08 18:28:18 -0400

    moth uses item sprite

--- Items/Other/Critters/Critters.cs
+++ Items/Other/Critters/Critters.cs
@@ -54,7 +54,6 @@ namespace Origins.Items.Other.Critters {
 		}
 	}
 	public class Peppered_Moth_Item : Critter_Item<Peppered_Moth> {
-		public override string Texture => typeof(Amoeba_Buggy_Item).GetDefaultTMLName();
 		public override void SafeSetDefaults() {
 			Item.bait = 40;
 			Item.value = Item.sellPrice(silver: 5);
