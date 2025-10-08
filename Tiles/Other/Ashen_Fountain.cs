b123f6ff62574edb78444361a19d2772ece070d3
R31v4x-x <amandagraham232@gmail.com>
2025-10-08 18:50:05 -0400

    water fountain

---
+++ Tiles/Other/Ashen_Fountain.cs
@@ -0,0 +1,11 @@
+﻿using Origins.Water;
+using Origins.World.BiomeData;
+using Terraria.ModLoader;
+
+namespace Origins.Tiles.Other {
+	public class Ashen_Fountain : WaterFountainBase<Ashen_Biome> {
+		public override string Texture => typeof(Defiled_Fountain).GetDefaultTMLName();
+		public override void SetBiomeActive() => Ashen_Biome.forcedBiomeActive = true;
+		public override ModWaterStyle WaterStyle => ModContent.GetInstance<Defiled_Water_Style>();
+	}
+}
