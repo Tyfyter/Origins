263832083a84381d722c471948b9e9eb0c193693
Tyfyter <tyfyter2616@gmail.com>
2025-10-07 20:51:34 -0600

    Rusty Piping

---
+++ Tiles/Ashen/Rusty_Piping.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Origins.Dev;
+using Origins.World.BiomeData;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.Graphics;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Origins.Tiles.Ashen {
+	public class Rusty_Piping : OriginTile, IAshenTile {
+		public override void Load() {
+			Mod.AddContent(new TileItem(this));
+		}
+		public override void SetStaticDefaults() {
+			Origins.PotType.Add(Type, ((ushort)TileType<Ashen_Pot>(), 0, 0));
+			Origins.PileType.Add(Type, ((ushort)TileType<Ashen_Foliage>(), 0, 6));
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileMergeDirt[Type] = false;
+			TileID.Sets.Stone[Type] = false;
+			TileID.Sets.Conversion.Stone[Type] = false;
+			TileID.Sets.CanBeClearedDuringGeneration[Type] = true;
+			TileID.Sets.CanBeClearedDuringOreRunner[Type] = true;
+			AddMapEntry(FromHexRGB(0x6c452c));
+
+			MinPick = 65;
+			MineResist = 2;
+			HitSound = SoundID.Tink;
+			DustType = Ashen_Biome.DefaultTileDust;
+		}
+	}
+}
