589731a6e27f3b15f668bca3d62ddf151803e32e
Tyfyter <tyfyter2616@gmail.com>
2025-10-21 14:10:55 -0600

    Scrap Heap

---
+++ Tiles/Ashen/Scrap_Heap.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace Origins.Tiles.Ashen {
+	public class Scrap_Heap : ComplexFrameTile, IAshenTile {
+		public override void Load() {
+			Mod.AddContent(new TileItem(this));
+		}
+		public override void SetStaticDefaults() {
+			Main.tileSolid[Type] = true;
+			TileID.Sets.CanBeClearedDuringGeneration[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			AddMapEntry(FromHexRGB(0x2c212a));
+			DustType = DustID.Copper;
+			HitSound = SoundID.NPCHit18;
+		}
+	}
+}
