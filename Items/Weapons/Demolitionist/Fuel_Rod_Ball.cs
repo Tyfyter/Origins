da1ca7fd24d0cd3b3d531716e0330df52bf0d78d
Tyfyter <tyfyter2616@gmail.com>
2025-10-30 14:34:26 -0600

    Improved Fuel Rod Ball

--- Items/Weapons/Demolitionist/Fuel_Rod_Ball.cs
+++ Items/Weapons/Demolitionist/Fuel_Rod_Ball.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Origins.Projectiles;
 using System;
+using System.IO;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent.Golf;
 using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -13,11 +15,20 @@ namespace Origins.Items.Weapons.Demolitionist {
 			OriginsSets.Items.ItemsThatCanChannelWithRightClick[Type] = true;
 		}
 		public override void SetDefaults() {
-			Item.CloneDefaults(ItemID.GolfBallDyedBlack);
+			Item.shootSpeed = 12f;
+			Item.width = 18;
+			Item.height = 20;
+			Item.maxStack = 1;
+			Item.useAnimation = 14;
+			Item.useTime = 14;
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
 			Item.damage = 45;
+			Item.useStyle = ItemUseStyleID.HiddenAnimation;
 			Item.DamageType = DamageClasses.ThrownExplosive;
 			Item.shoot = ModContent.ProjectileType<Fuel_Rod_Ball_P>();
 			Item.value = Item.sellPrice(gold: 1);
+			Item.UseSound = SoundID.Item1;
 			Item.channel = true;
 		}
 		public override bool AltFunctionUse(Player player) => true;
@@ -26,7 +37,27 @@ namespace Origins.Items.Weapons.Demolitionist {
 	public class Fuel_Rod_Ball_P : Golf_Ball_Projectile {
 		public override string Texture => typeof(Fuel_Rod_Ball).GetDefaultTMLName() + "_Outer";
 		AutoLoadingAsset<Texture2D> glowTexture = typeof(Fuel_Rod_Ball).GetDefaultTMLName() + "_Inner";
-		public int ChargeLevel => (int)(Projectile.ai[2] / 28);
+		public int ChargeLevel => (int)(Projectile.ai[2] / (useTimeMax * 2));
+		int useTimeMax = 14;
+		Color GlowColor {
+			get {
+				switch (ChargeLevel) {
+					case 1:
+					return Color.Orange;
+					case 2:
+					return Color.Yellow;
+					case 3:
+					return Color.White;
+				}
+				return Color.Transparent;
+			}
+		}
+		static void GetShotVelocity(Player player, out Vector2 position, out Vector2 velocity, out float progress) {
+			position = player.MountedCenter + Vector2.UnitX * player.direction * player.width * 0.5f;
+			velocity = (Main.MouseWorld - position).Normalized(out float dist);
+			progress = Utils.GetLerpValue(32, 320, dist, true);
+			velocity *= (4 + progress * 20f);
+		}
 		public override void SetDefaults() {
 			base.SetDefaults();
 			Projectile.width = 12;
@@ -38,21 +69,33 @@ namespace Origins.Items.Weapons.Demolitionist {
 			Projectile.localNPCHitCooldown = -1;
 		}
 		public override void OnSpawn(IEntitySource source) {
-			if (source is EntitySource_Parent { Entity: Player player }) Projectile.ai[0] = player.altFunctionUse;
+			if (source is EntitySource_Parent { Entity: Player player }) {
+				Projectile.ai[0] = player.altFunctionUse;
+				useTimeMax = player.itemTimeMax;
+			}
 		}
 		public override bool PreAI() {
 			Projectile.TryGetOwner(out Player player);
 			switch ((int)Projectile.ai[0]) {
 				case 2: {
-					Projectile.Center = player.MountedCenter + new Vector2(player.direction * 12, Utils.PingPongFrom01To010(++Projectile.ai[1] / 14) * 16);
+					Projectile.Center = player.MountedCenter + new Vector2(player.direction * 12, Utils.PingPongFrom01To010(++Projectile.ai[1] / useTimeMax) * 16);
 					if (Projectile.IsLocallyOwned() && PlayerInput.Triggers.JustPressed.MouseLeft) {
-						Projectile.velocity = (Main.MouseWorld - player.MountedCenter).Normalized(out float dist) * (4 + float.Pow(dist, 0.25f) * 4);
+						GetShotVelocity(player, out Projectile.position, out Projectile.velocity, out _);
+						Projectile.position -= Projectile.Size * 0.5f;
 						Projectile.ai[0] = 0;
 						Projectile.ai[1] = 0;
+						Projectile.netUpdate = true;
+						player.SetDummyItemTime(useTimeMax);
 						return false;
 					}
-					if (Projectile.ai[1] % 14 >= 7) player.heldProj = Projectile.whoAmI;
+					if ((Projectile.ai[1] + useTimeMax / 2) % (useTimeMax * 2) >= useTimeMax) {
+						player.heldProj = Projectile.whoAmI;
+						player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, player.direction * -1.5f);
+					} else {
+						player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.ThreeQuarters, player.direction * -1.2f);
+					}
 					if (!player.channel) Projectile.active = false;
+					player.SetDummyItemTime(useTimeMax);
 					Projectile.ai[2]++;
 					if (ChargeLevel > 3) Projectile.Kill();
 					return false;
@@ -66,9 +109,13 @@ namespace Origins.Items.Weapons.Demolitionist {
 				default:
 				if (Projectile.velocity.WithinRange(default, 1) && (Projectile.Hitbox.Add(Vector2.UnitY * (1 + Projectile.height)) with { Height = 2 }).OverlapsAnyTiles(false)) {
 					Projectile.ai[0] = 1;
+					Projectile.netUpdate = true;
 				}
 				return base.PreAI();
 			}
+		}
+		public override void PostAI() {
+			Lighting.AddLight(Projectile.Center, GlowColor.ToVector3() * 0.3f);
 		}
 		public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers) {
 			modifiers.SourceDamage += ChargeLevel * 0.5f;
@@ -83,25 +130,18 @@ namespace Origins.Items.Weapons.Demolitionist {
 		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
 			if (Projectile.penetrate > 0) Projectile.Kill();
 		}
+		public override void SendExtraAI(BinaryWriter writer) {
+			writer.Write((byte)useTimeMax);
+		}
+		public override void ReceiveExtraAI(BinaryReader reader) {
+			useTimeMax = reader.ReadByte();
+		}
 		public override void OnKill(int timeLeft) {
 			ExplosiveGlobalProjectile.DoExplosion(Projectile, 96, sound: SoundID.Item14);
 		}
 		public override void PostDraw(Color lightColor) {
-			Color color = Color.White;
-			switch (ChargeLevel) {
-				case 0:
-				color = Color.Chocolate.MultiplyRGBA(lightColor);
-				break;
-				case 1:
-				color = Color.Orange;
-				break;
-				case 2:
-				color = Color.Yellow;
-				break;
-				case 3:
-				color = Color.White;
-				break;
-			}
+			Color color = GlowColor;
+			color = Color.Lerp(Color.Chocolate.MultiplyRGBA(lightColor), color, GlowColor.A / 255f);
 			Main.EntitySpriteDraw(
 				glowTexture,
 				Projectile.Center - Main.screenPosition,
@@ -112,6 +152,15 @@ namespace Origins.Items.Weapons.Demolitionist {
 				Projectile.scale,
 				0
 			);
+			if (!Projectile.IsLocallyOwned() || Projectile.ai[0] != 2) return;
+			Vector2 position = Projectile.position;
+			try {
+				GetShotVelocity(Main.LocalPlayer, out Projectile.position, out Vector2 impactVelocity, out float progress);
+				Projectile.position -= Projectile.Size * 0.5f;
+				GolfHelper.DrawPredictionLine(Projectile, impactVelocity, progress, 1);
+			} finally {
+				Projectile.position = position;
+			}
 		}
 	}
 }
