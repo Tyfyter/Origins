60d9a6ffd5737f231584c51957b019f64a84001b
R31v4x-x <amandagraham232@gmail.com>
2025-11-16 20:20:59 -0500

    bugged me there was no debug item

--- Tiles/Riven/Riven_Dungeon_Chest.cs
+++ Tiles/Riven/Riven_Dungeon_Chest.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using Origins.Items.Materials;
 using Origins.World.BiomeData;
 using Terraria;
@@ -63,6 +62,13 @@ namespace Origins.Tiles.Riven {
 			Item.value = 500;
 			Item.createTile = ModContent.TileType<Riven_Dungeon_Chest>();
 			Item.glowMask = glowmask;
+		}
+	}
+	public class Locked_Riven_Dungeon_Chest_Item : Riven_Dungeon_Chest_Item {
+		public override string Texture => "Origins/Tiles/Riven/Riven_Dungeon_Chest_Item";
+		public override void SetDefaults() {
+			base.SetDefaults();
+			Item.placeStyle = 1;
 		}
 	}
 }
