5b13b0c990d34715b83f44138d8c9ae0203a8b0f
R31v4x-x <amandagraham232@gmail.com>
2025-11-18 00:17:49 -0500

    Update Hyrax_Cage.cs

--- Tiles/Other/Hyrax_Cage.cs
+++ Tiles/Other/Hyrax_Cage.cs
@@ -3,15 +3,18 @@ using Origins.Items.Other.Critters;
 
 namespace Origins.Tiles.Other {
 	public class Hyrax_Cage : CageBase<Hyrax_Item> {
-		public override string Texture => "Origins/Tiles/Other/Cicada_3301_Cage";
-		public override int LidType => 3;
+		public override int LidType => 0;
 		public override int[] FrameIndexArray { get; } = new int[Main.cageFrames];
-		public override CageKinds CageKind => CageKinds.SmallCage;
+		public override CageKinds CageKind => CageKinds.BigCage;
 		public override void ExtraAnimate() {
 			for (int k = 0; k < Main.cageFrames; k++) {
-				if (++FrameCounter[k] >= Main.rand.Next(10, 30)) {
+				int extraTime = 0;
+				if (FrameIndexArray[k] == 0) extraTime = Main.rand.Next(30, 40);
+				if (FrameIndexArray[k] == 16) extraTime = Main.rand.Next(10, 20);
+				if (FrameIndexArray[k] == 18) extraTime = Main.rand.Next(40, 70);
+				if (++FrameCounter[k] >= Main.rand.Next(2, 6) + extraTime) {
 					FrameCounter[k] = 0;
-					if (++FrameIndexArray[k] >= 20)
+					if (++FrameIndexArray[k] >= 30)
 						FrameIndexArray[k] = 0;
 				}
 			}
