f833b084b8e43d1f38a628d472e8186d1c15c16e
Tyfyter <tyfyter2616@gmail.com>
2025-12-11 12:46:35 -0700

    We forgor 🦴💀🦴

--- Items/Accessories/Dysfunctional_Endless_Explosives_Bag.cs
+++ Items/Accessories/Dysfunctional_Endless_Explosives_Bag.cs
@@ -9,9 +9,15 @@ namespace Origins.Items.Accessories {
 			"Combat",
 			"ExplosiveBoostAcc"
 		];
+		public override void Load() {
+			On_Item.CanShimmer += (orig, self) => {
+				if (self.type == Type && !Main.hardMode) return false;
+				return orig(self);
+			};
+		}
 		public override void SetStaticDefaults() {
-			ItemID.Sets.ShimmerTransformToItem[ItemID.MagicQuiver] = ModContent.ItemType<Dysfunctional_Endless_Explosives_Bag>();
-			ItemID.Sets.ShimmerTransformToItem[ModContent.ItemType<Dysfunctional_Endless_Explosives_Bag>()] = ItemID.MagicQuiver;
+			ItemID.Sets.ShimmerTransformToItem[ItemID.MagicQuiver] = Type;
+			ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MagicQuiver;
 		}
 		public override void SetDefaults() {
 			Item.DefaultToAccessory(20, 34);
@@ -20,6 +26,9 @@ namespace Origins.Items.Accessories {
 		}
 		public override void UpdateEquip(Player player) {
 			player.GetModPlayer<OriginPlayer>().endlessExplosives = true;
+		}
+		public override void Update(ref float gravity, ref float maxFallSpeed) {
+			
 		}
 	}
 }
