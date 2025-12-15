33b3cf89985a6cc53b7944206f4d020e6c58bd49
Tyfyter <tyfyter2616@gmail.com>
2025-12-15 14:42:18 -0700

    some food

---
+++ Items/Other/Consumables/Food/BBQ_Skewer.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Items.Other.Consumables.Food {
+	public class BBQ_Skewer : ModItem {
+        public override void SetStaticDefaults() {
+			Item.ResearchUnlockCount = 5;
+			ItemID.Sets.FoodParticleColors[Type] = [
+				FromHexRGB(0x753F1A),
+				FromHexRGB(0x342613),
+				FromHexRGB(0x853E15)
+			];
+			ItemID.Sets.IsFood[Type] = true;
+			Main.RegisterItemAnimation(Type, new DrawAnimationVertical(int.MaxValue, 3));
+		}
+		public override void SetDefaults() {
+			Item.DefaultToFood(
+				32, 24,
+				BuffID.WellFed3,
+				60 * 60 * 2
+			);
+			Item.value = Item.sellPrice(silver: 20);
+			Item.rare = ItemRarityID.Orange;
+		}
+	}
+}
---
+++ Items/Other/Consumables/Food/Crockin_Sprocks.cs
@@ -0,0 +1,37 @@
+﻿using Origins.Items.Materials;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.Items.Other.Consumables.Food {
+	public class Crockin_Sprocks : ModItem {
+        public override void SetStaticDefaults() {
+			Item.ResearchUnlockCount = 5;
+			ItemID.Sets.FoodParticleColors[Type] = [
+				FromHexRGB(0xD3A189),
+				FromHexRGB(0x9A380B),
+				FromHexRGB(0x753F1A)
+			];
+			ItemID.Sets.IsFood[Type] = true;
+			Main.RegisterItemAnimation(Type, new DrawAnimationVertical(int.MaxValue, 3));
+		}
+		public override void SetDefaults() {
+			Item.DefaultToFood(
+				32, 24,
+				BuffID.WellFed2,
+				60 * 60 * 5
+			);
+			Item.value = Item.sellPrice(silver: 20);
+			Item.rare = ItemRarityID.Orange;
+		}
+		public override bool ConsumeItem(Player player) {
+			player.AddBuff(BuffID.Regeneration, Item.buffTime);
+			return true;
+		}
+		public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ModContent.ItemType<Biocomponent10>(), 10)
+			.AddTile(TileID.CookingPots)
+			.Register();
+	}
+}
