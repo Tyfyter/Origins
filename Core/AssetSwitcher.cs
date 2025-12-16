b7753fcef5880ba6777bb18284e05e907f91fb94
Tyfyter <tyfyter2616@gmail.com>
2025-12-16 14:29:44 -0700

    I guess it doesn't even really need to be assets

--- Core/AssetSwitcher.cs
+++ Core/AssetSwitcher.cs
@@ -9,15 +9,15 @@ namespace Origins.Core {
 	public interface IPreDrawAnything {
 		public void PreDrawAnything();
 	}
-	public class AprilFoolsAssetSwitcher<T> : IPreDrawAnything where T : class {
-		static readonly List<(RefGet<Asset<T>> slot, Asset<T> afOn, Asset<T> afOff)> options = [];
+	public class AprilFoolsAssetSwitcher<TAsset> : IPreDrawAnything {
+		static readonly List<(RefGet<TAsset> slot, TAsset afOn, TAsset afOff)> options = [];
 		static bool lastAF = false;
-		public static void Add(RefGet<Asset<T>> slot, Asset<T> afOn, Asset<T> afOff = null) => options.Add((slot, afOn, afOff ?? slot()));
-		static AprilFoolsAssetSwitcher() => OriginSystem.assetSwitchers.Add(new AprilFoolsAssetSwitcher<T>());
+		public static void Add(RefGet<TAsset> slot, TAsset afOn) => options.Add((slot, afOn, slot()));
+		static AprilFoolsAssetSwitcher() => OriginSystem.assetSwitchers.Add(new AprilFoolsAssetSwitcher<TAsset>());
 		void IPreDrawAnything.PreDrawAnything() {
 			if (!lastAF.TrySet(OriginsModIntegrations.CheckAprilFools())) return;
 			for (int i = 0; i < options.Count; i++) {
-				(RefGet<Asset<T>> slot, Asset<T> afOn, Asset<T> afOff) = options[i];
+				(RefGet<TAsset> slot, TAsset afOn, TAsset afOff) = options[i];
 				slot() = lastAF ? afOn : afOff;
 			}
 		}
@@ -26,9 +26,9 @@ namespace Origins.Core {
 	/// Helper methods
 	/// </summary>
 	public static class AprilFoolsTextures {
-		public static void AddItem(int type, Asset<Texture2D> afOn) => AprilFoolsAssetSwitcher<Texture2D>.Add(() => ref TextureAssets.Item[type], afOn, TextureAssets.Item[type]);
+		public static void AddItem(int type, Asset<Texture2D> afOn) => AprilFoolsAssetSwitcher<Asset<Texture2D>>.Add(() => ref TextureAssets.Item[type], afOn);
 		public static void AddItem(ModItem item) => AddItem(item.Type, ModContent.Request<Texture2D>(item.Texture + "_AF"));
-		public static void AddNPC(int type, Asset<Texture2D> afOn) => AprilFoolsAssetSwitcher<Texture2D>.Add(() => ref TextureAssets.Npc[type], afOn, TextureAssets.Npc[type]);
+		public static void AddNPC(int type, Asset<Texture2D> afOn) => AprilFoolsAssetSwitcher<Asset<Texture2D>>.Add(() => ref TextureAssets.Npc[type], afOn);
 		public static void AddNPC(ModNPC npc) => AddNPC(npc.Type, ModContent.Request<Texture2D>(npc.Texture + "_AF"));
 	}
 }
