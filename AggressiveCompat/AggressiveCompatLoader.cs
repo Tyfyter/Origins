451985f53a14eac20b26665d85fdc279bb4bb1ce
Tyfyter <tyfyter2616@gmail.com>
2025-12-16 20:42:46 -0700

    Should fix fargos

---
+++ AggressiveCompat/AggressiveCompatLoader.cs
@@ -0,0 +1,22 @@
+﻿using MonoMod.Cil;
+using System;
+using System.Reflection;
+using Terraria.ModLoader;
+
+namespace Origins.AggressiveCompat {
+	internal abstract class AggressiveCompatLoader : ILoadable {
+		public abstract bool IsLoadingEnabled(Mod mod);
+		public abstract void Load();
+		void ILoadable.Load(Mod mod) {
+			try {
+				Load();
+			} catch (Exception ex) {
+				if (Origins.LogLoadingILError(GetType().Name, ex)) throw;
+			}
+		}
+		void ILoadable.Unload() { }
+		protected static void ILHook(string modName, string typeName, string methodName, ILContext.Manipulator hook) {
+			MonoModHooks.Modify(ModLoader.GetMod(modName).Code.GetType(typeName).GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance), hook);
+		}
+	}
+}
---
+++ AggressiveCompat/FargoWingFix.cs
@@ -0,0 +1,22 @@
+﻿using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.AggressiveCompat {
+	internal class FargoWingFix : AggressiveCompatLoader {
+		const string mod_name = "Fargowiltas";
+		public override bool IsLoadingEnabled(Mod mod) => ModLoader.HasMod(mod_name);
+		public override void Load() {
+			ILHook(mod_name, "Fargowiltas.Items.FargoGlobalItem", "VerticalWingSpeeds", static il => {
+				ILCursor c = new(il);
+				if (!c.TryGotoNext(i => i.MatchLdfld<Item>(nameof(Item.wingSlot)))) return;
+				c.GotoNext(MoveType.After, i => i.MatchBrtrue(out _));
+				ILLabel skip = c.MarkLabel();
+				c.GotoPrev(MoveType.AfterLabel, i => i.MatchLdsfld<ArmorIDs.Wing.Sets>(nameof(ArmorIDs.Wing.Sets.Stats)));
+				c.EmitBr(skip);
+			});
+		}
+	}
+}
