89948ea4b23d6754c9a0df3ce0413499db39c019
R31v4x-x <amandagraham232@gmail.com>
2025-12-17 00:01:44 -0500

    boom shuriken now fully explosive

---
+++ CrossMod/Fargos/Projectiles/TOFargoGlobalProjectile.cs
@@ -0,0 +1,15 @@
+﻿using Fargowiltas.Projectiles.Explosives;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Origins.Items {
+	[ExtendsFromMod(nameof(Fargowiltas))]
+	public class TOFargoGlobalProjectile: GlobalProjectile {
+		public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) {
+			return entity.type == ModContent.ProjectileType<ShurikenProj>();
+		}
+		public override void SetDefaults(Projectile entity) {
+			entity.DamageType = DamageClasses.Explosive;
+		}
+	}
+}
