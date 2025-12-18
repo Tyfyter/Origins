73e35ad3a7db5db1a541c11b3d611bc39643fdc0
Tyfyter <tyfyter2616@gmail.com>
2025-12-18 13:13:29 -0700

    This gives it thrown explosive velocity bonuses
    I know it's also adding throwing scaling, but we're already adding generic scaling & it has low base damage anyway

--- CrossMod/Fargos/Items/TOFargoGlobalItem.cs
+++ CrossMod/Fargos/Items/TOFargoGlobalItem.cs
@@ -2,14 +2,14 @@
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Origins.Items {
+namespace Origins.CrossMod.Fargos.Items {
 	[ExtendsFromMod(nameof(Fargowiltas))]
 	public class TOFargoGlobalItem : GlobalItem {
 		public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
 			return entity.type == ModContent.ItemType<BoomShuriken>();
 		}
 		public override void SetDefaults(Item item) {
-			item.DamageType = DamageClasses.Explosive;
+			item.DamageType = DamageClasses.ThrownExplosive;
 			item.StatsModifiedBy.Add(Mod);
 		}
 	}
--- CrossMod/Fargos/Projectiles/TOFargoGlobalProjectile.cs
+++ CrossMod/Fargos/Projectiles/TOFargoGlobalProjectile.cs
@@ -2,14 +2,14 @@
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Origins.Items {
+namespace Origins.CrossMod.Fargos.Projectiles {
 	[ExtendsFromMod(nameof(Fargowiltas))]
-	public class TOFargoGlobalProjectile: GlobalProjectile {
+	public class TOFargoGlobalProjectile : GlobalProjectile {
 		public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) {
 			return entity.type == ModContent.ProjectileType<ShurikenProj>();
 		}
 		public override void SetDefaults(Projectile entity) {
-			entity.DamageType = DamageClasses.Explosive;
+			entity.DamageType = DamageClasses.ThrownExplosive;
 		}
 	}
 }
