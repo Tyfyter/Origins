e59746faca5ef9761bd9be4c37d26f498ecbfb2a
Tyfyter <tyfyter2616@gmail.com>
2026-01-09 20:14:36 -0700

    And no help

--- OriginPlayer.cs
+++ OriginPlayer.cs
@@ -491,7 +491,7 @@ namespace Origins {
 			if (rasterizedTime > 0) {
 				Player.velocity = Vector2.Lerp(Player.velocity, Player.oldVelocity, rasterizedTime * 0.06f);
 				Player.position = Vector2.Lerp(Player.position, Player.oldPosition, rasterizedTime * 0.06f);
-				if (Player.velocity.Y == 0) ModContent.GetInstance<Slow_Loading_Bar>().Condition.Value += Math.Abs(Player.position.X - Player.oldPosition.X) / 16f;
+				if (Player.velocity.Y == 0 && (Player.mount?.Active != true)) ModContent.GetInstance<Slow_Loading_Bar>().Condition.Value += Math.Abs(Player.position.X - Player.oldPosition.X) / 16f;
 			}
 			Player.oldVelocity = Player.velocity;
 			rivenWet = false;
