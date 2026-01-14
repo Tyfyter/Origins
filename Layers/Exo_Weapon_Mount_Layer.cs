ae62d4cffacaf039ca365c2ae09fb6f8e1a8376f
Tyfyter <tyfyter2616@gmail.com>
2026-01-13 22:36:07 -0700

    Started exo-weapon mount visual

--- Layers/Exo_Weapon_Mount_Layer.cs
+++ Layers/Exo_Weapon_Mount_Layer.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json.Linq;
 using Origins.Items.Accessories;
 using Origins.Items.Armor.Fiberglass;
+using PegasusLib;
+using System.Numerics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Origins.Layers {
@@ -27,6 +31,47 @@ namespace Origins.Layers {
 				data.effect ^= SpriteEffects.FlipHorizontally;
 				data.origin = data.effect.ApplyToOrigin(new(-4, 9), data.sourceRect.Value);
 				drawInfo.DrawDataCache[i] = data;
+
+				if (frame == 0) break;
+				Item lastItem = player.inventory[player.OriginPlayer().exoWeaponMountLastWeapon];
+				Rectangle itemDrawFrame = player.GetItemDrawFrame(lastItem.type);
+				Vector2 origin = new(itemDrawFrame.Width * 0.5f - itemDrawFrame.Width * 0.5f * player.direction, itemDrawFrame.Height);
+				if (player.gravDir == -1f) {
+					origin.Y = itemDrawFrame.Height - origin.Y;
+				}
+				switch (lastItem.useStyle) {
+					default:
+					return;
+					case ItemUseStyleID.Shoot:
+					Vector2 vector10 = Main.DrawPlayerItemPos(player.gravDir, lastItem.type);
+					if (player.direction == -1) {
+						origin = new Vector2(-vector10.X, itemDrawFrame.Height / 2);
+					} else {
+						origin = new Vector2(itemDrawFrame.Width + vector10.X, itemDrawFrame.Height / 2);
+					}
+					break;
+				}
+				//origin += vector;
+				Vector2 position = data.position + (frame switch {
+					0 => new(28, -9),
+					1 => new(28, -7),
+					2 => new(26, 2),
+					3 => new(28, 6),
+					4 => new(30, 9),
+					5 => new(28, 6),
+					6 => new(28, -7),
+					_ => new Vector2(28, -9)
+				}).Apply(data.effect, default).RotatedBy(drawInfo.rotation);
+				drawInfo.DrawDataCache.Add(new DrawData(
+					TextureAssets.Item[lastItem.type].Value,
+					position,
+					itemDrawFrame,
+					lastItem.GetAlpha(drawInfo.colorArmorBody),
+					-MathHelper.PiOver2 * player.direction,
+					origin,
+					lastItem.scale,
+					data.effect
+				));
 				break;
 			}
 		}
