fbae0ed52726da8a6a9a547ac2a827e64da2b59d
Tyfyter <tyfyter2616@gmail.com>
2026-01-15 16:12:19 -0700

    Artifiber?

--- Tiles/Ashen/Artifiber.cs
+++ Tiles/Ashen/Artifiber.cs
@@ -1,21 +1,21 @@
-﻿using Origins.Dev;
-using Origins.Walls;
+﻿using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
 namespace Origins.Tiles.Ashen {
-	public class Artifiber : OriginTile {
-		public string[] Categories => [
-			WikiCategories.Plant
-        ];
+	public class Artifiber : ComplexFrameTile {
         public override void SetStaticDefaults() {
 			Main.tileSolid[Type] = true;
 			Main.tileBlockLight[Type] = true;
 			AddMapEntry(new Color(143, 114, 94));
 			mergeID = TileID.WoodBlock;
 			DustType = DustID.WoodFurniture;
+		}
+		protected override IEnumerable<TileOverlay> GetOverlays() {
+			yield return new TileMergeOverlay(merge + "Murk_Overlay", TileType<Murky_Sludge>());
+			yield return new TileMergeOverlay(merge + "Murk_Overlay", TileType<Ashen_Murky_Sludge_Grass>());
 		}
 	}
 	public class Artifiber_Item : ModItem {
