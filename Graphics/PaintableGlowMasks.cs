97ce26f71fb0cd6a4959de213e7c8cd52968de77
Tyfyter <tyfyter2616@gmail.com>
2026-01-19 20:39:34 -0700

    Update PaintableGlowMasks.cs

--- Graphics/PaintableGlowMasks.cs
+++ Graphics/PaintableGlowMasks.cs
@@ -2,9 +2,6 @@
 using ReLogic.Content;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
