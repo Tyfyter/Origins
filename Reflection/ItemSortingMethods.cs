d410a9c19fa40cbbf482a364c58831461f8c22eb
Tyfyter <tyfyter2616@gmail.com>
2026-01-26 21:31:30 -0700

    Most of the buttons for special chests

---
+++ Reflection/ItemSortingMethods.cs
@@ -0,0 +1,11 @@
+﻿using PegasusLib.Reflection;
+using Terraria;
+using Terraria.UI;
+
+namespace Origins.Reflection {
+	internal class ItemSortingMethods : ReflectionLoader {
+		public delegate void Del_Sort(Item[] inv, params int[] ignoreSlots);
+		[ReflectionParentType(typeof(ItemSorting))]
+		public static Del_Sort Sort { get; private set; }
+	}
+}
