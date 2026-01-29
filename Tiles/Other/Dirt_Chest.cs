87732702356e420659306b6e701de211846e3be8
R31v4x-x <amandagraham232@gmail.com>
2026-01-29 18:57:54 -0500

    dirty... dirt?

---
+++ Tiles/Other/Dirt_Chest.cs
@@ -0,0 +1,40 @@
+﻿using Origins.Core;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Origins.Core.SpecialChest;
+
+namespace Origins.Tiles.Other {
+	public class Dirt_Chest : ModSpecialChest {
+		public override ChestData CreateChestData() => new Dirt_Chest_Data();
+		public override void Load() {
+			Mod.AddContent(new TileItem(this).WithExtraStaticDefaults(this.DropTileItem));
+		}
+		public override void ModifyTileData() {
+			TileObjectData.newTile.Width = 1;
+			TileObjectData.newTile.SetHeight(1);
+			AddMapEntry(FromHexRGB(0x976B4B), CreateMapEntryName(), MapChestName);
+			AdjTiles = [TileID.Containers];
+			DustType = DustID.Dirt;
+			Main.tileNoAttach[Type] = true;
+		}
+		public override LocalizedText DefaultContainerName(int frameX, int frameY) => CreateMapEntryName();
+		public override void MouseOver(int i, int j) => SpecialChest.MouseOver(i, j);
+		public override void MouseOverFar(int i, int j) {
+			MouseOver(i, j);
+			Player player = Main.LocalPlayer;
+			if (player.cursorItemIconText == "") {
+				player.cursorItemIconEnabled = false;
+				player.cursorItemIconID = ItemID.None;
+			}
+		}
+		public record class Dirt_Chest_Data() : Storage_Container_Data() {
+			public override int Capacity => 1;
+			public override int Width => 1;
+			public override int Height => 1;
+			protected internal override bool IsValidSpot(Point position) => Main.tile[position].TileIsType(ModContent.TileType<Dirt_Chest>());
+		}
+	}
+}
