6f46bda6eed74395cd1c1a9df0a32b7f0997e9a1
R31v4x-x <amandagraham232@gmail.com>
2026-02-25 01:42:11 -0500

    brine bucket sprites

--- Items/Tools/Liquids/Brine_Bucket.cs
+++ Items/Tools/Liquids/Brine_Bucket.cs
@@ -1,11 +1,8 @@
 ﻿using Origins.Liquids;
 
 namespace Origins.Items.Tools.Liquids {
-	public class Brine_Bucket : BucketBase<Brine> {
-		public override string Texture => typeof(Oil_Bucket).GetDefaultTMLName();
-	}
+	public class Brine_Bucket : BucketBase<Brine> { }
 	public class Brine_Bottomless_Bucket : Brine_Bucket {
-		public override string Texture => typeof(Oil_Bottomless_Bucket).GetDefaultTMLName();
 		public override bool Endless => true;
 	}
 }
