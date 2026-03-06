105f93cae10d595e6cee12804c724573a82f90f2
Tyfyter <tyfyter2616@gmail.com>
2026-03-06 13:41:00 -0700

    Moved Derrick 2 pixels and made huge adjustments to fix what I noticed while doing that

--- Tiles/Ashen/Oil_Derrick.cs
+++ Tiles/Ashen/Oil_Derrick.cs
@@ -1,15 +1,17 @@
-﻿using Origins.Core;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Origins.Core;
 using Origins.World.BiomeData;
 using ReLogic.Utilities;
 using Terraria;
 using Terraria.Audio;
+using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 
 namespace Origins.Tiles.Ashen {
-	public class Oil_Derrick : ModTile, IComplexMineDamageTile {
-		Sound ambientSound = EnvironmentSounds.Register<Sound>();
+	public class Oil_Derrick : ModTile, IComplexMineDamageTile, IMultiTypeMultiTile {
+		readonly Sound ambientSound = EnvironmentSounds.Register<Sound>();
 		public override void Load() => new TileItem(this).RegisterItem();
 		public override void SetStaticDefaults() {
 			Main.tileFrameImportant[Type] = true;
@@ -19,6 +21,9 @@ namespace Origins.Tiles.Ashen {
 			TileObjectData.newTile.Width = 8;
 			TileObjectData.newTile.SetHeight(7);
 			TileObjectData.newTile.SetOriginBottomCenter();
+			TileObjectData.newTile.HookPlaceOverride = MultiTypeMultiTile.PlaceWhereTrue(TileObjectData.newTile, IsPart);
+			TileObjectData.newTile.AnchorBottom = new(AnchorType.SolidTile, TileObjectData.newTile.Width - 1, 1);
+			TileObjectData.newTile.FlattenAnchors = true;
 			this.SetAnimationHeight();
 			TileObjectData.addTile(Type);
 			AddMapEntry(new Color(40, 30, 18), this.GetTileItem().DisplayName);
@@ -28,6 +33,13 @@ namespace Origins.Tiles.Ashen {
 		public override bool CanExplode(int i, int j) => false;
 		public override void AnimateTile(ref int frame, ref int frameCounter) {
 			if (frameCounter.CycleUp(8)) frame.CycleUp(7);
+		}
+		public override bool PreDrawPlacementPreview(int i, int j, SpriteBatch spriteBatch, ref Rectangle frame, ref Vector2 position, ref Color color, bool validPlacement, ref SpriteEffects spriteEffects) {
+			position.Y += 2;
+			return base.PreDrawPlacementPreview(i, j, spriteBatch, ref frame, ref position, ref color, validPlacement, ref spriteEffects);
+		}
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) {
+			offsetY = 2;
 		}
 		public override void NearbyEffects(int i, int j, bool closer) {
 			if (closer) return;
@@ -43,9 +55,45 @@ namespace Origins.Tiles.Ashen {
 					playingSound.Volume = 0.2f / float.Max(pos.DistanceSQ(Main.Camera.Center) / (16 * 20 * 16 * 20), 1);
 					return true;
 				});
-				if (Main.tileFrame[type] == 1) SoundEngine.PlaySound(SoundID.Item108.WithPitch(-1.4f).WithVolume(0.32f * mult), position);
+				if (Main.tileFrame[type] == 5) SoundEngine.PlaySound(SoundID.Item108.WithPitch(-1.4f).WithVolume(0.32f * mult), position);
 				if (Main.rand.NextBool(150)) SoundEngine.PlaySound(SoundID.Item148.WithPitch(-0.5f).WithVolume(0.48f * mult), position);
 			}
 		}
+		public static bool IsPart(int i, int j) {
+			switch (i, j) {
+				case (0, 0):
+				case (0, 1):
+				case (0, 2):
+				case (0, 3):
+				case (0, 6):
+
+				case (1, 0):
+				case (1, 1):
+				case (1, 2):
+
+				case (2, 0):
+				case (2, 1):
+				case (2, 2):
+
+				case (3, 0):
+				case (4, 0):
+				case (5, 0):
+				return false;
+			}
+			return true;
+		}
+
+		public bool IsValidTile(Tile tile, int left, int top) {
+			(int i, int j) = tile.GetTilePosition();
+			i -= left;
+			j -= top;
+			if (IsPart(i, j)) {
+				if (tile.TileType != Type) return false;
+				Tile topLeft = Main.tile[left, top];
+				return tile.TileFrameX == topLeft.TileFrameX + i * 18 && tile.TileFrameY == topLeft.TileFrameY + j * 18;
+			}
+			return true;
+		}
+		public bool ShouldBreak(int x, int y, int left, int top) => IsPart(x - left, y - top);
 	}
 }
