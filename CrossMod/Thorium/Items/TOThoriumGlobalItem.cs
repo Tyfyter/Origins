e95d81c9d682f4f5ed50a3369390733825dd13c3
R31v4x-x <amandagraham232@gmail.com>
2026-03-13 23:50:00 -0400

    thorium items now explosive

---
+++ CrossMod/Thorium/Items/TOThoriumGlobalItem.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Origins.CrossMod.Thorium.Items {
+	[ExtendsFromMod("ThoriumMod")]
+	public class TOThoriumGlobalItem : GlobalItem {
+		internal static bool isOriginsItemCloningDefaults = false;
+		public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
+			return entity?.ModItem?.Mod != OriginsModIntegrations.Thorium;
+		}
+		public override void SetDefaults(Item item) {
+			bool statsModified = false;
+			if (item.useAmmo == ItemID.RocketI) item.DamageType = DamageClasses.ExplosiveVersion[DamageClass.Ranged];
+
+			if (statsModified && !isOriginsItemCloningDefaults) item.StatsModifiedBy.Add(Mod);
+		}
+	}
+}
