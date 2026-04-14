ec2d956cb646cdf339825e2e96db4bf0b9d02db5
Tyfyter <tyfyter2616@gmail.com>
2026-04-14 16:32:25 -0600

    Truss-t me, this wasn't complicated

---
+++ Walls/Truss_Wall.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ID;
+
+namespace Origins.Walls; 
+public class Truss_Wall : OriginsWall {
+	public override WallVersion WallVersions => WallVersion.Placed_Unsafe | WallVersion.Safe;
+	public override Color MapColor => new Color(190, 145, 112);
+	public override int DustType => DustID.Copper;
+	public override bool CanBeReplacedByWallSpread => false;
+	public override void SetStaticDefaults() {
+		base.SetStaticDefaults();
+		WallID.Sets.AllowsWind[Type] = true;
+		WallID.Sets.Transparent[Type] = true;
+	}
+}
