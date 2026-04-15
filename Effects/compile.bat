3d9d3ce1da37308c84af12b283429408f2174c62
Tyfyter <tyfyter2616@gmail.com>
2026-04-14 20:42:11 -0600

    Better .fx compiler

--- Effects/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-fxcompiler.exe>log.log
-powershell -Command "(gc log.log) -replace 'C:\\Users\\%USERNAME%\\Documents\\My Games\\Terraria\\tModLoader\\ModSources\\Origins\\Effects\\', '' | Out-File -encoding ASCII log.log"
-log.log
