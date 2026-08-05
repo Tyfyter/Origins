cd8bdb916f4766eec4dfe8827a28331c56ae1250
R31v4x-x <amandagraham232@gmail.com>
2026-08-05 02:34:59 -0400

    moved classified blueprint entry

--- Items/Other/Consumables/Classified_Blueprint.cs
+++ Items/Other/Consumables/Classified_Blueprint.cs
@@ -6,6 +6,7 @@ using JournalSortIndex = Origins.Journal.JournalEntry.JournalSortIndex;
 
 namespace Origins.Items.Other.Consumables;
 public class Classified_Blueprint : MaterialItem, IJournalEntryProvider {
+	public static JournalSortIndex SortIndex => new("Mechanicus_Sovereignty", 7);
 	public override int ResearchUnlockCount => 1;
 	public override int Rare => ItemRarityID.Orange;
 	public override int Value => Item.sellPrice(gold: 3);
