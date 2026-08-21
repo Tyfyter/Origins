ae396911638fdbffc8ea34ef9821f5ebe3d79c32
Tyfyter <tyfyter2616@gmail.com>
2026-08-20 19:37:44 -0600

    Animate amebic gelfall

--- Liquids/Waterfalls/Amebic_Gel_Fall.cs
+++ Liquids/Waterfalls/Amebic_Gel_Fall.cs
@@ -10,5 +10,8 @@ namespace Origins.Liquids.Waterfalls {
 			float mult = 0.666f * World.BiomeData.Riven_Hive.NormalGlowValue.GetValue();
 			Lighting.AddLight(i, j, 0, 0.9f * mult, 1f * mult);
 		}
+		public override void AnimateWaterfall(ref int frame, ref int frameBackground, ref int frameCounter) {
+			if (frameCounter.CycleUp(5)) frame.CycleUp(15);
+		}
 	}
 }
