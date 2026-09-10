443067c9ab655881dbddbbc5fd7657458d1abf1f
R31v4x-x <amandagraham232@gmail.com>
2026-09-09 21:01:38 -0400

    Update Amebic_Gel_Bucket.cs

--- Items/Tools/Liquids/Amebic_Gel_Bucket.cs
+++ Items/Tools/Liquids/Amebic_Gel_Bucket.cs
@@ -2,7 +2,6 @@
 
 namespace Origins.Items.Tools.Liquids {
 	public class Amebic_Gel_Bottomless_Bucket : BucketBase<Amebic_Gel> {
-		public override string Texture => typeof(Brine_Bottomless_Bucket).GetDefaultTMLName();
 		public override bool Endless => true;
 	}
 }
